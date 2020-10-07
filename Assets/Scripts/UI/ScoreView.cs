d25b4e9a88bd0b07016ac3a249d9870a929a6092
Rustam Mamedov <mamedov@panzerdog.com>
2020-10-07 08:11:05 +0200

    ScoreView class created

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using Events;
+using Game;
+using UnityEngine;
+
+namespace UI {
+
+    public class ScoreView : MonoBehaviour {
+
+        [SerializeField]
+        private float _scoreCountDelay;
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScoreValue;
+
+        private int _currentScore;
+        private bool isBusy;
+
+        private void Awake() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            if (_currentScoreValue.value > _currentScore && !isBusy) {
+                StartCoroutine(SetScoreCoroutine(_currentScoreValue.value));
+            }
+        }
+
+        public IEnumerator SetScoreCoroutine(int score) {
+            isBusy = true;
+            while (_currentScore < score) {
+                _currentScore++;
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(_scoreCountDelay);
+            }
+            isBusy = false;
+        }
+    }
+}
