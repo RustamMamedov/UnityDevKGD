ca6f7ab7df6de32fa502440fe53473f730357b01
Valeriy <mr.robariii@gmail.com>
2020-10-08 19:51:55 +0200

    ScoreView Commit
    ScoreView script added
    Additional: Removed useless libraries from (Preloader, UIManager), GamePlay Scene updated, Resources/CarDodged updated

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,33 @@
+﻿using Events;
+using Game;
+using System.Collections;
+using UnityEngine;
+
+namespace UI {
+    public class ScoreView : MonoBehaviour {
+
+        public ScriptableIntValue currentScoreAsset;
+        public EventListener eventListener;
+
+        [SerializeField]
+        private int _currentScore = 0;
+
+        private void Start() {
+            eventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            if(currentScoreAsset.score > _currentScore) {
+                StartCoroutine(SetScoreCoroutine(currentScoreAsset.score));
+            }
+        }
+
+        private IEnumerator SetScoreCoroutine(int score) {
+            while (_currentScore < score) {
+                _currentScore++;
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
