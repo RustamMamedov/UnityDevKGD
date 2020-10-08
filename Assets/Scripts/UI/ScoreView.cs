28d1c3bbb7ed03c0e719aff6bf97abd192e1b16b
Evgenii <jekaweek1@gmail.com>
2020-10-08 20:15:18 +0200

    ScoreView commit
    Add class ScoreView and test method UpdateBehaviour on Gameplay Scene

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game;
+using Events;
+namespace UI {
+
+    public class ScoreView : MonoBehaviour {
+
+        public ScriptableIntValue currentScore;
+
+        private int _currentScore;
+
+        public EventListener eventListener;
+
+        private void Start() {
+            eventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            if(currentScore.score > _currentScore) {
+                StartCoroutine(SetScoreCoroutine(currentScore.score));
+            }
+        }
+        
+        private IEnumerator SetScoreCoroutine(int newScore) {
+
+            while(_currentScore < newScore) {
+                _currentScore++;
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+        }
+    }
+
+}
