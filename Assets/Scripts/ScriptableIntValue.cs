5c5089d0405ef2ecdb168db6b4c9e2aab54233b5
SaraJess <sarycheva.00@mail.ru>
2020-10-08 22:57:35 +0200

    Create ScoreView

--- Assets/Scripts/ScriptableIntValue.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Score", menuName = "New Score")]
-public class ScriptableIntValue : ScriptableObject
-{
-    public int score;
-}
---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,26 @@
+﻿using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI { 
+    public class ScoreView : MonoBehaviour
+    {
+            [SerializeField]
+            private ScriptableIntValue CurrentScore; //отслеживает текущий счет
+
+            [SerializeField]
+            private int _currentScore; //счет, который выводится
+
+            private IEnumerator SetScoreCoroutine() { 
+                
+            }
+
+            private void UpdateBehaviour() {
+                if (_currentScore < CurrentScore.value) {
+                    StartCoroutine(SetScoreCoroutine());
+                }
+            }
+
+    }
+}
