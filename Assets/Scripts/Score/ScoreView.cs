41bf553d47ff8e318d7e9ee718f52aa0c5d70632
Alexandr <navacar@mail.ru>
2020-10-09 17:18:53 +0200

    Adding ScoreView
    adding ScoreView.cs and debuging score in console

---
+++ Assets/Scripts/Score/ScoreView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+using Events;
+
+namespace UI {
+
+    public class ScoreView : MonoBehaviour {
+        [SerializeField] 
+        private ScriptableIntValue _scoreLink;
+        [SerializeField]
+        private EventListener _update;
+
+        private int _currentScore = 0;
+        
+        private void Awake() {
+            _update.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            if (_scoreLink.score > _currentScore) {
+                StartCoroutine(SetScoreCoroutine(_scoreLink.score));
+            }
+        }
+
+        private IEnumerator SetScoreCoroutine(int score) {
+            while (_currentScore != score) {
+                _currentScore ++;
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
