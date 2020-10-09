db1c55a261464ba02e7230e553e00d140f2a5353
Alex Silkov <st.jimmy39@mail.ru>
2020-10-09 08:28:50 +0200

    Preloader class

---
+++ Assets/Scripts/UI/Preloader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI {
+
+
+    public class Preloader : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+
+            StartCoroutine(LoadMenuScene());
+        }
+
+        private IEnumerator LoadMenuScene() {
+
+            var asyncOperation = SceneManager.LoadSceneAsync("Menu");
+            while (!asyncOperation.isDone) {
+                Debug.Log(asyncOperation.progress);
+                yield return null;
+            }
+            
+        }
+    }
+}
