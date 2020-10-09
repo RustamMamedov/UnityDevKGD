e9fd40c94de3b68c93f6f68a0562bc00bc71ff3a
Nikita <Nkanunov@gmail.com>
2020-10-09 15:55:35 +0200

    ScoreView script changed

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
+using Events;
+using Game;
 using UnityEngine;
 
 namespace UI {
 
     public class ScoreView : MonoBehaviour {
-        private uint _currentScore;
+
+        [SerializeField]
+        private ScriptableIntValue _scriptableIntValue;
+
+        [SerializeField]
+        private EventListener _eventListener;
+        [SerializeField]
+        private ScriptableEvent _event;
+
+        private uint _currentScore = 0;
+
+        
+        private void Awake() {
+            _eventListener.onEventHappened += UpdateBehavior;
+        }
 
         private void UpdateBehavior() {
+
+            while (_currentScore < _scriptableIntValue.currentSсore) {
+                StartCoroutine(SetScoreCoroutine(_scriptableIntValue.currentSсore));
+            }
+
+        }
+
+        private IEnumerator SetScoreCoroutine(int score) { 
+             Debug.Log(_currentScore);  
+           
+            while (_currentScore < score) {      
+                _currentScore++; 
+                yield return new WaitForSeconds(0.1f); 
+            } 
             
-        }
+        } 
 
-        private IEnumerator SetScoreCoroutine (){
-
-        }
     }
 }
