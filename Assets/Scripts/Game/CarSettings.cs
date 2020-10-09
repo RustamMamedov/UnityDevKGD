b259e83ccd330c0edd8b195abbeebb4a53ea8ce1
Alexandr <navacar@mail.ru>
2020-10-09 14:52:13 +0200

    fix dodgeScore
    made it public

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Car setting", menuName = "Car setting")]
 public class CarSettings : ScriptableObject {
     [SerializeField] 
-    private int _dodgeScore; 
+    public int _dodgeScore; 
 }
