f237049c9c155d27877df0e7df7de3fb6e269d0d
Maxim <korunets1901@gmail.com>
2020-10-10 21:56:01 +0300

    Hurray, it's alive ...
    The SetScoreCoroutine coroutine takes the current score of the player from the asset CurrentScore as a parameter and starts changing the _currentScore by 1 at each iteration.

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -3,24 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Values;
+using Events;
 
 namespace UI {
     public class ScoreView : MonoBehaviour
     {
         private int _currentScore;
-        
+
+        [SerializeField]
         private ScriptableIntValue _valueScore;
 
+        [SerializeField]
+        private EventListener _updateEventListener;
+        private void Start() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
         public void UpdateBehaviour() {
-            while (_valueScore.score >= _currentScore) {
-            StartCoroutine(SetScoreCoroutine(_valueScore.score));
-            Debug.Log(_currentScore);
+            if(_valueScore.score >= _currentScore) {
+            StartCoroutine(SetScoreCoroutine(_valueScore.score));    
             }
+            Debug.Log(_currentScore);
         
         }
 
         private IEnumerator SetScoreCoroutine(int valueScore) {
-            while(_currentScore < valueScore) {
+            while(valueScore > _currentScore) {
                 _currentScore++;
                 yield return new WaitForSeconds(0.1f);
             }
