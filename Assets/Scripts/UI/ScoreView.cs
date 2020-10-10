7a43a0711565837ccdfe25e3d09cf7ef4788d66f
Maxim <korunets1901@gmail.com>
2020-10-10 20:18:06 +0300

    Everything is working
    Created the ScoreView class, added a link to the CurrentScore and an integer private field _currentScore

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -1,102 +1,24 @@
-<<<<<<< Updated upstream
-﻿using Events;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Values;
-
-namespace UI {
-
-    public class ScoreView : MonoBehaviour {
-
-        // Fields.
-
-        [SerializeField]
-        private EventListener _updateEventListener;
-
-        [SerializeField]
-        private ScriptableIntValue _currentScoreSource;
-
-        [Tooltip("Time between displayed score updates.")]
-        [SerializeField]
-        private float _currentScoreIncrementDelay = 0.1f;
-
-        // Displayed score.
-        private int _currentScore;
-
-        // Number of the current SetScoreCoroutine.
-        private int _currentSetScoreCoroutine = 0;
-
-        // To what value is current score being changed at the moment.
-        private int _currentTargetScore;
-
-
-        // Life cycle.
-
-        private void Start() {
-            _currentScore = _currentScoreSource.score;
-            _currentTargetScore = _currentScore;
-            _updateEventListener.OnEventHappened += UpdateBehaviour;
-        }
-
-        private void UpdateBehaviour() {
-            var targetScore = _currentScoreSource.score;
-            if (_currentTargetScore != targetScore) {
-                StartCoroutine(SetScoreCoroutine(targetScore));
-            }
-            Debug.Log(_currentScore);
-        }
-
-
-        // Coroutines.
-
-        // Update score by one toward target every _currentScoreIncrementDelay seconds.
-        private IEnumerator SetScoreCoroutine(int targetScore) {
-
-            // Make number and set it as current coroutine number.
-            // If current coroutine number is changed, this coroutine must stop.
-            // As a result, no more than one coroutine will change _currentScore
-            // at any given frame (unless several are called simultaneously).
-            int coroutineNumber = _currentSetScoreCoroutine + 1;
-            _currentSetScoreCoroutine = coroutineNumber;
-            _currentTargetScore = targetScore;
-
-            // Update current score.
-            int scoreChange = _currentScore < targetScore ? 1 : -1;
-            while (true) {
-                if (_currentSetScoreCoroutine != coroutineNumber || _currentScore == targetScore) {
-                    break;
-                }
-                _currentScore += scoreChange;
-                yield return new WaitForSeconds(_currentScoreIncrementDelay);
-            }
-
-        }
-
-
-    }
-
-}
-
-=======
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace UI {
-    public class NewBehaviourScript : MonoBehaviour
+    public class ScoreView : MonoBehaviour
     {
-        [SerializeField]
         private int  _currentScore;
-
+        
+        private ScriptableIntValue _valueScore;
         public void UpdateBehaviour() {
-            StartCorutine(SetScoreCoroutine());
+            while (CurrentScore >= _currentScore) {
+            StartCorutine(SetScoreCoroutine(CurrentScore));
+            }
         }
 
         private IEnumerator SetScoreCoroutine(CurrentScore) {
-            
+            while(_currentScore < CurrentScore) {
+                _currentScore++;
+            }
         } 
     }
 }
->>>>>>> Stashed changes
