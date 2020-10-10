bce60849ceb4a05e0bf3ac1e4f12cfaefadff33c
Sevastian Kozhunov <sky.seva@gmail.com>
2020-10-10 18:38:25 +0200

    Create ScoreView script
    Create ScoreView script which allows to show current game score and slowly update what's showed when original is changed.

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,63 @@
+﻿using Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Values;
+
+namespace UI {
+
+    public class ScoreView : MonoBehaviour {
+
+        // Fields.
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScoreSource;
+
+        [Tooltip("Time between displayed score updates.")]
+        [SerializeField]
+        private float _currentScoreIncrementDelay = 0.1f;
+
+        // Displayed score.
+        private int _currentScore;
+
+        // True if score is currently updated.
+        private bool _isBeingUpdated;
+
+
+        // Life cycle.
+
+        private void Start() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            var targetScore = _currentScoreSource.score;
+            if (_currentScore != targetScore) {
+                StartCoroutine(SetScoreCoroutine(targetScore));
+            }
+            Debug.Log(_currentScore);
+        }
+
+
+        // Coroutines.
+
+        // Update score by one toward target every _currentScoreIncrementDelay seconds.
+        private IEnumerator SetScoreCoroutine(int targetScore) {
+            while (_currentScore != targetScore) {
+                if (_currentScore < targetScore) {
+                    _currentScore--;
+                } else {
+                    _currentScore++;
+                }
+                yield return new WaitForSeconds(_currentScoreIncrementDelay);
+            }
+        }
+
+
+    }
+
+}
+
