0677439de6f185ba4422c802eeeb94d8f9c4f1da
BepeckoB <BepeckoB@gmail.com>
2020-10-10 14:15:45 +0200

    created ScoreView script

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+using Events;
+
+namespace UI {
+
+    public class ScoreView : MonoBehaviour {
+
+        [SerializeField]
+        private int _currentScore;
+
+        [SerializeField]
+        private ScriptableIntValue _assetScore;
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        private void Awake() {
+            _updateEventListener.OnEventHappened += UpdateBehavior;
+        }
+
+        public void UpdateBehavior() {
+            if (_currentScore < _assetScore.score) {
+               StartCoroutine(SetScoreCoroutine(_assetScore.score));
+            }
+        }
+
+        private IEnumerator SetScoreCoroutine(int assetScore) {
+
+            while (_currentScore != assetScore) {
+
+                if (_currentScore < assetScore) {
+                    _currentScore++;
+                }
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+    }
+}
