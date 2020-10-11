5692fd06f4dcfd11e4fb323c465e7372650fa331
vladislav <vladislavnikwork@gmail.com>
2020-10-11 18:46:57 +0200

    Fixed "ScoreView.cs"
    Fixed constantly calling the launch of Сoroutine in the  "UpdateBehaviour" when calling Update

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -18,23 +18,47 @@ namespace UI {
         [SerializeField]
         private float _currentScoreDelay = 0.1f;
 
+        private int _scoreChanged;
+
+        private bool _isStartCoroutine = false;
+        private bool _isCurrentScoreChanged = true;
+
         private void Awake() {
             _eventListener.OnEventHappened += UpdateBehaviour;
+            _scoreChanged = _currentScoreAsset._score;
         }
 
         private void UpdateBehaviour() {
-            var target = _currentScoreAsset._score;
-            if (target > _currentScore) {
-                Debug.Log(_currentScore + "->" + target);
-                StartCoroutine(SetScoreCoroutine(target));
+            var target =_currentScore;
+
+            if (_scoreChanged != _currentScoreAsset._score) {
+                _isCurrentScoreChanged = true;
+                _scoreChanged = _currentScoreAsset._score;
             }
+
+            if (target < _currentScoreAsset._score && _isCurrentScoreChanged) {
+                _isCurrentScoreChanged = false;
+                _isStartCoroutine = true;
+                target = _currentScoreAsset._score;
+            }
+
+            if (_isStartCoroutine) {
+                _isStartCoroutine = false;
+                Debug.Log(_currentScore + " -> " + target);
+                StartCoroutine(SetScoreCoroutine(target));
+
+            }
+
         }
 
         private IEnumerator SetScoreCoroutine(int target) {
-
+            
             while (_currentScore < target) {
                 _currentScore++;
                 Debug.Log(_currentScore + " -> " + target);
+                if (target!= _currentScoreAsset._score) {
+                    target = _currentScoreAsset._score;
+                }
                 yield return new WaitForSeconds(_currentScoreDelay);
             }
 
