df521a280aadbe2b2cd11d1965164c4e7b48c580
Potapenko_Ivan <van.van123@list.ru>
2020-10-11 20:13:17 +0200

    Added script ScoreView

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,56 @@
+﻿using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace UI {
+
+    public class ScoreView : MonoBehaviour {
+
+        [SerializeField]
+        private int _currentScore;
+
+        [SerializeField]
+        private ScriptableIntValue _scriptableIntValue;
+
+        [SerializeField]
+        private EventListener _updateListener;
+
+        [SerializeField]
+        private bool _isActiveSetScoreCoroutine = false;
+
+        private void Awake() {
+
+            _updateListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private IEnumerator SetScoreCoroutine(int score) {
+
+            while (score > _currentScore) {
+                _currentScore++;
+                yield return new WaitForSeconds(0.1f);
+
+            }
+
+            _isActiveSetScoreCoroutine = false;
+
+            yield return null;
+
+        }
+
+        public void UpdateBehaviour() {
+
+            if (_scriptableIntValue.value > _currentScore && !_isActiveSetScoreCoroutine) {
+                _isActiveSetScoreCoroutine = true;
+                StartCoroutine(SetScoreCoroutine(_scriptableIntValue.value));
+            }
+            Debug.Log(_currentScore);
+
+        }
+
+    }
+
+}
+
+
