87dcdbeadaab62b1b4c66e82d33044aae07e62b5
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-10-11 23:43:49 +0200

    ScoreView class

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UI {
+    public class ScoreView : MonoBehaviour {
+
+        [SerializeField]
+        private int _currentScore = 0; 
+
+        public Game.ScriptableIntValue currentScoreAsset;
+        public Events.EventListener eventListener;
+
+        private void Awake() {
+
+            eventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+        
+            if (_currentScore < currentScoreAsset.score) {
+                StartCoroutine(SetScoreCoroutine(currentScoreAsset.score));
+            }
+                
+        }
+
+        private IEnumerator SetScoreCoroutine(int gameScore) {
+
+            while (_currentScore < gameScore) {
+                _currentScore++;
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+    }
+}
+
