24bb240b62bf8323c405e6f34ff6206137d02664
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-12 12:15:51 +0200

    Created Score View

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events; 
+using Game; 
+
+ 
+namespace UI { 
+ 
+    public class ScoreView : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableIntValue _currentScoreAsset;
+
+        [SerializeField]
+        private int _currentScore;
+
+        [SerializeField]
+        private EventListener _eventListener;
+
+
+        private void Awake() {
+            _eventListener.OnEventHappend += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            if (_currentScoreAsset.score > _currentScore)
+                StartCoroutine(SetScoreCoroutine(_currentScoreAsset.score));
+            
+        }
+
+        private IEnumerator SetScoreCoroutine(int score) {
+            while (_currentScore < score) {
+                _currentScore++;
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
