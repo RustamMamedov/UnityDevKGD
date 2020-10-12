d9fe8974ecbc9d6a3c0fc7618c6574d2a28cae94
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-12 12:18:43 +0200

    Created Fader

---
+++ Assets/Scripts/UI/Fader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+namespace UI {
+    public class Fader : MonoBehaviour {
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+        [SerializeField]
+        private float _fadeTime;
+
+        public event Action OnFadeIn;
+        public event Action OnFadeOut = delegate { };
+
+        public void FadeIn() {
+            StartCoroutine(FadeInCoroutine());
+        }
+
+        public void FadeOut() {
+            StartCoroutine(FadeOutCoroutine());
+        }
+
+        private IEnumerator FadeInCoroutine() {
+            yield return StartCoroutine(FadeCoroutine(1f, 0f));
+            OnFadeIn.Invoke();
+        }
+
+        private IEnumerator FadeOutCoroutine() {
+            yield return StartCoroutine(FadeCoroutine(0f, 1f));
+            OnFadeOut();
+        }
+
+        private IEnumerator FadeCoroutine(float fromAlpha, float targetAlpha) {
+            var timer = 0f;
+
+            _canvasGroup.alpha = fromAlpha;
+
+            while (timer < _fadeTime) {
+                timer += Time.deltaTime;
+                _canvasGroup.alpha = Mathf.Lerp(_canvasGroup.alpha, targetAlpha, timer / _fadeTime);
+                _canvasGroup.interactable = _canvasGroup.alpha > 0;
+                yield return null;
+            }
+        }
+    }
+}
