d7dc90691aa8b46a45330e9b88beef85f5b206d7
Anastasia <nastya_kalinina2000@mail.ru>
2020-10-12 17:49:49 +0200

    ScoreView correct some scripts

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Events;
+using Game;
+
+namespace UI {
+    public class ScoreView : MonoBehaviour {
+        [SerializeField]
+        private EventListeners _eventListeners;
+        [SerializeField]
+        private ScriptTableIntValue _CurrentScore;
+        private int _currentscore;
+
+        public void Awake() {
+            _eventListeners.OnEventHappened += UpdateBehaviour;
+        }
+        public void UpdateBehaviour() { 
+          if(_CurrentScore.Score > _currentscore) {
+                StartCoroutine(SetScoreCoroutine(_CurrentScore.Score));
+            }
+        }
+        public IEnumerator SetScoreCoroutine(int score) {
+            while (score > _currentscore) {
+                _currentscore ++;
+                Debug.Log(_currentscore);
+                yield return new WaitForSeconds(0.1f);
+            }  
+        }
+    }
+}
