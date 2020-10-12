6274fab82f3e294a93a5e2f2c6a434413f61bd08
Oleg <oleg.kalyugin2001@mail.ru>
2020-10-12 14:40:38 +0200

    Edit1

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UI {
+
+
+    [SerializeField]
+    public class ScoreView:MonoBehaviour {
+
+    }
+}
