28a5a1e27cd68958cab514eae836d8c5a6c36d63
KrupinMA <krupin732@yandex.ru>
2020-10-12 14:47:25 +0200

    HW2

---
+++ Assets/Scripts/Settings/CarSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+namespace Settings {
+    [CreateAssetMenu(fileName = "Car", menuName = "Car")]
+    public class CarSettings : ScriptableObject {
+
+        public int dodgeScore;
+    }
+}
---
+++ Assets/Scripts/Settings/CurrentScore.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Settings;
+
+public class CurrentScore : ScriptableIntValue {
+    public int save_score;
+}
