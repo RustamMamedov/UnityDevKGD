3b28f67e82a567da5dc1681617f8750c774be9be
Kvotua <kvotua.job@gmail.com>
2020-10-12 10:46:16 +0200

    ScoreView

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+using Game;
+
+namespace UI {
+    public class ScoreView: MonoBehaviour {
+
+        [SerializeField] private int _currentScore;
+        [SerializeField] ScriptableIntValue _assetScore;
+        [SerializeField] EventListener _updateEventListener;
+
+        private void Awake() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            if (_assetScore.score > _currentScore) {
+                StartCoroutine(SetScoreCoroutine(_assetScore.score));
+            }
+        }
+
+        private IEnumerator SetScoreCoroutine(int assetScore) {
+            while (_currentScore != assetScore) {
+                if (_currentScore < assetScore) {
+                    _currentScore++;
+                }
+                Debug.Log(_currentScore);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    } 
+}
