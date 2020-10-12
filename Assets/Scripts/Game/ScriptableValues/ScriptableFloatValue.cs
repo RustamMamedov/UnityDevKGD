20381b874a78309e7fbe3a8d46e0e277f3c4b114
TechesDePainture <kirillzelen00@gmail.com>
2020-10-12 20:19:15 +0200

    Scene Loading

---
+++ Assets/Scripts/Game/ScriptableValues/ScriptableFloatValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Scriptable Float Value")]
+public class ScriptableFloatValue : ScriptableObject
+{
+    public float value;
+}
