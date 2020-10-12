4adcaf4ccda5f2ff29a6dfcd940cdbcd538a4ac9
KrupinMA <krupin732@yandex.ru>
2020-10-12 20:22:54 +0200

    UI

---
+++ Assets/Scripts/UI/PreloaderView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+using Events;
+using UnityEngine.UI;
+
+namespace UI {
+    public class PreloaderView : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableFoatValue _sceneLoadingValue;
+
+
+        [SerializeField]
+        private EventListener _updateListener;
+
+        [SerializeField]
+        private Image _prorgressImage;
+
+        private void Awake() {
+            _updateListener.OnEventHappened += UpdateBehavior;
+        }
+        
+        private void nDestroy() {
+            _updateListener.OnEventHappened-= UpdateBehavior;
+        }
+        private void UpdateBehavior() {
+            //    
+            _prorgressImage.fillAmount = _sceneLoadingValue.value;
+        }
+
+    }
+}
