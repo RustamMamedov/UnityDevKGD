486d3651bd4abd80ead033b287332a7b34b8f859
Oleg <oleg.kalyugin2001@mail.ru>
2020-10-12 15:12:48 +0200

    ScoreView

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 
 namespace UI {
 
 
-    [SerializeField]
-    public class ScoreView:MonoBehaviour {
 
+    public class ScoreView:MonoBehaviour {
+        private int _currentScore;
+
+        [SerializeField]
+        private CurrentScore _Scores;
+        
+        public void Update() {
+        UpdateScore();
+            
+        }
+        private IEnumerator UpdateScore() {
+            yield return new WaitForSeconds(0.1f);
+            if(_currentScore > _Scores.save_score) {
+                _currentScore += 1;
+            }
+
+        }
     }
+
 }
