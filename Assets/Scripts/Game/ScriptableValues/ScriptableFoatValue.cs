242f428ae7920aa4bbac884cc3c70851b9b8290e
KrupinMA <krupin732@yandex.ru>
2020-10-12 20:23:32 +0200

    others

---
+++ Assets/Scripts/Game/ScriptableValues/ScriptableFoatValue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    [CreateAssetMenu(fileName ="ScriptableFloatValue")]
+    public class ScriptableFoatValue : ScriptableObject {
+
+        public float value;
+
+    }
+}
