82564056629ee7085284250dfd22bd8d327a2ece
Oleg <oleg.kalyugin2001@mail.ru>
2020-10-12 15:36:54 +0200

    Finaly solution

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -2,30 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
+using Events;
 
 namespace UI {
-
-
-
     public class ScoreView:MonoBehaviour {
+        [SerializeField]
         private int _currentScore;
-
         [SerializeField]
         private CurrentScore _Scores;
-        
-        public void Update() {
-        UpdateScore();
-            
+        [SerializeField]
+        private EventListener _eventListener;
+        private void Awake() {
+            _eventListener.OnEventHappened += UpdateBehaviour;
+        }
+        public void UpdateBehaviour() {
+            if(_currentScore > _Scores.save_score) {
+                StartCoroutine(UpdateScore());
+            }
         }
         private IEnumerator UpdateScore() {
             yield return new WaitForSeconds(0.1f);
             if(_currentScore > _Scores.save_score) {
                 _currentScore += 1;
+                Debug.Log(_currentScore);
             }
-
         }
     }
-
 }
