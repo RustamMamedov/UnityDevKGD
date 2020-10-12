fce1de983f2e1963b116a021e5722017f30d98fe
KrupinMA <krupin732@yandex.ru>
2020-10-12 15:30:29 +0200

    ScoreView

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace UI {
+    public class ScoreView : MonoBehaviour {
+        [SerializeField]
+        private int _currentScore;
+        [SerializeField]
+        private SciptableIntValue _Scores;
+
+
+
+        public void UpdateBehaviour() {
+            if (_currentScore > _Scores.save_score) {
+                StartCoroutine(UpdateScore());
+            }
+            
+            
+        }
+        private IEnumerator UpdateScore() {
+            yield return new WaitForSeconds(0.1f);
+            if (_currentScore > _Scores.save_score) {
+                _currentScore += 1;
+            }
+        }
+    }
+}
