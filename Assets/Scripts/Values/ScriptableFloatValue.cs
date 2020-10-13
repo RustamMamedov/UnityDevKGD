2e5cded538cbafd3d3a160a0ee620c991230328b
Sevastian Kozhunov <sky.seva@gmail.com>
2020-10-13 13:10:25 +0200

    Add SceneLoadingProgress value
    Add scriptable object class for floating-point values.
    
    Add floating-point scriptable value for scene loading progress.

---
+++ Assets/Scripts/Values/ScriptableFloatValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Values {
+
+    [CreateAssetMenu(fileName = "Float Value", menuName = "Values/Float Value")]
+    public class ScriptableFloatValue : ScriptableObject {
+
+        public float value;
+
+    }
+
+}
