89b480d05328c654b95852c9155a3acddc00bd48
Sevastian Kozhunov <sky.seva@gmail.com>
2020-10-13 13:16:28 +0200

    Add Preloading Screen prefab
    Create PreloaderView script which allows to show loading progress via image component.
    
    Create Preloading Screen prefab for loading progress representation.

---
+++ Assets/Scripts/UI/PreloaderView.cs
@@ -0,0 +1,44 @@
+﻿using Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Values;
+
+namespace UI {
+
+    public class PreloaderView : MonoBehaviour {
+
+        // Fields.
+
+        [SerializeField]
+        private ScriptableFloatValue _sceneLoadingProgress;
+
+        [SerializeField]
+        private EventListener _updateListener;
+
+        [SerializeField]
+        private Image _progressImage;
+
+
+        // Life cycle.
+
+        private void Awake() {
+            _updateListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void OnDestroy() {
+            _updateListener.OnEventHappened -= UpdateBehaviour;
+        }
+
+
+        // Event handlers.
+
+        private void UpdateBehaviour() {
+            _progressImage.fillAmount = _sceneLoadingProgress.value;
+        }
+
+
+    }
+
+}
