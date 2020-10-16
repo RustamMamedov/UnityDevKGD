1dbed8b8727d2f4337130c427d3a63fb175b9c2f
Maxim <korunets1901@gmail.com>
2020-10-16 14:49:09 +0300

    Create methods in UIManager
    ShowMenuScreen,ShowGameScreen,ShowLeaderboardScreen,HideAllScreens

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 namespace UI {
 
-    public class UIManager : MonoBehaviour {}
+    public class UIManager : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject _menuScreen;
+
+        [SerializeField]
+        private GameObject _gameScreen;
+
+        [SerializeField]
+        private GameObject _leaderboardScreen;
+
+        public void ShowMenuScreen() {
+            _menuScreen.SetActive(true);
+        }
+
+        public void ShowGameScreen() {
+            _gameScreen.SetActive(true);
+        }
+
+        public void ShowLeaderboardScreen() {
+            _leaderboardScreen.SetActive(true);
+        }
+
+        public void HideAllScreens() {
+            _menuScreen.SetActive(false);
+            _gameScreen.SetActive(false);
+            _leaderboardScreen.SetActive(false);
+        }
+        
+    }
 
 }
