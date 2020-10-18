b0713090ada4f88aa0f75c9bcf30777c14498912
Nikita <Nkanunov@gmail.com>
2020-10-18 19:29:14 +0200

    Scene loading progress view

---
+++ Assets/Scripts/Game/ScriptableFloatValue.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Game {
+    
+    [CreateAssetMenu(fileName = "ScriptableFloatValue")]
+    public class ScriptableFloatValue : ScriptableObject {
+
+        public float value;
+    }
+}
---
+++ Assets/Scripts/UI/PreloaderView.cs
@@ -0,0 +1,29 @@
+﻿using Events;
+using UnityEngine;
+using Game;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class PreloaderView : MonoBehaviour {
+
+        [SerializeField] private ScriptableFloatValue _sceneLoadingValue;
+
+        [SerializeField] private EventListener _updateListener;
+
+        [SerializeField] private Image _progressImage;
+
+        private void Awake() {
+            _updateListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void OnDestroy() {
+            _updateListener.OnEventHappened -= UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            _progressImage.fillAmount = _sceneLoadingValue.value;
+        }
+    }
+
+}
