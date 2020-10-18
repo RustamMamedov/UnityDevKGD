70b05f890c641227ac0921b5e12cd3e5680f8eb3
Alex Silkov <st.jimmy39@mail.ru>
2020-10-18 23:11:57 +0200

    Temp code removed

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -24,12 +24,6 @@ namespace UI {
             DontDestroyOnLoad(gameObject);
         }
 
-        private void Start() {
-
-            _fader.OnFadeIn += OnSceneFadeIn;
-            _fader.FadeIn();
-        }
-
 
         private void OnSceneFadeIn() {
 
