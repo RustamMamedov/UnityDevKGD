cd1ac4238cb3f5858ca7d35382ac133941c8f4b2
Nikita <nikitadedov66@gmail.com>
2020-10-18 15:33:57 +0200

    public methods added

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -56,5 +56,20 @@ namespace UI {
             _fader.FadeIn();
         }
 
+        public void ShowMenuScreen() {
+
+        }
+
+        public void ShowGameScreen() {
+
+        }
+
+        public void ShowLeaderboardsScreen() {
+
+        }
+
+        public void HideAllScreens() {
+
+        }
     }
 }
