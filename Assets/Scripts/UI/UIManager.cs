cadeaf63c9767bdc2b128a9e494821f8cce2103c
vladislav <vladislavnikwork@gmail.com>
2020-10-19 00:02:40 +0200

    Added changes to "UIManager"
    Added methods: "ShowMenuScreen", "ShowGameScreen" , "ShowLeaderboardScreen" and "HideAllScreens"

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -12,6 +12,16 @@ namespace UI {
         [SerializeField] 
         private Fader _fader;
 
+        [Header("UI Screens")]
+        [SerializeField]
+        private GameObject _menuScreen;
+
+        [SerializeField]
+        private GameObject _gameScreen;
+
+        [SerializeField]
+        private GameObject _leaderboardScreen;
+
         private string _currentSceneName = "Gameplay";
 
         private void Awake() {
@@ -50,6 +60,24 @@ namespace UI {
             yield return new WaitForSeconds(3f);
             
             _fader.FadeIn();
+        }
+
+        public void ShowMenuScreen() {
+            _menuScreen.SetActive(true);
+        }
+
+        public void ShowGameScreen() {
+            _gameScreen.SetActive(true);
+        }
+
+        public void ShowLeaderboardScreen() {
+            _leaderboardScreen.SetActive(true);
+        }
+
+        public void HideAllScreens() {
+            _menuScreen.SetActive(false);
+            _gameScreen.SetActive(false);
+            _leaderboardScreen.SetActive(false);
         }
     }
 }
