b6d706d9feac477095a35d8a7413a6c1d074d2b8
Kvotua <kvotua.job@gmail.com>
2020-10-19 12:00:55 +0200

    scene loading progress view

--- Assets/Scripts/UI/Preloader.cs
+++ Assets/Scripts/UI/Preloader.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Game;
 
 
 namespace UI {
 
-
+    
     public class Preloader : MonoBehaviour {
 
+        [SerializeField] private ScriptableFloatValue _sceneLoading;
         private void Start() {
            StartCoroutine(LoadMenuScene());
         }
 
         private IEnumerator LoadMenuScene() {
             var asyncOperation = SceneManager.LoadSceneAsync("Menu");
-            while (!asyncOperation.isDone) {
-                Debug.Log(asyncOperation.progress);
+            asyncOperation.allowSceneActivation = false;
+            while (asyncOperation.progress < 0.9f) {
+               _sceneLoading.value= asyncOperation.progress;
                 yield return null;
             }
+            _sceneLoading.value = 1f;
+
+            yield return new WaitForSeconds(2f);
+            asyncOperation.allowSceneActivation = true;
         }
 
     }
---
+++ Assets/Scripts/UI/PreloaderView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+using Events;
+using UnityEngine.UI;
+
+namespace UI {
+
+
+    public class PreloaderView : MonoBehaviour {
+        [SerializeField] private ScriptableFloatValue _sceneLoadingValue;
+
+        [SerializeField] private EventListener _upadateListener;
+
+        [SerializeField] private Image _progressImage;
+
+        private void Awake() {
+            _upadateListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void OnDestroy() {
+            _upadateListener.OnEventHappened -= UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            _progressImage.fillAmount = _sceneLoadingValue.value;
+        }
+    }
+}
