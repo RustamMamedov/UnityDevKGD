196b1f2fafa714e0159de63a1b60571cfc3f8165
ErvinRudakov <ervin_rudakov@mail.ru>
2020-10-19 18:17:40 +0200

    Update UIManager.cs

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -52,22 +52,22 @@ namespace UI {
         }
         public void ShowMenuScreen()
         {
-
+            Debug.Log("ShowMenuScreen");
         }
 
         public void ShowGameScreen()
         {
-
+            Debug.Log("ShowGameScreen");
         }
 
         public void ShowLeaderboardsScreen()
         {
-
+            Debug.Log("ShowLeaderboardsScreen");
         }
 
         public void HideAllScreens()
         {
-
+            Debug.Log("HideAllScreens");
         }
     }
 }
