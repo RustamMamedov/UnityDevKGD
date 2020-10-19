6f7e2b91d377a010a7c487706794a37f79107a02
ErvinRudakov <ervin_rudakov@mail.ru>
2020-10-19 18:28:30 +0200

    some scripts added

---
+++ Assets/Scripts/Game/ScriptableValues/ScriptableFloatValue.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "ScriptableFloatValue")]
+public class ScriptableFloatValue : ScriptableObject
+{
+    public float value;
+}
---
+++ Assets/Scripts/UI/PreloaderView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+using Events;
+using UnityEngine.UI;
+
+
+namespace UI {
+
+    public class PreloaderView : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableFloatValue _sceneLoadingValue;
+
+        [SerializeField]
+        EventListener _updateListener;
+
+        [SerializeField]
+        private Image _progressImage;
+
+
+        private void Awake() {
+            _updateListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void OnDestroy() {
+            _updateListener.OnEventHappened -= UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            _progressImage.fillAmount = _sceneLoadingValue.value;
+        }
+
+
+    }
+
+}
