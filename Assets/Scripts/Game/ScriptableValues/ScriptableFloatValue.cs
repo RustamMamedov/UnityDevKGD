205a257401baf4ff1b124f0b188c604d5da2de43
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-19 14:39:09 +0200

    Scene loading progress view

---
+++ Assets/Scripts/Game/ScriptableValues/ScriptableFloatValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "ScriptableFloatValue")]
+    public class ScriptableFloatValue : ScriptableObject {
+
+        public float value;
+    }
+}
+
