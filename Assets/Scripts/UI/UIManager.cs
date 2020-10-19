3c5dc5b7fdaf5d2a34f7f8f73621ce694b1fbfd0
KrupinMA <krupin732@yandex.ru>
2020-10-19 15:03:11 +0200

    UIManager HW

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -51,5 +51,26 @@ namespace UI {
             _fader.FadeIn();
         }
 
+        public void ShowMenuScreen() {
+            GameObject.Find("MenuScreen").SetActive(true);
+        
+        }
+        public void ShowGameScreen() {
+            GameObject.Find("GameScreen").SetActive(true);
+
+        }
+        public void ShowLeaderBoardScreen() {
+            GameObject.Find("LeaderBoard").SetActive(true);
+
+        }
+        public void HideAllScreens() {
+            GameObject.Find("LeaderBoard").SetActive(false);
+            GameObject.Find("GameScreen").SetActive(false);
+            GameObject.Find("MenuScreen").SetActive(false);
+
+        }
+
+
+
     }
 }
