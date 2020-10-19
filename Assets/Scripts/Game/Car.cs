68ea288768c99e0c9adfac1310d335ddc44e5d8d
Alexandr <navacar@mail.ru>
2020-10-19 20:35:42 +0200

    Car movement
    adding car move methods

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -3,15 +3,49 @@ using UnityEngine;
 
 namespace Game {
     public class Car : MonoBehaviour {
+
+        [SerializeField]
+        protected CarSettings _carSettings;
+
         [SerializeField]
         private EventListener _updateEventListener;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        private float _currentSpeed;
+
+        protected virtual void OnEnable() {
+            SubcribeToEvents();
         }
 
-        private void Move() {
-            Debug.Log("Move");
+        protected virtual void OnDisable() {
+            UnsubcribeToEvents();
+        }
+
+        private void SubcribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehavior;
+            _carCollisionEventListener.OnEventHappened += UpdateBehavior;
+        }
+
+        private void UnsubcribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehavior;
+            _carCollisionEventListener.OnEventHappened -= UpdateBehavior;
+        }
+
+        private void OnCarCollision() {
+            UnsubcribeToEvents();
+        }
+
+        private void UpdateBehavior() {
+            Move();
+        }
+
+        protected virtual void Move() {
+            if(_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.maxSpeed;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
         }
     }
 }
