34ebf101da3e808691b4cf16df98c34fb45f999e
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-19 15:48:26 +0200

    Text label added to score view

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -3,16 +3,22 @@ using Game;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI {
     public class ScoreView : MonoBehaviour {
 
         [SerializeField]
+        private float _scoreCountDelay;
+        [SerializeField]
         private ScriptableIntValue _currentScoreAsset;
 
         [SerializeField]
         private EventListener _eventListener;
-        
+
+        [SerializeField]
+        private Text _scoreLabel;
+
         private int _currentScore = 0;
         private bool _isScoreChanging = false;
 
@@ -30,9 +36,9 @@ namespace UI {
         private IEnumerator SetScoreCoroutine(int targetScore) {
             while (_currentScore != targetScore) {
                 _currentScore++;
-                Debug.Log(_currentScore);
+                _scoreLabel.text = $"{_currentScore}";
 
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(_scoreCountDelay);
             }
             _isScoreChanging = false;
         }
