ccd80f5bf8ab78e7aec27cdd061e21e7173f3101
Anastasia <nastya_kalinina2000@mail.ru>
2020-10-19 17:09:45 +0200

    preloader.cs

--- Assets/Scripts/UI/Preloader.cs
+++ Assets/Scripts/UI/Preloader.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using Game;
 namespace UI {
 
     public class Preloader : MonoBehaviour {
+        [SerializeField]
+        private ScriptableFloatValue _screenFloatValue;
 
         private void Start() {
             StartCoroutine(LoadMenuScene());
@@ -12,10 +14,14 @@ namespace UI {
 
         private IEnumerator LoadMenuScene() {
             var asyncOperation = SceneManager.LoadSceneAsync("Menu");
-            while (!asyncOperation.isDone) {
-                Debug.Log(asyncOperation.progress);
+            asyncOperation.allowSceneActivation = false;
+            while (asyncOperation.progress < 0.9f) {
+                _screenFloatValue.value = asyncOperation.progress;
                 yield return null;
             }
+            _screenFloatValue.value = 1f;
+            yield return new WaitForSeconds(2f);
+            asyncOperation.allowSceneActivation = true;
         }
     }
 }
---
+++ Assets/Scripts/UI/PreloaderView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Events;
+using Game;
+namespace UI {
+    public class PreloaderView : MonoBehaviour {
+        [SerializeField]
+        private Image _loadImage;
+        [SerializeField]
+        private EventListeners _updateList;
+        [SerializeField]
+        private ScriptableFloatValue _loadValue;
+        private void Awake() {
+            _updateList.OnEventHappened += UpdateBehavour;
+        }
+        private void OnDestroy() {
+            _updateList.OnEventHappened -= UpdateBehavour;
+        }
+        private void UpdateBehavour() {
+            _loadImage.fillAmount = _loadValue.value;
+        }
+
+    }
+}
