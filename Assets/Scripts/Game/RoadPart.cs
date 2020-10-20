3424e2a5ed176da5a590387f85e32011a2dea5ba
Denis <denis.zhuravkov@gmail.com>
2020-10-20 18:40:53 +0200

    RoadPart logic
    Changes: 
    - Added RIgidbody to Player's car prefab to make it call OnTriggerEnter on RoadPart it crossed
    - "RoadCollision" event created
    - "RoadPart" script created 
    - Added RoadPart script to Road prefab with RoadCollision EventDispatcher, so the script dispatch event when road is being crossed by car

---
+++ Assets/Scripts/Game/RoadPart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Events;
+using UnityEngine;
+
+namespace Game {
+    
+    public class RoadPart : MonoBehaviour {
+
+        [SerializeField] 
+        private EventDispatcher _roadTriggerEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                Debug.Log("Road collision");
+                _roadTriggerEventDispatcher.Dispatch();
+            }
+        }
+    }
+}
+
