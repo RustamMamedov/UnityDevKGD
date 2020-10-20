7089956fc601b93ef8c439e0d573562942cea46e
Denis <denis.zhuravkov@gmail.com>
2020-10-20 19:03:35 +0200

    "Environment" script created
    This script handles logic of creating environment around player, especially roads for now
    From start of the game, it generates some roads based on prefab, then it listens to RoadCollision event. When this event happen, this script move first piece of road to the top road.

---
+++ Assets/Scripts/Game/Environment.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+namespace Game {
+    
+    public class Environment : MonoBehaviour {
+        
+#region Fields
+[Header("Settings")]
+        [SerializeField] 
+        private EventListener _roadCollisionEventListener;
+
+        [SerializeField] 
+        private GameObject _roadPrefab;
+
+        [SerializeField] 
+        private int _initialRoadNumber = 10;
+
+        [SerializeField] 
+        private int _roadLength = 12;
+
+        private List<Transform> _roadTransforms;
+        
+#endregion
+
+#region LifeCycle
+        private void Start() {
+            GenerateRoad();
+        }
+
+        private void OnEnable() {
+            _roadCollisionEventListener.OnEventHappened += HandleRoadCollision;
+        }
+
+        private void OnDisable() {
+            _roadCollisionEventListener.OnEventHappened -= HandleRoadCollision;
+        }
+        
+#endregion
+
+#region Methods
+        private void GenerateRoad() {
+            _roadTransforms = new List<Transform>();
+            for (int i = 0; i < _initialRoadNumber; ++i) {
+                var pos = new Vector3(0f, 0f, (i - 1) * _roadLength);
+                var road = Instantiate(_roadPrefab, pos, Quaternion.identity);
+                _roadTransforms.Add(road.transform);
+            }
+            
+            _roadPrefab.gameObject.SetActive(false);
+        }
+        
+        private void HandleRoadCollision() {
+            MoveFirstRoadPart();
+        }
+
+        private void MoveFirstRoadPart() {
+            var firstRoadPart = _roadTransforms[0];
+            _roadTransforms.RemoveAt(0);
+            firstRoadPart.position = 
+                new Vector3(0f, 0f, _roadTransforms[_roadTransforms.Count - 1].position.z + _roadLength);
+            _roadTransforms.Add(firstRoadPart);
+        }
+        
+#endregion
+        
+    }
+}
+
+
