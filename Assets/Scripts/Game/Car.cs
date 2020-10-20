bebc3667c2e68603550c72b21a3c40791e52479b
Denis <denis.zhuravkov@gmail.com>
2020-10-20 18:26:56 +0200

    Car logic implemented
    - From now Player's car is moving towards
    - PlayerCar prefab setted up for work on Gameplay scene

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -1,19 +1,63 @@
-﻿using Events;
+﻿using System;
+using Events;
 using UnityEngine;
 
 namespace Game {
 
     public class Car : MonoBehaviour {
-
+        
+#region Fields
+        [SerializeField] 
+        protected CarSettings _carSettings;
+        
         [SerializeField]
         private EventListener _updateEventListener;
+        
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
+        protected float _currentSpeed;
+        
+#endregion
+        
+        
+#region LifeCycle
+        protected void OnEnable() {
+            SubscribeToEvents();
         }
 
-        private void Move() {
-            Debug.Log("Move");
+        protected void OnDisable() {
+            UnsubscribeToEvents();
         }
+        
+#endregion
+        
+#region Methods
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+        
+        private void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+        
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+        
+        private void UpdateBehaviour() {
+            Move();
+        }
+
+        protected virtual void Move() {
+            if (_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            transform.Translate(transform.forward * (_currentSpeed * Time.deltaTime), Space.World);
+        }
+        
+#endregion
     }
 }
