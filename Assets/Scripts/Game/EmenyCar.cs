f6f3953aae2238a4344771240c4d5ca16f8fe9bf
SaraJess <sarycheva.00@mail.ru>
2020-10-23 01:40:15 +0200

    Реализовать логику OnTriggerEnter

--- Assets/Scripts/Game/EmenyCar.cs
+++ Assets/Scripts/Game/EmenyCar.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Events;
 
 namespace Game {
     public class EmenyCar : Car {
+        [SerializeField]
+        private EventDispatcher _carCollisionDispatcher;
 
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carCollisionDispatcher.Dispatch();
+            }
+        }
     }
 }
