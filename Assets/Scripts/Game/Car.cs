9fcd082a436a45dd234f64acaac2fea76281e28a
SaraJess <sarycheva.00@mail.ru>
2020-10-22 23:25:06 +0200

    Car logic and correct position

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -6,15 +6,60 @@ namespace Game {
 
     public class Car : MonoBehaviour {
 
+#region 1
+        [SerializeField]
+        protected CarSettings _carSettings;
+
         [SerializeField]
         private EventListener _updateEventListener;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        protected float _currentSpeed;
+
+#endregion 1
+
+#region 3
+        protected virtual void OnEnable() {
+            SubscribeToEvents();
         }
 
-        private void Move() {
-            Debug.Log("Move");
+        protected virtual void OnDisable() {
+            UnsubscribeToEvents();
         }
+
+#endregion 3
+
+#region 2
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+
+        private void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+        private void UpdateBehaviour() {
+            Move();
+        }
+
+#endregion 2
+
+#region 4
+
+        protected virtual void Move() {
+            if (_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+
+#endregion 4
     }
 }
