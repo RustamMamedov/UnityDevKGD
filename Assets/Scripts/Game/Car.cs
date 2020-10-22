1deca523e3452bc6a8e47a49ca4970d4117621a2
TechesDePainture <kirillzelen00@gmail.com>
2020-10-22 21:19:16 +0200

    Car.cs corrected, CarCollision created

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -1,19 +1,63 @@
 ﻿using Events;
+using UnityEditor.MemoryProfiler;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Game {
 
     public class Car : MonoBehaviour {
 
+        #region data
+        [SerializeField]
+        private CarSettings _carSettings;
+
         [SerializeField]
         private EventListener _updateEventListener;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        protected float _currentSpeed;
+
+        #endregion data
+
+        #region EventMethods
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
         }
 
-        private void Move() {
-            Debug.Log("Move");
+        protected virtual void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
         }
+
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+
+        private void UpdateBehaviour() {
+            Move();
+        }
+        #endregion EventMethods
+
+        #region Eneble\Disable
+        protected virtual void OnEnable() {
+            SubscribeToEvents();
+        }
+        protected virtual void OnDisable() {
+            UnsubscribeToEvents();
+        }
+        #endregion Eneble\Disable
+
+        #region Move
+        protected virtual void Move() {
+            if (_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            Debug.Log(1);
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+        #endregion Move
     }
 }
