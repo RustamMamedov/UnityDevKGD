c19d82152b1724ea4e3975a1a5d185f48bed90bc
Ilya <elya5911@mail.ru>
2020-10-23 00:24:10 +0200

    Environment_generate_road_parts

---
+++ Assets/Scripts/Game/Environment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+    public class Environment : MonoBehaviour {
+        [SerializeField]
+        private EventListeners _roadCollisionsEventListeners;
+
+        [SerializeField]
+        private GameObject _roadPrefab;
+
+        [SerializeField]
+        private int _initialRoadNumber;
+
+        [SerializeField]
+        private int _roadLength = 12;
+
+        private List<Transform> _roadTransforms;
+    }
+}
