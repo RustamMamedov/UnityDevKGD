2609d2d6882e0bf33824cc5b95626313f6b1c259
TechesDePainture <kirillzelen00@gmail.com>
2020-10-22 21:50:42 +0200

    Enviroment.cs created, RoadPart created, both added.

---
+++ Assets/Scripts/Game/Environment.cs
@@ -0,0 +1,18 @@
+﻿using Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Game {
+    public class Environment : MonoBehaviour {
+        [SerializeField]
+        private EventListener _roadCollisionEventListener;
+
+        [SerializeField]
+        private GameObject _roadPrefab;
+        [SerializeField]
+        private int _initialRoadNumber = 10;
+        [SerializeField]
+        private int _roadLength = 12;
+        private List<Transform> _roadTransforms;
+    }
+}
