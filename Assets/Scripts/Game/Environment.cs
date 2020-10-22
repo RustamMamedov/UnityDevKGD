38353005c3c14f3c576f0441f92107ca5e324bc9
SaraJess <sarycheva.00@mail.ru>
2020-10-22 23:53:32 +0200

    Environment: variables

---
+++ Assets/Scripts/Game/Environment.cs
@@ -0,0 +1,22 @@
+﻿using Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public class Environment : MonoBehaviour {
+        [SerializeField]
+        private EventListener _roadCollisionEventListener;
+
+        [SerializeField]
+        private GameObject _roadPrefab;
+
+        [SerializeField]
+        private int _initialRoadNumber = 10;
+
+        [SerializeField]
+        private int _roadLenght = 12;
+
+        private List<Transform> _roadTransforms;
+    }
+}
