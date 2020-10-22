0e2d15b1d5d4255d0532aebf03628673a9e18d71
TechesDePainture <kirillzelen00@gmail.com>
2020-10-23 01:39:25 +0200

    Logic emenyCar

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using Events;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game {
     public class EnemyCar : Car {
+
+        [SerializeField]
+        private EventDispatcher _carCollisionDispatcher;
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")){
+                _carCollisionDispatcher.Dispatch();
+            }
+        }
 
     }
 }
