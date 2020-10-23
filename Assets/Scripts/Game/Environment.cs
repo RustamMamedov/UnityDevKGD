d00b2e3283a728f040dc7b7719826fb719e45d09
Ilya <elya5911@mail.ru>
2020-10-23 03:14:58 +0200

    Moved_roadPart

--- Assets/Scripts/Game/Environment.cs
+++ Assets/Scripts/Game/Environment.cs
@@ -23,6 +23,14 @@ namespace Game {
             GenerateRoad();
         }
 
+        private void OnEnable() {
+            _roadCollisionsEventListeners.OnEventHappened += HandleRoadCollision;
+        }
+
+        private void OnDisable() {
+            _roadCollisionsEventListeners.OnEventHappened -= HandleRoadCollision;
+        }
+
         private void GenerateRoad() {
             _roadTransforms = new List<Transform>();
             for (int i=0;i<_initialRoadNumber+1;i++) {
@@ -31,6 +39,17 @@ namespace Game {
                 _roadTransforms.Add(road.transform);
             }
             _roadPrefab.SetActive(false);
+        }
+
+        private void HandleRoadCollision() {
+            MoveFirsRoadPart();
+        }
+
+        private void MoveFirsRoadPart() {
+            var furstRoadPart = _roadTransforms[0];
+            _roadTransforms.RemoveAt(0);
+            furstRoadPart.position = new Vector3(0f, 0f, _roadTransforms[_roadTransforms.Count - 1].position.z + _roadLength);
+            _roadTransforms.Add(furstRoadPart);
         }
     }
 }
