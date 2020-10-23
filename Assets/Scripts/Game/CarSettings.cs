6766f818ff01828e5340c2a6499f8a38f719619a
Vlad <vlad.shiltsin@gmail.com>
2020-10-23 16:47:02 +0200

    maxSpeed and acceleration added into CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -2,8 +2,10 @@
 
 namespace Game {
 
-    [CreateAssetMenu (fileName = "CarSettings", menuName = "Car/CarSettings")]
+    [CreateAssetMenu(fileName = "CarSettings", menuName = "Car/CarSettings")]
     public class CarSettings : ScriptableObject {
         public int dodgeScore;
+        public float maxSpeed;
+        public float acceleration;
     }
 }
