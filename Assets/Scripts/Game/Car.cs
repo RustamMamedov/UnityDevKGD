d1510c659ccaadc52affe20599b42d27eb737fef
Andrey <DrEMartstud@gmail.com>
2020-10-24 21:31:06 +0200

    Car now moves and can collide

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -9,12 +9,41 @@ namespace Game {
     public class Car : MonoBehaviour {
 
         [SerializeField] 
+        protected CarSettings _carSettings;
+        
+        [SerializeField] 
         private EventListener _updateEventListener;
+
+        [SerializeField] 
+        private EventListener _carCollisionEventListener;
+
+        protected float _currentSpeed;
         private void Awake() {
             _updateEventListener.OnEventHappened += Move;
         }
-        private void Move() {
-            Debug.Log("Move");
+        protected virtual void Move() {
+            if (_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+
+        private void SubscribeToEvent() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+        
+        private void UnSubscribeToEvent() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            UnSubscribeToEvent();
+        }
+
+        private void UpdateBehaviour() {
+            Move();
         }
     }
 }
