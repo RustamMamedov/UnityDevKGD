665250598a8b9e0d93dd459b2792362879f1a299
Evgenii <jekaweek1@gmail.com>
2020-10-24 14:49:23 +0200

    Car logic

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -6,18 +6,48 @@ namespace Game {
     public class Car : MonoBehaviour {
 
         [SerializeField]
+        protected CarSettings _carSettings;
+
+        [SerializeField]
         private EventListener _updateEventListener;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
-            _updateEventListener.OnEventHappened += Die;
-        }
-        private void Move() {
-            Debug.Log("Move");
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        protected float _currentSpeed;
+
+        protected virtual void OnEnable() {
+            SubscribeToEvents();
         }
 
-        private void Die() {
-
+        protected virtual void OnDisable() {
+            UnsubscribeToEvents();
         }
+
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+
+        protected virtual void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+
+        private void UpdateBehaviour() {
+            Move();
+        }
+
+        protected virtual void Move() {
+            if(_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+
     }
 }
