f984c85657bf189f3ee5fcafb365aea9d951aa10
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-26 01:05:22 +0200

    update EnemyCar

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
+using Events;
 
 namespace Game {
     public class EnemyCar : Car {
+
+        [SerializeField]
+        private EventDispatcher _carCollisionEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carCollisionEventDispatcher.Dispatch();
+                Debug.Log("Car Collision!");
+            }
+        }
 
     }
 }
