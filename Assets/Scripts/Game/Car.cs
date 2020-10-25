df117ca0ce463a3511e0e691b5942d54ea98a81d
Anastasia <nastya_kalinina2000@mail.ru>
2020-10-25 20:54:24 +0200

    create PlayerCar and add to Gameplay

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using Events;
 
+
 namespace Game {
     public class Car : MonoBehaviour {
+#region 1
+        [SerializeField]
+        protected CarsSettings _carSetting;
 
         [SerializeField]
         private EventListeners _updateEventListeners;
 
-        private void Awake() {
-            _updateEventListeners.OnEventHappened += Move;
-        }
+        [SerializeField]
+        private EventListeners _carCollisionEventListener;
 
-        private void Move() {
-            Debug.Log("Move");
+        protected float _currentSpeed;
+#endregion 1
+
+#region 3
+        protected virtual void OnEnable() {
+            SubscribeToEvent();
         }
+        protected virtual void OnDisable() {
+            UnsubscribeToEvent();
+        }
+#endregion 3
+
+#region 2
+        private void SubscribeToEvent() {
+            _updateEventListeners.OnEventHappened += UpdateBehavior;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+        protected virtual void UnsubscribeToEvent() {
+            _updateEventListeners.OnEventHappened -= UpdateBehavior;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+        private void OnCarCollision() {
+            UnsubscribeToEvent();
+        }
+        private void UpdateBehavior() {
+            Move();
+        }
+#endregion 2
+
+#region 4
+        protected virtual void Move() {
+            if(_currentSpeed < _carSetting.maxSpeed) {
+                _currentSpeed += _carSetting.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+#endregion 4
     }
 }
