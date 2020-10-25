4c6688b504d6efb8be369d02f145d37bc2bde956
Evgenii <jekaweek1@gmail.com>
2020-10-25 10:46:19 +0200

    edit EnemyCar script and edit prefabs enemy car

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Events;
 
 namespace Game {
     public class EnemyCar : Car {
-        
+
+        [SerializeField]
+        private EventDispatcher _carCollisionEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carCollisionEventDispatcher.Dispatch();
+                Debug.Log("Car Collision!");
+            }
+        }
+
     }
 
 }
