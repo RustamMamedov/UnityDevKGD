e9b65251eeab7d1add0b83a839972a58d3f160ed
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-26 00:14:01 +0200

    add logic Car.cs

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -3,20 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using Events;
 
+
 namespace Game {
 
 
     public class Car : MonoBehaviour {
-	[SerializeField]
-	private EventListener _updateEventListener;
 
-	private void Awake() {
-	    _updateEventListener.OnEventHappend += Move;
-	}
+        #region  1
+        [SerializeField]
+        protected CarSettings _carSettings;
 
-	private void Move() {
-	    Debug.Log("Move");
-	    
-	}
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        protected float _currentSpeed;
+
+        #endregion  1
+
+        #region  3
+        protected virtual void OnEnable() {
+            SubscribeToEvents();
+        }
+
+        protected virtual void OnDisable() {
+            UnsubscribeToEvents();
+        }
+
+        #endregion  3
+
+        #region  2
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappend += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappend += OnCarCollision;
+        }
+
+        protected virtual void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappend -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappend -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+
+        private void UpdateBehaviour() {
+            Move();
+        }
+        #endregion  2
+
+        #region  4
+        protected virtual void Move() {
+            if (_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+
+        #endregion  4
     }
 }
