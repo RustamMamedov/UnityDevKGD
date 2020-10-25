6b5bd0628d2424b8c7a84ecaf33e6b4e575f58ee
Alex Osi <noemail>
2020-10-26 00:08:14 +0200

    Car script edited

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -1,19 +1,60 @@
 ﻿using Events;
 using UnityEngine;
+using Game;
+using UnityEngine.Scripting.APIUpdating;
 
 namespace Game {
 
     public class Car : MonoBehaviour {
+        #region 1
+        [SerializeField]
+        protected CarSettings _carSettings;
 
         [SerializeField]
         private EventListener _updateEventListener;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
+        [SerializeField]
+        private EventListener _carCollisionEvenetListener;
+
+        protected float _currentSpeed;
+        #endregion 1
+
+        #region 2
+        private void SubcribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEvenetListener.OnEventHappened += OnCarCollision;
         }
 
-        private void Move() {
-            Debug.Log("Move");
+        protected virtual void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEvenetListener.OnEventHappened -= OnCarCollision;
         }
+
+        private void OnCarCollision() {
+           UnsubscribeToEvents();
+        }
+
+        private void UpdateBehaviour() {
+            Move();
+        }
+        #endregion 2
+
+        #region 3
+        private void OnEnable() {
+            SubcribeToEvents();
+        }
+        private void OnDisable() {
+            UnsubscribeToEvents();
+        }
+        #endregion 3
+
+        #region 4
+        protected virtual void Move() {
+            if (_currentSpeed <= _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.maxSpeed;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+        #endregion 4
     }
 }
