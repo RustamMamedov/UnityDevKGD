999cf422834518773e1e8a141defdfc285078540
Alexandr <navacar@mail.ru>
2020-10-25 13:47:26 +0200

    EnemyCar loggic
    Adding logic for CarCollision and updating Cars prefabs

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Game;
+using Events;
 using UnityEngine;
 
 namespace Game {
 
 	public class EnemyCar : Car {
+        [SerializeField] 
+        private EventDispatcher _carTriggerEventDispatcher;
 
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carTriggerEventDispatcher.Dispatch();
+                Debug.Log("Collision with " + transform.name);
+            }
+        }
 		
 
 	}
