abe9eab620a512ae0539e9fb7c0aebd0070d3163
ErvinRudakov <ervin_rudakov@mail.ru>
2020-10-25 15:22:26 +0200

    OnTriggerEnter added to EnemyCar

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Events;
 
 
 namespace Game {
 
     public class EnemyCar : Car {
-        
-    }
+        [SerializeField]
+        private EventDispatcher _carTriggerEventDispatcher;
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("CarCollision");
+                _carTriggerEventDispatcher.Dispatch();
+            }
+        }
+    }
 }
