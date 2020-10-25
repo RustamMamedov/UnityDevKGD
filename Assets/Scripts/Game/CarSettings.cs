78a776d2b2d533ef95dff4b1b47cee675c73cb92
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-26 00:12:35 +0200

    Update CarSettings add new variable

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -6,9 +6,10 @@ namespace Game {
 
     [CreateAssetMenu(fileName = "Car Assets", menuName = "Car/Settings")]
     public class CarSettings : ScriptableObject {
+
         [SerializeField]
         private int _dodgeScore;
-
-        
+        public float maxSpeed;
+        public float acceleration;
     }
 }
