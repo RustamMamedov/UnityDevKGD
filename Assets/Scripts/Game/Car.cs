727ff9bf3090e29a771b2153133b13a486598d78
BarbadosP <var0720@gmail.com>
2020-10-25 23:21:35 +0200

    Create PlayerCar and add to Gameplay

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -1,19 +1,57 @@
-﻿using Events;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Events;
+
 
 namespace Game {
-
     public class Car : MonoBehaviour {
+        #region 1
+        [SerializeField]
+        protected CarSettings _carSetting;
 
         [SerializeField]
-        private EventListener _updateEventListener;
+        private EventListener _updateEventListeners;
 
-        private void Awake() {
-            _updateEventListener.OnEventHappened += Move;
-        }
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
 
-        private void Move() {
-            Debug.Log("Move");
+        protected float _currentSpeed;
+        #endregion 1
+
+        #region 3
+        protected virtual void OnEnable() {
+            SubscribeToEvent();
         }
+        protected virtual void OnDisable() {
+            UnsubscribeToEvent();
+        }
+        #endregion 3
+
+        #region 2
+        private void SubscribeToEvent() {
+            _updateEventListeners.OnEventHappened += UpdateBehavior;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+        protected virtual void UnsubscribeToEvent() {
+            _updateEventListeners.OnEventHappened -= UpdateBehavior;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+        private void OnCarCollision() {
+            UnsubscribeToEvent();
+        }
+        private void UpdateBehavior() {
+            Move();
+        }
+        #endregion 2
+
+        #region 4
+        protected virtual void Move() {
+            if (_currentSpeed < _carSetting.maxSpeed) {
+                _currentSpeed += _carSetting.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+        #endregion 4
     }
 }
