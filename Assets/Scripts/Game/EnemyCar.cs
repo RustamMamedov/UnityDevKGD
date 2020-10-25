8c0ab6ea57e756bcd1fc7ca0bda837165592fe03
Valeriy <mr.robariii@gmail.com>
2020-10-25 10:09:25 +0200

    Updated EnemyCar

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 namespace Game {
     public class EnemyCar : Car {
-        // Start is called before the first frame update
-        void Start() {
-
-        }
-
-        // Update is called once per frame
-        void Update() {
-
-        }
+        
     }
 
 }
