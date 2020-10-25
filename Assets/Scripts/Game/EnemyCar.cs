3876e8dcca70eae1e8d8ccc97d70d5daafc0c189
BarbadosP <var0720@gmail.com>
2020-10-26 01:13:14 +0200

    Added Method OnTriggerEnter

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
+using Events;
 
 namespace Game{
     public class EnemyCar : Car
     {
-        
+        [SerializeField]
+        private EventDispatcher _enemyCarsTriggerEventDispatch;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _enemyCarsTriggerEventDispatch.Dispatch();
+            }
+        }
     }
 }
