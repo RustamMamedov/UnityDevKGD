2608349b20dcdcc5d6f5f5fa39feed540d2af60c
Nikita <nikitadedov66@gmail.com>
2020-10-26 00:06:32 +0200

    OnTriggerEnter realized

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -5,6 +5,15 @@ namespace Game
 {
     public class EnemyCar : Car
     {
-        
+        [SerializeField]
+        private EventDispatcher _carTriggerEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                Debug.Log("CarCollision");
+                _carTriggerEventDispatcher.Dispatch();
+            }
+        }
+
     }
 }
