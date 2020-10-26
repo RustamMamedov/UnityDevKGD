695395e09ecbdb0b872caae594b205cf91671eee
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-26 16:35:03 +0200

    Car logic added

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 namespace Game {
 
     public class Car : MonoBehaviour {
+        [SerializeField]
+        protected CarSettings _carSettings;
 
         [SerializeField]
         private EventListener _updateEventListener;
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+
+        protected float _currentSpeed;
 
         private void Awake() {
             _updateEventListener.OnEventHappened += Move;
         }
 
-        private void Move () {
-            Debug.Log("Move");
+
+
+
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
         }
 
-    }
+        private void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
 
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+
+        private void UpdateBehaviour() {
+            Move();
+        }
+
+        protected virtual void Move() {
+            if (_currentSpeed < _carSettings.maxSpeed) {
+                _currentSpeed += _carSettings.acceleration;
+            }
+            transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+        }
+    }
 }
