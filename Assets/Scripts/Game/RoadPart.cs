5c2ae2c7eb583547cc56f440646598cfb4bc0a97
Nikita <Nkanunov@gmail.com>
2020-10-26 15:26:46 +0200

    Added RoadPart script

---
+++ Assets/Scripts/Game/RoadPart.cs
@@ -0,0 +1,16 @@
+using Events;
+using UnityEngine;
+
+namespace Game {
+    
+    public class RoadPart : MonoBehaviour {
+        [SerializeField] private EventDispatcher _roadTriggerEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _roadTriggerEventDispatcher.Dispatch();
+            }
+        }
+
+    }
+}
