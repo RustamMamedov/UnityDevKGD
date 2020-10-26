d4729de710192b12921422f9cdb4b2fed30b734f
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-26 17:27:32 +0200

    Created EnemyCar script

---
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -0,0 +1,18 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Game {
+    public class EnemyCar : Car {
+
+        [SerializeField]
+        private EventDispatcher _collisionTrigger;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _collisionTrigger.Dispatch();
+                Debug.Log("trigger");
+            }
+        }
+    }
+}
+
