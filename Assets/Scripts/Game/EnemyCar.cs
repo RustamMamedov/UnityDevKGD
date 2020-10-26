63cc4ec74d74611f41f180a5553348ef84ff1edf
KrupinMA <krupin732@yandex.ru>
2020-10-26 15:07:50 +0200

    Scripts

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Game;
-
+using TMPro;
+using Events;
 
 namespace Game {
 
     public class EnemyCar:Car {
+        [SerializeField]
+        private EventDispatcher eventDispatcher;
+        private void OnTriggerEnter(Collider other) {
+            Debug.Log("CarCollision");
+            
 
+
+
+        }
     }
 }
