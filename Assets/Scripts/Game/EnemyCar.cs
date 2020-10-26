4809b8d98204d0c77678a687a066e4114e8e918b
Denis <denis.zhuravkov@gmail.com>
2020-10-26 02:04:57 +0200

    Added OnTriggerEnter method to "EnemyCar"
    It detects collision with player car

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using Events;
 using UnityEngine;
 
 namespace Game {
@@ -6,6 +6,14 @@ namespace Game {
     public class EnemyCar : Car
     {
         
+        [SerializeField]
+        private EventDispatcher _playerCarCollisionDispatcher;
+        private void OnTriggerEnter(Collider other) {
+            if(other.CompareTag("Player")) {
+                _playerCarCollisionDispatcher.Dispatch();
+                Debug.Log("EnemyCar collision!");
+            }
+        }
     }
 }
 
