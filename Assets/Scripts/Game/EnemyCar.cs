57518ee430fe6c3629826d19a4f4d9d9be53ce10
Rfnt <kvotua.job@gmail.com>
2020-10-26 14:15:32 +0200

    Enemy Car created

---
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public class EnemyCar : Car {
+        [SerializeField]
+        private GameObject _familyCarPrefab;
+        [SerializeField]
+        private GameObject _suvPrefab;
+        [SerializeField]
+        private GameObject _truckPrefab;
+
+    }
+}
