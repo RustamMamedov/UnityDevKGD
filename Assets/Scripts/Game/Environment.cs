870427289a4ae15b67848ce096a2fd695a30ca95
Rfnt <kvotua.job@gmail.com>
2020-10-26 12:44:29 +0200

    Environments varlables

---
+++ Assets/Scripts/Game/Environment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+
+    public class Environment : MonoBehaviour {
+        [SerializeField]
+        private EventListener _roadCollisionEventListener;
+
+        [SerializeField]
+        private GameObject _roadPrefab;
+
+        [SerializeField]
+        private int _intialRoadNumber = 10;
+
+        [SerializeField]
+        private int _roadLenght = 12;
+
+        private List<Transform> _roadTransforms;
+    }
+}
