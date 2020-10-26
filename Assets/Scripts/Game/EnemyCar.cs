9d71ddaef45f2b1dc8f13b7bfa999ee981f5d219
Potapenko_Ivan <van.van123@list.ru>
2020-10-26 02:18:45 +0200

    in script EnemyCar added OnTriggerEnter + fixed prefabs

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 using Game;
 using Events;
 
-public class EnemyCar : Car
-{
+namespace Game {
+    public class EnemyCar : Car {
+        [SerializeField]
+        private EventDispatcher _carTrigerEventDispatcher;
 
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carTrigerEventDispatcher.Dispatch();
+                Debug.Log("CarCollision with " + transform.name);
+            }
+        }
+        protected override void Move() {
+          
+        }
+    }
 }
+
