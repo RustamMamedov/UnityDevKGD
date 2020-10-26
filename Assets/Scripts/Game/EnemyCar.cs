661c6acd0ad8e62e95df77424bb68850b49ce096
Andrey <DrEMartstud@gmail.com>
2020-10-26 13:37:57 +0200

    Added  OnTriggerEnter
    Created logic + debug line

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Events;
 using UnityEngine;
 
 namespace Game {
+    
     public class EnemyCar : Car {
+        [SerializeField] 
+        private EventDispatcher _carTriggerEventDispatcher;
 
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carTriggerEventDispatcher.Dispatch();
+                Debug.Log("Collided with " + transform.name);
+            }
+        }
     }
 }
