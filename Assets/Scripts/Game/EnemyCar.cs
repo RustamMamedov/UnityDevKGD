595feb1223d037dee345f3184b075fa9ea32770b
gg <66880r@gmail.com>
2020-10-26 11:50:30 +0200

    collision handling for an enemy car implemented

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Events;
+﻿using Events;
 using UnityEngine;
 
 namespace Game {
+
     public class EnemyCar : Car {
 
+        [SerializeField]
+        private EventDispatcher _carTriggerEventDispatcher;
 
-
-
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _carTriggerEventDispatcher.Dispatch();
+                Debug.Log("CarCollision");
+            }
+        }
     }
 }
