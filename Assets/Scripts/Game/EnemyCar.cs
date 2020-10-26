57f897fe1fff941dcf41e2694add40350af7e53f
Oleg <oleg.kalyugin2001@mail.ru>
2020-10-26 15:08:55 +0200

    EnemyCar script

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game;
+using Events;
 
 namespace Game {
     public class EnemyCar : Game.Car {
-        
+
+        [SerializeField]
+        private EventDispatcher _roadCollisionEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            Debug.Log("CarCollision");
+        }
     }
 }
