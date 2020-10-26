89260e84e6cc39996ba7c23cbaf8ab4fe842d28b
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-26 16:36:19 +0200

    Created RoadPart script and RoadCollision asset

---
+++ Assets/Scripts/Game/RoadPart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Game {
+    public class RoadPart : MonoBehaviour {
+
+        [SerializeField]
+        private EventDispatcher _roadTriggerEventDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _roadTriggerEventDispatcher.Dispatch();
+            }
+        }
+    }
+}
+
