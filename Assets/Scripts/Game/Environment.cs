8e69ffce0338102e366666666748280647a98259
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-26 16:36:55 +0200

    Created Environment script
    added variables
    added generation of road parts
    added road parts movements

---
+++ Assets/Scripts/Game/Environment.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+
+namespace Game {
+    public class Environment : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _roadCollisionEventListener;
+
+        [SerializeField]
+        private GameObject _roadPrefab;
+
+        [SerializeField]
+        private int _initialRoadNumber = 10;
+
+        [SerializeField]
+        private int _roadLength = 12;
+
+        private List<Transform> _roadTransforms;
+
+        private void Start() {
+            GenerateRoad();    
+        }
+
+        private void OnEnable() {
+            _roadCollisionEventListener.OnEventHappened += HandleRoadCollision;
+        }
+
+        private void OnDisable() {
+            _roadCollisionEventListener.OnEventHappened -= HandleRoadCollision;
+        }
+
+
+        private void GenerateRoad() {
+            _roadTransforms = new List<Transform>();
+            for (int i = 0; i < _initialRoadNumber + 1; i++) {
+                var position = new Vector3(0f, 0f, (i - 1) * _roadLength);
+                var road = Instantiate(_roadPrefab, position, Quaternion.identity);
+                _roadTransforms.Add(road.transform);
+            }
+
+            _roadPrefab.gameObject.SetActive(false);
+        }
+
+        private void HandleRoadCollision() {
+            MoveFirstRoadPart();
+        }
+
+        private void MoveFirstRoadPart() {
+            var firstRoadPart = _roadTransforms[0];
+            _roadTransforms.RemoveAt(0);
+            firstRoadPart.position = new Vector3(0f, 0f, _roadTransforms[_roadTransforms.Count - 1].position.z + _roadLength);
+            _roadTransforms.Add(firstRoadPart);
+        }
+    }
+}
+
