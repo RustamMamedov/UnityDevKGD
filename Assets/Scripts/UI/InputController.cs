edf395de3a4e45ccf2b767dfbee0db4b42fa4d63
Alexandr <navacar@mail.ru>
2020-10-26 19:58:49 +0200

    InputControllerr

---
+++ Assets/Scripts/UI/InputController.cs
@@ -0,0 +1,49 @@
+﻿using Events;
+using Game;
+using UnityEngine;
+
+namespace UI {
+    public class InputController : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private EventDispatcher _touchEventDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _touchSide;
+
+        private void OnEnable() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void OnDisable() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            var touchPosition = Vector2.zero;
+
+#if UNITY_EDITOR
+            if (!Input.GetMouseButton(0)) {
+                return;
+            }
+            touchPosition = Input.mousePosition;
+
+#else 
+            if (!Input.touchCount < 1) {
+                return;
+            }
+            touchPosition = InputController.touches[0].position;
+#endif
+
+            _touchSide.value = touchPosition.x > Screen.width * .5 ? 1 : -1;
+            Debug.Log(_touchSide.value);
+            _touchEventDispatcher.Dispatch();
+            _touchSide.value = 0;
+
+        }
+
+    }
+}
