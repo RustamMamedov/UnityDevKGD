71f7908e6c345b8e24e01cf2735275e01fddc9fb
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-10-29 21:56:06 +0200

    input controller: input handle added

---
+++ Assets/Scripts/Game/InputController.cs
@@ -0,0 +1,46 @@
+﻿using Events;
+using Game;
+using UnityEngine;
+
+namespace Game {
+
+    public class InputController : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private EventDispatcher _touchEventDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _touchSide;
+
+        private void OnEnable() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void OnDisable() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            var touchPosition = Vector2.zero;
+#if UNITY_EDITOR
+            if (!Input.GetMouseButtonDown(0)) {
+                return;
+            }
+
+            touchPosition = Input.mousePosition;
+#else 
+            if (Input.touches[0] < 1) {
+                return;
+            }
+
+            touchPosition = Input.touches[0].position;
+#endif
+            _touchSide.value = touchPosition.x > Screen.width / 2f ? 1 : -1;
+            _touchEventDispatcher.Dispatch();
+            _touchSide.value = 0;
+        }
+    }
+}
