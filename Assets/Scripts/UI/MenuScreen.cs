cab8d71f2145ca243fa96fe11ba0d8c353b0eab7
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-10-30 15:29:29 +0200

    MenuScreen script added to MenuScreen prefab

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class MenuScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _playButton;
+
+        private void Awake() {
+            UIManager.instance.ShowMenuScreen();
+            _playButton.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick() {
+            UIManager.instance.LoadGameplay();
+        }
+
+    }
+
+}
