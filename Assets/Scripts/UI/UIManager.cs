a97d98a478b8dbd563e501c47d608364f82fd308
ErvinRudakov <ervin_rudakov@mail.ru>
2020-10-30 09:23:20 +0200

    Scenes Settings changed

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -20,8 +20,6 @@ namespace UI {
         [SerializeField]
         private GameObject _leaderboardScreen;
 
-        private string _currentSceneName = "Gameplay";
-
         private void Awake() {
             if (Instance != null) {
                 Destroy(gameObject);
@@ -31,49 +29,49 @@ namespace UI {
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-
-        private void Start() {
-            _fader.OnFadeIn += OnSceneFadeIn;
-            _fader.FadeIn();
+        public void LoadMenu()
+        {
+            _fader.OnFadeOut += LoadMenuScene;
+            _fader.FadeOut();
         }
-
-        private void OnSceneFadeIn() {
-            StartCoroutine(FadeOutAndLoadGameplay());
-        }
-
-        private IEnumerator FadeOutAndLoadGameplay() {
-            yield return new WaitForSeconds(3f);
+        public void LoadGameplay()
+        {
 
             _fader.OnFadeOut += LoadGameplayScene;
             _fader.FadeOut();
         }
-
-        private void LoadGameplayScene() {
-            _fader.OnFadeOut -= LoadGameplayScene;
-            StartCoroutine(LoadSceneCoroutine(_currentSceneName));
-            _currentSceneName = _currentSceneName == "Gameplay" ? "Menu" : "Gameplay";
+        private void LoadMenuScene()
+        {
+            _fader.OnFadeOut -= LoadMenuScene;
+            StartCoroutine(LoadSceneCoroutine("Menu"));
+            ShowMenuScreen();
         }
-
+        private void LoadGameplayScene()
+        {
+            _fader.OnFadeOut -= LoadGameplayScene;
+            StartCoroutine(LoadSceneCoroutine("Gameplay"));
+            ShowGameScreen();
+        }
         private IEnumerator LoadSceneCoroutine(string sceneName) {
             var asyncOperation = SceneManager.LoadSceneAsync(sceneName);
             while (!asyncOperation.isDone) {
                 yield return null;
             }
-
-            yield return new WaitForSeconds(3f);
-
             _fader.FadeIn();
         }
 
         public void ShowMenuScreen() {
+            HideAllScreens();
             _menuScreen.SetActive(true);
         }
 
         public void ShowGameScreen() {
+            HideAllScreens();
             _gameScreen.SetActive(true);
         }
 
         public void ShowLeaderboardsScreen() {
+            HideAllScreens();
             _leaderboardScreen.SetActive(true);
         }
 
