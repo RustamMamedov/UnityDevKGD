a881a811f7486194eedbb68d76f7f4bb8f92897f
Alexandr <navacar@mail.ru>
2020-10-30 21:44:46 +0200

    Making OnCarCollision protected virtual

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -33,7 +33,7 @@ namespace Game {
             _carCollisionEventListener.OnEventHappened -= OnCarCollision;
         }
 
-        private void OnCarCollision() {
+        protected virtual void OnCarCollision() {
             UnsubscribeToEvents();
         }
 
