a46e706602d62ddabc32b5c8b60b1f6e9fc48b8c
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-10-30 16:04:01 +0200

    LeaderboardScreen script added to Leaderboard prefab

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class LeaderboardScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _menuButton;
+
+        private void Awake() {
+            _menuButton.onClick.AddListener(OnClickMenuButton);
+        }
+
+        private void OnClickMenuButton() {
+            UIManager.instance.LoadMenu();
+        }
+    }
+}
