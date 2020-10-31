8b70fb72bfe392c6284bb0d2d33150b6d1643726
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-10-31 23:48:27 +0200

    Added asset to handle dodged cars

---
+++ Assets/Scripts/Game/Cars.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "Cars", menuName = "Car/Cars")]
+    public class Cars : ScriptableObject {
+
+        public List<GameObject> carsList = new List<GameObject>();
+    }
+}
