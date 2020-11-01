4b950f15d4243fe4c3dc6426efd2c9a1c116a15c
Maxim <korunets1901@gmail.com>
2020-11-01 21:22:42 +0300

    Random car at EnemySpawner

--- Assets/Scripts/Game/EnemySpawner.cs
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -13,7 +13,7 @@ namespace Game {
         private EventListener _carCollisionListener;
 
         [SerializeField]
-        private GameObject _carPrefab;
+        private List<GameObject> _carPrefab;
 
         [SerializeField]
         private float _spawnCooldown;
@@ -70,7 +70,8 @@ namespace Game {
         private void SpawnCar() {
             var randomRoad = Random.Range(-1, 2);
             var position = new Vector3(1f * randomRoad * _roadWidth.value, 0f, _playerPositionZ.value + _distanceToPlayerToSpawn);
-            var car = Instantiate(_carPrefab, position, Quaternion.Euler(0f, 180f, 0f));
+            var randomCar = Random.Range(0,_carPrefab.Count);
+            var car = Instantiate(_carPrefab[randomCar], position, Quaternion.Euler(0f, 180f, 0f));
             _cars.Add(car);
         }
 
