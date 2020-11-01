5ebe128e7e32b4226810ffe6aad7bf50ff468144
Andrey <DrEMartstud@gmail.com>
2020-11-01 15:09:30 +0200

    Created LeaderBoard class and added it to prefab

---
+++ Assets/Scripts/UI/LeaderBoardScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class LeaderBoardScreen : MonoBehaviour {
+
+        
+        [SerializeField] 
+        private Button _startGameButton;
+
+        private void Awake() {
+            _startGameButton.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick() {
+            UIManager.Instance.LoadMenu();
+        }
+    }
+}
