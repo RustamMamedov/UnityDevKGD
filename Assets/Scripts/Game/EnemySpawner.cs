455f23b1f976a2b2fda1589317ec668c9a410825
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-02 00:35:35 +0200

    Created EnemySpawner and add to GamePlay

---
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+
+    public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private EventListener _updateEventListener;
+
+	[SerializeField]
+	private EventListener _carCollisionListener;
+
+	[SerializeField]
+	private GameObject _carPrefab;
+
+	[SerializeField]
+	private float _spawnCooldown;
+
+	[SerializeField]
+	private float _distanceToPlayerToSpawn;
+
+	[SerializeField]
+	private float _distanceToPlayerToDestroy;
+
+	[SerializeField]
+	private ScriptableFloatValue _playerPositionZ;
+
+	[SerializeField]
+	private ScriptableFloatValue _roadWidth;
+
+	private float _currentTimer;
+	private List<GameObject> _cars = new List<GameObject>();
+
+	private void OnEnable() {
+	    SubscribeToEvents();
+	}
+
+	private void OnDisable() {
+	    UnsubscribeToEvents();
+	}
+
+	private void SubscribeToEvents() {
+	    _updateEventListener.OnEventHappend += UpdateBehaviour;
+	    _carCollisionListener.OnEventHappend += OnCarCollision;
+	}
+
+	private void UnsubscribeToEvents() {
+	    _updateEventListener.OnEventHappend -= UpdateBehaviour;
+	    _carCollisionListener.OnEventHappend -= OnCarCollision;
+	}
+
+	private void OnCarCollision() {
+	    UnsubscribeToEvents();
+	}
+
+	private void UpdateBehaviour() {
+	    HandlerCarsBehindPlayer();
+
+	    _currentTimer += Time.deltaTime;
+	    if (_currentTimer < _spawnCooldown) {
+		return;
+	    }
+
+	    _currentTimer = 0;
+
+	    SpawnCar();
+	}
+
+	private void SpawnCar() {
+	    var randomRoad = Random.Range(-1, 2);
+	    var position = new Vector3(1f * randomRoad * _roadWidth.value, 0f, _playerPositionZ.value + _distanceToPlayerToSpawn);
+	    var car = Instantiate(_carPrefab, position, Quaternion.Euler(0f,180f,0f));
+	    _cars.Add(car);
+	}
+
+	private void HandlerCarsBehindPlayer() {
+	    for (int i = _cars.Count - 1; i > -1; i--) {
+		if (_playerPositionZ.value - _cars[i].transform.position.z > _distanceToPlayerToDestroy) {
+		    Destroy(_cars[i]);
+		    _cars.RemoveAt(i);	  
+		}
+	    }
+	}
+    }
+}
