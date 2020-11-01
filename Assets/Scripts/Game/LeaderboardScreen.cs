f403e4056969b6b5f83b7f12876d1b9b0a0276b3
vladislav <vladislavnikwork@gmail.com>
2020-11-02 00:46:25 +0200

    Created "LeaderboardScreen.cs".

---
+++ Assets/Scripts/Game/LeaderboardScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Events;
+namespace UI {
+
+    public class LeaderboardScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _menuB;
+
+        private void Awake() {
+            _menuB.onClick.AddListener(OnMenuButtonClick);
+        }
+        private void OnMenuButtonClick() {
+            UIManager.Instance.LoadMenu();
+        }
+
+
+    }
+
+}
