e2fe298222c418e6a6fd88362c65076467d41fb4
Dan <kdanil01@mail.ru>
2020-11-01 23:08:14 +0200

    Created new CarDodged script

---
+++ Assets/Scripts/Game/CarDodged.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Game{
+    public class CarDodged : MonoBehaviour{
+        [SerializeField]
+        private EventDispatcher _enemyCarsBackTriggerEventDispatch;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("CarBack")) {
+                _enemyCarsBackTriggerEventDispatch.Dispatch();
+                Debug.Log("erer");
+            }
+        }
+    }
+}
