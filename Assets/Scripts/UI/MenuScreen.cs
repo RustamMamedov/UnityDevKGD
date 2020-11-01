5ed5ed3e9c98b407d9217f98bac3923884e147ce
Andrey <DrEMartstud@gmail.com>
2020-11-01 15:14:51 +0200

    Refactored code

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,5 +17,4 @@ namespace UI {
             UIManager.Instance.LoadGameplay();
         }
     }
-
 }
