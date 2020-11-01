555c361314c59037ef83e7a5cabea4221bf266c2
vladislav <vladislavnikwork@gmail.com>
2020-11-02 00:38:11 +0200

    Created "MenuScreen.cs"

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class MenuScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _playB;
+
+        private void Awake() {
+            _playB.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick() {
+            UIManager.Instance.LoadGameplay();
+        }
+    }
+}
