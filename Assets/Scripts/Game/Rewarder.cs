c1192398c2c4598b63c205ef6db1caa69b074197
Valeriy <mr.robariii@gmail.com>
2020-11-01 10:36:40 +0200

    Rewarder script added
    Dodge rewarder script

---
+++ Assets/Scripts/Game/Rewarder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public class Rewarder : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableIntValue _currentScore;
+
+        private void OnDisable() {
+            _currentScore.value = 0;
+        }
+
+        public void SetScorePoints(EnemyCarToDodge enemyCar) {
+            _currentScore.value += enemyCar.currentCar.GetComponent<EnemyCar>().CarSettings.dodgeScore;
+        }
+
+       
+
+    }
+}
+
