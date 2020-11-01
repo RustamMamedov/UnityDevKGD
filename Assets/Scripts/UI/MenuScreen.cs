f6f189290a6c284b68e07eb3b01e23190252d344
Nikita <Nkanunov@gmail.com>
2020-11-01 23:24:48 +0200

    Added MenuScreen script

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScreen : MonoBehaviour {
+    private void Awake() {
+    }
+
+    private void OnPlayButtonClick() {
+        
+    }
+}
