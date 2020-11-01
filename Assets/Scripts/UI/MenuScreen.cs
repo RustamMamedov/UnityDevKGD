52de9df6a320410990f95e4f629a1c8950888de1
Nikita <Nkanunov@gmail.com>
2020-11-02 00:31:33 +0200

    Added PlayButton OnClick method

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using UI;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuScreen : MonoBehaviour {
+
+    [SerializeField]
+    private Button _playButton;
     private void Awake() {
+        _playButton.onClick.AddListener(OnPlayButtonClick);
     }
 
     private void OnPlayButtonClick() {
-        
+        UIManager.Instance.LoadGameplay();
     }
 }
