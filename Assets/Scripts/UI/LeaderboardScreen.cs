b03227a5c9169b3282fea7efbd0929a329ae3743
Dan <kdanil01@mail.ru>
2020-11-01 21:27:38 +0200

    LeaderScreen changes

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI{
+    
+    public class LeaderboardScreen : MonoBehaviour{
+        [SerializeField]
+        private Button _menuButton;
+
+        private void Awake() {
+            _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnMenuButtonClick(){
+            UIManager.Instance.LoadMenu();
+        }
+    }
+}
