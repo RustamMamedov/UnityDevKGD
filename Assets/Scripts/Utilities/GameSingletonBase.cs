fce2cb046fbc0723279e137818b81795381daf13
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-01 16:43:33 +0200

    Add GameSingletonBase generic script

---
+++ Assets/Scripts/Utilities/GameSingletonBase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Utilities {
+    
+    public class GameSingletonBase<TSelf> : MonoBehaviour
+        where TSelf : GameSingletonBase<TSelf> {
+
+        // Fields.
+
+        private static TSelf _instance;
+
+
+        // Properties.
+
+        public static TSelf Instance => _instance;
+
+
+        // Life cycle.
+
+        protected virtual void Awake() {
+            if (_instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+            _instance = (TSelf) this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+    }
+
+}
