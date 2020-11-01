41dbd1f09b1f389f62763126821dbbdfa9588765
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-02 00:57:58 +0200

    create EnemySpawner and add PlayerPositionZ in

---
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+    public class EnemySpawner : MonoBehaviour{
+        [SerializeField]
+        private EventListeners _updateEventListeners;
+
+        [SerializeField]
+        private EventListeners _carCollisionListener;
+
+        [SerializeField]
+        private GameObject _carPrefab;
+
+        [SerializeField]
+        private float _spawnCooldown;
+
+        [SerializeField]
+        private float _distanceToPlayerToSpawner;
+
+        [SerializeField]
+        private float _distanceToPlayerToDestroy;
+
+        [SerializeField]
+        private ScriptableFloatValue _playerPisotionZ;
+
+        [SerializeField]
+        private ScriptableFloatValue _roadWidth;
+
+        private float _currentTimer;
+        private List<GameObject> _cars = new List<GameObject>();
+
+        private void OnEnable() {
+            SubscribeToEvent();
+        }
+        private void OnDisable() {
+            UnsubscribeToEvent();
+        }
+
+        private void SubscribeToEvent() {
+            _updateEventListeners.OnEventHappened += UpdateBehaviour;
+            _carCollisionListener.OnEventHappened += OnCarCollision;
+        }
+        private void UnsubscribeToEvent() {
+            _updateEventListeners.OnEventHappened -= UpdateBehaviour;
+            _carCollisionListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            UnsubscribeToEvent();
+        }
+
+        private void UpdateBehaviour() {
+            HandleCarsBehindPlayer();
+            _currentTimer += Time.deltaTime;
+            if (_currentTimer < _spawnCooldown) {
+                return;
+            }
+            _currentTimer = 0f;
+
+            SpawnCar();
+        }
+
+        private void SpawnCar() {
+            var randomRoad = Random.Range(-1, 2);
+            var position = new Vector3(1f * randomRoad * _roadWidth.value, 0f, _playerPisotionZ.value + _distanceToPlayerToSpawner);
+            var car = Instantiate(_carPrefab, position, Quaternion.Euler(0f, 180f, 0f));
+            _cars.Add(car);
+        }
+
+         private void HandleCarsBehindPlayer() {
+            for(int i = _cars.Count - 1; i > -1; i--) {
+                if (_playerPisotionZ.value - _cars[i].transform.position.z > _distanceToPlayerToDestroy) {
+                    Destroy(_cars[i]);
+                    _cars.RemoveAt(i);
+                }
+            }
+        }  
+    }
+}
+
