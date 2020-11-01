c619d3fe6feb6e8b6038678dec5912d90c6294f4
Ilya <elya5911@mail.ru>
2020-11-01 22:15:20 +0200

    Create InputController

---
+++ Assets/Scripts/UI/InputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+using Game;
+
+namespace UI {
+    public class InputController : MonoBehaviour {
+        [SerializeField]
+        private EventListeners _updateEventListener;
+
+        [SerializeField]
+        private EventDispatcher _touchEventDiptacher;
+
+        [SerializeField]
+        private ScriptableIntValue _touchSide;
+
+        private void OnEnable() {
+            _updateEventListener.OnEventHappened +=UpdateBehavour;
+        }
+
+        private void OnDisable() {
+            _updateEventListener.OnEventHappened -= UpdateBehavour;
+        }
+        private void UpdateBehavour() {
+            var touchPosition = Vector2.zero;
+#if UNITY_EDITOR
+            if (!Input.GetMouseButton(0)) { //если не ножата левая кнопка то выходим иначе
+                return;
+            }
+            touchPosition = Input.mousePosition;//запоминаем позицию нажатия
+
+#else
+            if (Input.touchCount < 1) {
+                return;
+            }
+            touchPosition = Input.touches[0].position;
+#endif
+            _touchSide.Value = (touchPosition.x > Screen.width * .5 ? 1 : -1);
+            _touchEventDiptacher.Dispatch();
+            _touchSide.Value = 0;
+        }
+    }
+}
