e1d44c9d2a320004ec80e28bdadba714a84d84e7
Valeriy <mr.robariii@gmail.com>
2020-11-01 10:43:23 +0200

    Added LedaerBoardScreen script

---
+++ Assets/Scripts/UI/LeaderBoardScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UI {
+    public class LeaderBoardScreen : MonoBehaviour {
+
+        public void LoadMenuButton() {
+            UIManager.instance.LoadMenu();
+        }
+    }
+}
+
