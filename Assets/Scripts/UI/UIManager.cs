ad9e8600008b3cffb8311939f922862db879b708
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-01 16:46:14 +0200

    Modify UIManager script
    Make UIManager script a singleton.
    
    Allow the script to use Fader.
    
    Add scene loading methods to the script.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities;
 
 namespace UI {
 
-    public class UIManager : MonoBehaviour {
+    public class UIManager : GameSingletonBase<UIManager> {
+
+        // Constants.
+
+        private const string _menuSceneName = "Menu";
+        private const string _gameplaySceneName = "Gameplay";
+
+
+        // Fields.
+
+        [SerializeField]
+        private Fader _fader;
 
         [SerializeField]
         private GameObject _menuScreen;
@@ -14,6 +27,44 @@ namespace UI {
 
         [SerializeField]
         private GameObject _leaderboardScreen;
+
+
+        // Scene loading.
+
+        public void LoadGameplayScene() {
+            _fader.OnFadeOut += PrepareGameplayScene;
+            _fader.FadeOut();
+        }
+
+        public void LoadMenuScene() {
+            _fader.OnFadeOut += PrepareMenuScene;
+            _fader.FadeOut();
+        }
+
+        private void PrepareGameplayScene() {
+            _fader.OnFadeOut -= PrepareGameplayScene;
+            HideAllScreens();
+            ShowGameScreen();
+            StartCoroutine(LoadSceneCoroutine(_gameplaySceneName));
+        }
+
+        private void PrepareMenuScene() {
+            _fader.OnFadeOut -= PrepareMenuScene;
+            HideAllScreens();
+            ShowMenuScreen();
+            StartCoroutine(LoadSceneCoroutine(_menuSceneName));
+        }
+
+        private IEnumerator LoadSceneCoroutine(string name) {
+            var loading = SceneManager.LoadSceneAsync(name);
+            while (!loading.isDone) {
+                yield return null;
+            }
+            _fader.FadeIn();
+        }
+
+
+        // Screen showing/hiding methods.
 
         public void ShowMenuScreen() {
             _menuScreen.SetActive(true);
@@ -33,6 +84,7 @@ namespace UI {
             _leaderboardScreen.SetActive(false);
         }
         
+
     }
 
 }
