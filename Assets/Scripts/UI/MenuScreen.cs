caa91c234c8ec86e7dfb6bb356af14367dd864af
Denis <denis.zhuravkov@gmail.com>
2020-11-02 01:55:52 +0200

    "MenuScreen" script created
    MenuScreen contains all methods that we can use to handle UI click events (for now it's only PlayButton)
    
    Also added script to MenuScreen prefab

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    
+    public class MenuScreen : MonoBehaviour {
+
+        [SerializeField] 
+        private Button _playButton;
+        
+        private void Awake() {
+            _playButton.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnDestroy() {
+            _playButton.onClick.RemoveListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick(){
+            UIManager.Instance.LoadGameplay();
+        }
+    }
+}
+
