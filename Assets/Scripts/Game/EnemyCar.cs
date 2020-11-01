aa549735bb3644df38c1b1177b2f45baa25c9f73
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-01 19:59:51 +0200

    Add dodge registration to EnemyCar script

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,19 +1,55 @@
 ﻿using Events;
 using UnityEngine;
+using Values;
 
 namespace Game {
     
     public class EnemyCar : Car {
-        
+
+        // Fields.
+
+        // Minimal distance along Z axis between this and player cars
+        // needed for dodge to happen.
+        [SerializeField]
+        private float _dodgeDistance;
+
         [SerializeField]
         private EventDispatcher _carCollisionDispatcher;
+
+        [SerializeField]
+        private EventDispatcher _carDodgedDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScoreValue;
+
+        [SerializeField]
+        private ScriptableFloatValue _playerPositionZValue;
+
+        // True if dodge already happened.
+        private bool _dodged = false;
+
+
+        // Life cycle.
 
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carCollisionDispatcher.Dispatch();
-                
             }
         }
+
+
+        // Motion methods.
+
+        protected override void Move() {
+            base.Move();
+            if (!_dodged && transform.position.z < _playerPositionZValue.value - _dodgeDistance) {
+                _dodged = true;
+                _currentScoreValue.value += CarSettings.dodgeScore;
+                _carDodgedDispatcher.Dispatch();
+            }
+        }
+
+
     }
 
 }
