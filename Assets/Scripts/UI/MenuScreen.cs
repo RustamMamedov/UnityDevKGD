866f1ca434f6adcbf4f3a754a814de3d77568cab
TechesDePainture <kirillzelen00@gmail.com>
2020-11-01 22:06:36 +0200

    Created MenuScreen

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace UI { 
+
+    public class MenuScreen : MonoBehaviour {
+
+        [SerializeField]
+        private UIManager _uIManager;
+
+        [SerializeField]
+        private Button _playButtom;
+        private void Awake() {
+            _playButtom.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick() {
+            _uIManager.LoadGameplay();
+        }
+
+
+    }
+}
