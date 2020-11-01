0fd51e60f196f63826b88e5969383bcc829376cc
Alex Osi <noemail>
2020-11-01 19:08:36 +0200

    Fix Enemy Cars' prefabs

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -29,7 +29,7 @@ namespace Game {
         }
 
         private void OnCarCollision() {
-          // UnsubscribeToEvents();
+          UnsubscribeToEvents();
         }
 
         private void UpdateBehaviour() {
