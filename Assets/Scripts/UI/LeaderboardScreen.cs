3533cfd8cb7e078ec2e8a5388a29a7852f3bc16b
TechesDePainture <kirillzelen00@gmail.com>
2020-11-01 22:10:18 +0200

    LeaderboatdScreen created and menubutton added to prefab

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace UI { 
+
+    public class LeaderboardScreen : MonoBehaviour {
+
+        [SerializeField]
+        private UIManager _uIManager;
+
+        [SerializeField]
+        private Button _menuButtom;
+        private void Awake() {
+            _menuButtom.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnMenuButtonClick() {
+            _uIManager.LoadGameplay();
+        }
+
+
+    }
+}
