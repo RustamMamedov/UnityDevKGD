cb9cffbe87a3c634e9a7c569c015b7db74395758
Rustam Mamedov <mamedov@panzerdog.com>
2020-11-01 09:49:24 +0200

    MenuScreen

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class MenuScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _playButton;
+
+        private void Awake() {
+            _playButton.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick() {
+            UIManager.Instance.LoadGameplay();
+        }
+    }
+}
