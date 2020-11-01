b7f38cf4c004e34a4f4ac1df0863e3a979361126
TechesDePainture <kirillzelen00@gmail.com>
2020-11-01 16:25:59 +0200

    EnemySpawner created and added to GameScene

---
+++ Assets/Scripts/Game/EmenySpawner.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Events;
+namespace Game {
+    public class EmenySpawner : MonoBehaviour {
+        [SerializeField]
+        private EventListener _updateEventListener;
+        [SerializeField]
+        private EventListener _carCollisionListener;
+        [SerializeField]
+        private GameObject _carPrefab;
+        [SerializeField]
+        private float _spawnCooldown;
+        [SerializeField]
+        private float _distanceToPlayerToSpawn;
+        [SerializeField]
+        private float _distanceToPlayerToDestroy;
+        [SerializeField]
+        private ScriptableFloatValue _playerPositionZ;
+        [SerializeField]
+        private ScriptableFloatValue _roadWidth;
+
+        private float _currentTimer;
+
+        private void OnEnable() {
+            SubscribeToEvents();
+        }
+
+        private void OnDisable() {
+            UnsubscribeToEvents();
+        }
+
+        private void SubscribeToEvents() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+            _carCollisionListener.OnEventHappened += OnCarCollision;
+        }
+
+        private void UnsubscribeToEvents() {
+            _updateEventListener.OnEventHappened -= UpdateBehaviour;
+            _carCollisionListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void UpdateBehaviour() {
+            _currentTimer += Time.deltaTime;
+            if (_currentTimer<_spawnCooldown) {
+                return;
+            }
+            _currentTimer = 0f;
+
+            SpawnCar();
+        }
+        
+        private void OnCarCollision() {
+            UnsubscribeToEvents();
+        }
+
+        private void SpawnCar() {
+            var randomRoad = Random.Range(-1, 2);
+            var position = new Vector3((float)randomRoad*_roadWidth.value,0f,_playerPositionZ.value+_distanceToPlayerToSpawn);
+            var car = Instantiate(_carPrefab,position,Quaternion.Euler(0f,180f,0f));
+        }
+
+    }
+}
