f5a5c8505aab5fe54bb2385cd78e579ba235aff5
Nikita <Nkanunov@gmail.com>
2020-11-02 18:06:53 +0200

    Creaded LeaderboardShow script

---
+++ Assets/Scripts/UI/LeaderboardShow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace UI {
+    public class LeaderboardShow : MonoBehaviour {
+
+
+        [SerializeField] private EventListener _collisionEventListener;
+
+        private void Awake() {
+            _collisionEventListener.OnEventHappened += ShowLeaderboardScreen;
+        }
+
+        private void ShowLeaderboardScreen() {
+            UIManager.Instance.ShowLeaderboardScreen();
+        }
+
+    }
+}
