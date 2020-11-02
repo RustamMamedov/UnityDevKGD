2193e032c0d771b595c241e658681e4a56f66ef5
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-02 13:03:29 +0200

    refactoring code MenuScreen

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UI;
+
 
 namespace UI {
 
     public class MenuScreen : MonoBehaviour {
-        
+
+        [SerializeField]
+        private Button _playButton;
+
+        private void Awake() {
+            _playButton.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        private void OnPlayButtonClick() {
+            UIManager.Instance.LoadGamePlay();
+        }
+
     }
 }
