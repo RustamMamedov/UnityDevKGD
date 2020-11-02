1c041f0506f5d2ebf1dcb549e4294ba6107e9917
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-03 01:16:24 +0200

    Add CountingManager script
    Add script for score updating and other potential counting.

---
+++ Assets/Scripts/Game/CountingManager.cs
@@ -0,0 +1,56 @@
+﻿using Events;
+using UnityEngine;
+using Values;
+
+namespace Game {
+    
+    public class CountingManager : MonoBehaviour {
+
+        // Fields.
+
+        [SerializeField]
+        private EventListener _carDodgeEventListener;
+
+        [SerializeField]
+        private ScriptableIntValue _dodgeScoreValue;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScoreValue;
+
+
+        // Life cycle.
+
+        private void Awake() {
+            ResetScores();
+        }
+
+        private void OnEnable() {
+            _carDodgeEventListener.OnEventHappened += OnCarDodge;
+        }
+
+        private void OnDisable() {
+            _carDodgeEventListener.OnEventHappened -= OnCarDodge;
+        }
+
+        private void OnDestroy() {
+            ResetScores();
+        }
+
+
+        // Event handlers.
+
+        private void OnCarDodge() {
+            _currentScoreValue.value += _dodgeScoreValue.value;
+        }
+
+
+        // Supportive methods.
+
+        private void ResetScores() {
+            _currentScoreValue.value = 0;
+        }
+
+
+    }
+
+}
