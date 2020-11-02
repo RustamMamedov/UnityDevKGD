408b418f2710d16c957a4c8f221c09bf573f2744
BepeckoB <BepeckoB@gmail.com>
2020-11-02 10:23:03 +0200

    created main menu "play" button logic

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UI;
+
+public class MenuScreen : MonoBehaviour {
+
+    [SerializeField]
+    private Button _playButton;
+
+    private void Awake() {
+        Button playButton = _playButton.GetComponent<Button>();
+        playButton.onClick.AddListener(OnPlayButtonClick);
+        
+    }
+
+    public void OnPlayButtonClick() {
+        UIManager.Instance.LoadGameplay();
+    }
+}
+
