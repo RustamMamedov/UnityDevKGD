2979773d1d8acd4c92bf685902ba7e4ad98c1fb2
Rustam Mamedov <mamedov@panzerdog.com>
2020-11-02 10:26:55 +0200

    score counting on car dodge

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -8,9 +8,22 @@ namespace Game {
         [SerializeField]
         private EventDispatcher _carCollisionEventDispatcher;
 
+        [SerializeField]
+        private EventDispatcher _carDodgedEventDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _dodgedScore;
+
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carCollisionEventDispatcher.Dispatch();
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (other.CompareTag("PlayerDodge")) {
+                _dodgedScore.value = _carSettings.dodgeScore;
+                _carDodgedEventDispatcher.Dispatch();
             }
         }
     }
