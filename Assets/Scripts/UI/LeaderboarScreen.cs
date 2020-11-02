8db182974da8aa744ee3e0a24decf5fcd0c49fc7
Maxim <korunets1901@gmail.com>
2020-11-02 13:43:46 +0300

    crate leaderboardscreen script

---
+++ Assets/Scripts/UI/LeaderboarScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class LeaderboarScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _menuButton;
+
+        private void Awake() {
+            _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnMenuButtonClick() {
+            UIManager.Instance.LoadMenu();
+        }
+    }
+}
+
