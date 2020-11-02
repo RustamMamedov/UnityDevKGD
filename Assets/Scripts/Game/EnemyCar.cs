dd486f94a28de8a19d570115cfd7401fc87b19ce
SaraJess <sarycheva.00@mail.ru>
2020-11-02 22:46:18 +0200

    Create Score

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -8,10 +8,46 @@ namespace Game {
 
         [SerializeField]
         private EventDispatcher _carCollisionDispatcher;
+
+        [SerializeField]
+        private BoxCollider _boxCollider;
+
+        [SerializeField]
+        private ScriptableFloatValue _playerSize;
+
+        [SerializeField]
+        private ScriptableIntValue _score;
+
+        [SerializeField]
+        private EventDispatcher _carDodge;
+
+        [SerializeField]
+        private ScriptableFloatValue _carPositionZ;
+
+        private void IsDodge() {
+            if (_carPositionZ.value - transform.position.z > _playerSize.value + _boxCollider.size.z / 2) {
+                _carDodge.Dispatch();
+                _score.value += _carSettings.dodgeScore;
+                _updateEventListener.OnEventHappened -= IsDodge;
+            }
+        }
+
+        protected override void SubscribeToEvents() {
+            base.SubscribeToEvents();
+            _updateEventListener.OnEventHappened += IsDodge;
+        }
+
+        protected override void UnsubscribeToEvents() {
+            base.UnsubscribeToEvents();
+            _updateEventListener.OnEventHappened -= IsDodge;
+        }
+
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carCollisionDispatcher.Dispatch();
             }
         }
+
+
     }
 }
--- Assets/Scripts/Game/RoadPart.cs
+++ Assets/Scripts/Game/RoadPart.cs
@@ -14,6 +14,5 @@ namespace Game {
                 _roadTriggerEventDispatcher.Dispatch();
             }
         }
-
     }
 }
