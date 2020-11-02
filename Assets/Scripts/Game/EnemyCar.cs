835447ba5c6a1272eda6e0419c76bca56f7ca4a7
Ilya <elya5911@mail.ru>
2020-11-02 18:27:34 +0200

    Method Dodged to Dipatch OnCarDodged

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 using Events;
 
 namespace Game {
+
     public class EnemyCar : Car {
+
         [SerializeField]
         private EventDispatcher _enemyCarsCollisionEventDispatcher;
+
+        [SerializeField]
+        private ScriptableFloatValue _positionPlayerCarZ;
+
+        [SerializeField]
+        private float _distansCarZ;
+
+        [SerializeField]
+        private EventDispatcher _carDodgedEventDispatcher; 
 
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _enemyCarsCollisionEventDispatcher.Dispatch();
+            }
+        }
+
+        protected override void Move() {
+            base.Move();
+            Dodged();
+            
+        }
+
+        private void Dodged() {
+            var distance = _positionPlayerCarZ.Value - transform.position.z;
+            if (distance > _distansCarZ) {
+                _carDodgedEventDispatcher.Dispatch();
             }
         }
     }
