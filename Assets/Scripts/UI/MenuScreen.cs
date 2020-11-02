c5dd8484699fce4ca597de0b8cc61aa125f20e1c
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-02 16:46:19 +0200

    create MenuScrean add prefab

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace UI {
+    public class MenuScreen : MonoBehaviour {
+        [SerializeField]
+        private Button _play;
+
+        [SerializeField]
+        private UIManager _uiManager;
+        private void Awake() {
+            _play.onClick.AddListener(OnPlayButtonClick);
+        }
+        private void OnPlayButtonClick() {
+            _uiManager.LoadGameplay();
+        }
+    }
+}
