bce12d1fa5947ff22509cb8a0f6d4edfa1c812cf
Denis <denis.zhuravkov@gmail.com>
2020-11-02 02:17:02 +0200

    "LeaderboardScreen" script created
    LeaderboardScreen contains all methods that we can use to handle UI click events (for now it's only MenuButton)
    
    Also added script to LeaderboardScreen prefab

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    
+    public class LeaderboardScreen : MonoBehaviour {
+
+        [SerializeField] 
+        private Button _menuButton;
+        
+        void Awake() {
+            _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnDestroy() {
+            _menuButton.onClick.RemoveListener(OnMenuButtonClick);
+        }
+
+        private void OnMenuButtonClick() {
+            UIManager.Instance.LoadMenu();
+        }
+    }
+}
