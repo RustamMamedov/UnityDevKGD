835e1378d73286a78f076354069a0ca8af693f69
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-02 13:12:44 +0200

    logic EnemyCar Score

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,5 +1,9 @@
-﻿using Events;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Events;
+using UI;
+
 
 namespace Game {
 
@@ -8,10 +12,31 @@ namespace Game {
         [SerializeField]
         private EventDispatcher _carTriggerEventDispatcher;
 
+        [SerializeField]
+        private ScriptableIntValue _currentScore;
+
+        [SerializeField]
+        private ScriptableFloatValue _playerPositionZ;
+
+        [SerializeField]
+        private BoxCollider _boxColliderEnemy;
+        private bool _dodged = false;
+
+
+        protected override void UpdateBehaviour() {
+            if (_playerPositionZ.value >= gameObject.transform.position.z - _boxColliderEnemy.size.z && !_dodged)   {
+                _currentScore.value += _carSettings.dodgeScore;
+                _dodged = true;
+            }
+            base.UpdateBehaviour();
+        }
+
         private void OnTriggerEnter(Collider other) {
+        
             if (other.CompareTag("Player")) {
                 _carTriggerEventDispatcher.Dispatch();
-                Debug.Log("CarCollision");
+                UIManager.Instance.ShowLeaderboardScreen();
+
             }
         }
     }
