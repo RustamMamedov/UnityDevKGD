ab2fd8b61b4578d59a7eec8198899d1b24b5ce64
Nikita <Nkanunov@gmail.com>
2020-11-02 02:33:50 +0200

    Added dodge check and increase in points

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
 using Events;
+using UI;
 
 namespace Game {
-    
+
     public class EnemyCar : Car {
 
-        [SerializeField]
-        private EventDispatcher _carCollisionEventDispatcher;
+        [SerializeField] private EventDispatcher _carCollisionEventDispatcher;
+
+        [SerializeField] private ScriptableIntValue _currentScore;
 
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
+                _currentScore.value = 0;
                 _carCollisionEventDispatcher.Dispatch();
                 Debug.Log("CarCollision");
+                UIManager.Instance.ShowLeaderboardScreen();
+              
+
+            }
+
+            if (other.CompareTag("ScoreTrigger")) {
+                _currentScore.value++;
             }
         }
     }
