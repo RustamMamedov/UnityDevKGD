44ea9bce01301acf372cefbbb5f55547e278cb86
Nikita <nikitadedov66@gmail.com>
2020-11-02 13:38:05 +0200

    added menuscreen script

---
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Events;
+
+namespace UI {
+
+    public class MenuScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _playButton;
+
+        private void Awake() {
+
+            _playButton.onClick.AddListener(OnPlayButtonClick);
+        }
+
+        public void OnPlayButtonClick() {
+            UIManager.Instance.LoadGamePlay();
+        }
+
+    }
+}
