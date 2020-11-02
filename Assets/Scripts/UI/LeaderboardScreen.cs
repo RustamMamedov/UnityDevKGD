8be2cee46f5d739ce5fd751729b385bc67266124
BarbadosP <var0720@gmail.com>
2020-11-02 16:54:44 +0200

    Menubutton added

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace UI {
+    public class LeaderboardScreen : MonoBehaviour{
+        [SerializeField]
+        private Button _menu;
+
+        [SerializeField]
+        private UIManager _uiManager;
+
+        private void Awake() {
+            _menu.onClick.AddListener(OnPlayButtonClick);
+        
+        }
+
+        private void OnPlayButtonClick() {
+            _uiManager.LoadMenu();
+
+        }
+    }
+}
+
