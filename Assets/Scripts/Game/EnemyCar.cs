ba3bd0a35ee0d56b2cdd4a69b9673bf380862a3e
BepeckoB <BepeckoB@gmail.com>
2020-11-02 10:25:49 +0200

    edited EnemyCar
    now can show Leaderboard screen after collision with enemy car

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using UnityEngine;
 using Events;
+using UI;
 
 namespace Game {
 
     public class EnemyCar : Car {
+
         [SerializeField]
         private EventDispatcher _carTriggerEventDispatcher;
 
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
-                Debug.Log("CarCollision");
+
+                UIManager.Instance.ShowLeaderboardsScreen();
                 _carTriggerEventDispatcher.Dispatch();
             }
         }
+
     }
 }
