29a1ea7f3c791762c710827f0d45b0074caf9d4e
Nikita <Nkanunov@gmail.com>
2020-11-02 02:31:40 +0200

    Added LeaderboardScreen script

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,17 @@
+﻿using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardScreen : MonoBehaviour {
+   
+   [SerializeField]
+   private Button _menuButton;
+
+   private void Awake() {
+      _menuButton.onClick.AddListener(OnPlayButtonClick);
+   }
+
+   private void OnPlayButtonClick() {
+      UIManager.Instance.LoadMenu();
+   }
+}
