9261660ba860747b2d62a1178691bc0e6d134bf1
TechesDePainture <kirillzelen00@gmail.com>
2020-11-02 19:31:18 +0200

    Saved and load data

--- Assets/Scripts/Game/Save.cs
+++ Assets/Scripts/Game/Save.cs
@@ -14,16 +14,28 @@ namespace Game {
             public string score;
         }
 
+        //туть пропустил
+        [SerializeField]
+        private class SavedDataWrapper {
+
+            public List<SaveData> saveDatas;
+        }
+
         [SerializeField]
         private EventListener _carCollisionEventListener;
 
         [SerializeField]
         private ScriptableIntValue _currentScore;
 
-        private List<SaveData> _savedData;
+        private List<SaveData> _savedDatas;
+        public List<SaveData> SavedDatas => _savedDatas;
+
+
+        private const string RECOREDS_KEY = "records";
 
         private void Awake() {
-            _savedData = new List<SaveData>();
+            _savedDatas = new List<SaveData>();
+            LoadFromPlayerPrefs();
         }
 
         private void OnEnable() {
@@ -40,7 +52,25 @@ namespace Game {
                 score = _currentScore.value.ToString()
             };
             Debug.Log($"new record {newRecord.date} {newRecord.score}");
-            _savedData.Add(newRecord);
+            _savedDatas.Add(newRecord);
+
+            SaveToPlayerPrefs();
+        }
+
+        private void LoadFromPlayerPrefs() {
+            if (PlayerPrefs.HasKey(RECOREDS_KEY))
+                return;
+
+            var wrapper = JsonUtility.FromJson<SavedDataWrapper>(PlayerPrefs.GetString(RECOREDS_KEY));
+            _savedDatas = wrapper.saveDatas;
+        }
+
+        private void SaveToPlayerPrefs() {
+            var wrapper = new SavedDataWrapper {
+                saveDatas = _savedDatas
+            };
+            var json = JsonUtility.ToJson(wrapper);
+            PlayerPrefs.SetString(RECOREDS_KEY, json);
         }
 
     }
