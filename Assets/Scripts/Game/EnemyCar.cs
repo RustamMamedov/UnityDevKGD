2c1f285983105411485b14a194a488d1171253f0
Andrey <DrEMartstud@gmail.com>
2020-11-03 16:32:28 +0200

    Added empty line

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -54,6 +54,7 @@ namespace Game {
             AddScore();
         }
         
+        
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carTriggerEventDispatcher.Dispatch();
