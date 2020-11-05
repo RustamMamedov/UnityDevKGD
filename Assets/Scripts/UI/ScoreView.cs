995f1fb3a85b2890f81aecd96b94706d3ff5c9e8
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-05 19:08:34 +0200

    reset score

--- Assets/Scripts/UI/ScoreView.cs
+++ Assets/Scripts/UI/ScoreView.cs
@@ -11,24 +11,30 @@ namespace UI {
         private EventListeners _eventListeners;
         [SerializeField]
         private ScriptableIntValue _currentScore;
-        private int _currentscore;
+        private int _currentscore = 0;
         [SerializeField]
         private Text _lableValue;
 
-        public void Awake() {
+        private void Awake() {
             _eventListeners.OnEventHappened += UpdateBehaviour;
         }
-        public void UpdateBehaviour() { 
+        private void UpdateBehaviour() { 
           if(_currentScore.value > _currentscore) {
                 StartCoroutine(SetScoreCoroutine(_currentScore.value));
             }
         }
-        public IEnumerator SetScoreCoroutine(int score) {
+        private IEnumerator SetScoreCoroutine(int score) {
             while (score > _currentscore) {
                 _currentscore ++;
                 _lableValue.text = $"{_currentscore}"; 
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.01f);
             }  
         }
+        private void OnEnable() {
+            _currentscore = 0;
+            _currentScore.value = 0;
+            _lableValue.text = $"{_currentscore}";
+        }
     }
+
 }
