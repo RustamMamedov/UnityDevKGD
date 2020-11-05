8c90ce2e26c435f5f8b1c9044a5a7ebfb37def12
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-05 18:46:57 +0200

    Make records update dynamically

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Game;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI {
@@ -10,11 +11,33 @@ namespace UI {
         [SerializeField]
         private Button _menuButton;
 
+        [SerializeField]
+        private GameObject _recordsList;
+
+        [SerializeField]
+        private GameObject _recordViewPrefab;
+
 
         // Life cycle.
 
         private void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnEnable() {
+            var records = SaveManager.Instance.Records;
+            int distinguishedIndex = SaveManager.Instance.NewRecordIndex;
+            for (int i = 0; i < records.Length; ++i) {
+                var record = records[i];
+                var recordView = Instantiate(_recordViewPrefab, _recordsList.transform).GetComponent<RecordView>();
+                recordView.SetData(i + 1, record.DateTime, record.Score, i == distinguishedIndex);
+            }
+        }
+
+        private void OnDisable() {
+            for (int i = 0; i < _recordsList.transform.childCount; ++i) {
+                Destroy(_recordsList.transform.GetChild(i).gameObject);
+            }
         }
 
 
