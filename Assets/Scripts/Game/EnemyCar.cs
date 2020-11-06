f125bebb4884bd416de464175ec7fbacc5a9a88e
Dan <kdanil01@mail.ru>
2020-11-06 23:20:13 +0200

    Added the function to count the dodge score

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -7,9 +7,23 @@ namespace Game{
         [SerializeField]
         private EventDispatcher _enemyCarsTriggerEventDispatch;
 
+        [SerializeField]
+        private EventDispatcher _enemyCarsBackTriggerEventDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _dodgedScore;
+
         private void OnTriggerEnter(Collider other){
             if (other.CompareTag("Player")) {
                 _enemyCarsTriggerEventDispatch.Dispatch();
+            }
+        }
+        
+        private void OnTriggerExit(Collider other) {
+            if (other.CompareTag("CarBack")) {
+                _dodgedScore.value = _carSettings.dodgeScore;
+                _enemyCarsBackTriggerEventDispatcher.Dispatch();
+                Debug.Log("erer");
             }
         }
     }
