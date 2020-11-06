6b2d668a670ae3dbe4b555fb07890ebb87dcbc83
Dan <kdanil01@mail.ru>
2020-11-06 23:37:54 +0200

    Created Gamescreen script to abstract Leaderboard summoning

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,19 @@
+﻿using Events;
+using UnityEngine;
+
+namespace UI {
+
+    public class GameScreen : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        private void Awake() {
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            UIManager.Instance.ShowLeaderboardScreen();
+        }
+    }
+}
