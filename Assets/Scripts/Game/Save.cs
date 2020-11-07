835cbcde93d33eacdf9a6ace630b2023daf0fe8f
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-07 21:59:11 +0200

    Saves (PlayerPrefs)

---
+++ Assets/Scripts/Game/Save.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+namespace Game {
+
+    public class Save : MonoBehaviour {
+
+        [Serializable]
+        public class SaveData {
+
+            public string date;
+            public string score;
+        }
+
+        [Serializable]
+        private class SavedDataWrapper {
+
+            public List<SaveData> savedData;
+        }
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScore;
+
+        private List<SaveData> _savedData;
+        public List<SaveData> SavedData => _savedData;
+
+        private const string RECORDS_KEY = "records";
+
+        private void Awake() {
+            _savedData = new List<SaveData>();
+            LoadFromPlayerPrefs();
+        }
+
+        private void OnEnable() {
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+
+        private void OnDisable() {
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            var newRecord = new SaveData {
+                date = DateTime.Now.ToString("MM/dd/yyyy HH:mm"),
+                score = _currentScore.value.ToString()
+            };
+
+            _savedData.Add(newRecord);
+            SaveToPlayerPrefs();
+        }
+
+        private void LoadFromPlayerPrefs() {
+            if (!PlayerPrefs.HasKey(RECORDS_KEY)) {
+                return;
+            }
+
+            var wrapper = JsonUtility.FromJson<SavedDataWrapper>(PlayerPrefs.GetString(RECORDS_KEY));
+            _savedData = wrapper.savedData;
+        }
+
+        private void SaveToPlayerPrefs() {
+            var wrapper = new SavedDataWrapper {
+                savedData = _savedData
+            };
+            var json = JsonUtility.ToJson(wrapper);
+            PlayerPrefs.SetString(RECORDS_KEY, json);
+        }
+    }
+}
