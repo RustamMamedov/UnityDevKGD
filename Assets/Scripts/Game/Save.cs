0a7fa9d1a72765fd0e650564569bafa3d76d6790
Maxim <korunets1901@gmail.com>
2020-11-07 11:36:37 +0300

    Create Save script

---
+++ Assets/Scripts/Game/Save.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+    public class Save : MonoBehaviour {
+
+        public class SaveData {
+            public string date;
+            public string score;
+        }
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScore;
+
+        private List<SaveData> _savedData;
+
+        private void Awake() {
+            _savedData = new List<SaveData>();
+        }
+
+        private void OnEnable() {
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+
+        private void OnDisable() {
+             _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+
+        private void OnCarCollision() {
+            var newRecord = new SaveData {
+                date = DateTime.Now.ToString("MM/dd/yyyy HH:mm"),
+                score = _currentScore.value.ToString()
+            };
+            Debug.Log($"new record: {newRecord.date} {newRecord.score}");
+            _savedData.Add(newRecord);
+        }
+    }
+}
+
