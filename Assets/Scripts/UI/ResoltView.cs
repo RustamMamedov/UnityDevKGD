5aa5e3aa2b083be7b903f8c4d73cbd5403117a1f
TechesDePainture <kirillzelen00@gmail.com>
2020-11-08 16:38:48 +0200

    ResoltView.cs created

---
+++ Assets/Scripts/UI/ResoltView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class ResoltView : MonoBehaviour {
+        [SerializeField]
+        private Text _position;
+
+        [SerializeField]
+        private Text _date;
+
+        [SerializeField]
+        private Text _score;
+
+        public void SetData(int place,string date,string score) {
+            _position.text = place.ToString();
+            _date.text = date;
+            _score.text = score;
+        }
+
+    }
+}
