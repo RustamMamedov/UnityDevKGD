528c8ec3e880d661ae70a95c38cec781b24e432a
Denis <denis.zhuravkov@gmail.com>
2020-11-08 21:32:46 +0200

    "RecordView" script created
    It's a controller for RecordView UI item on LeaderboardScreen.
    
    Also added this script to RecordView prefab

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeText;
+        
+        [SerializeField]
+        private Text  _dateText;
+        
+        [SerializeField]
+        private Text _scoreText;
+
+        public void SetData(int position, string date, int score) {
+            _placeText.text = position.ToString();
+            _dateText.text = date;
+            _scoreText.text = score.ToString();
+        }
+    }
+}
+
