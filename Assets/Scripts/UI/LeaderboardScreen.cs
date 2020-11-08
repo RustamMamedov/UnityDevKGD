8ef110f4532f669efcdadba27aff9a286e1c9d83
TechesDePainture <kirillzelen00@gmail.com>
2020-11-08 17:42:26 +0200

    Destroy best scores onDisable

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -19,14 +19,27 @@ namespace UI {
         [SerializeField]
         private GameObject _table;
 
+        private List<GameObject> _resolts;
+
         private void Awake() {
             _menuButtom.onClick.AddListener(OnMenuButtonClick);
         }
 
         private void OnEnable() {
+            if(_resolts==null)
+                _resolts = new List<GameObject>();
             for (int i = 0; i < Save.SavedDatas.Count; i++) {
                 var Resolt =Instantiate(_resoltViewPrefab,_table.gameObject.transform);
                 Resolt.SetData(i+1, Save.SavedDatas[i].date, Save.SavedDatas[i].score);
+                _resolts.Add(Resolt.gameObject);
+            }
+        }
+
+        private void OnDisable() {
+            while (_resolts.Count > 0) {
+                var tmp = _resolts[_resolts.Count - 1];
+                _resolts.RemoveAt(_resolts.Count - 1);
+                Destroy(tmp);
             }
         }
 
