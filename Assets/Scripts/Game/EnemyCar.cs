66d5f48d498276d6261661a3eb6de9c792231287
Andrey <DrEMartstud@gmail.com>
2020-11-08 17:45:47 +0200

    Small optimization fix and code refactor
    Game won't do math when it is unnecessary to free up resources

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -14,35 +14,43 @@ namespace Game {
         [SerializeField] 
         private ScriptableIntValue _currentScoreAsset;
         
-        [SerializeField]
-        private ScriptableFloatValue _playerPositionZ;
-
         [SerializeField] 
         private ScriptableFloatValue _playerPositionX;
+        
+        [SerializeField]
+        private ScriptableFloatValue _playerPositionZ;
 
         [SerializeField] 
         private ScriptableBoolValue _crazyModeEnabled;
 
         private int _distanceToDodge = 40;
         private bool _enemyIsDodged = false;
+        private bool _mathShouldBeDone = true;
 
         private void CheckIfDodged() {
-            if (_crazyModeEnabled.value) {
-                if (Mathf.Ceil(transform.position.x) == Mathf.Ceil(_playerPositionX.value)) {
-                    if (transform.position.z - _playerPositionZ.value < _distanceToDodge) _enemyIsDodged = true;
+            if (_mathShouldBeDone) {
+                if (_crazyModeEnabled.value) {
+                    if (Mathf.Ceil(transform.position.x) == Mathf.Ceil(_playerPositionX.value)) {
+                        if (transform.position.z - _playerPositionZ.value < _distanceToDodge) {
+                            _enemyIsDodged = true;
+                            _mathShouldBeDone = false;
+                        }
+                    }
                 }
-            }
-            else {
-                if (transform.position.z - _playerPositionZ.value < _distanceToDodge) {
-                    if (Mathf.Abs(Mathf.Ceil(transform.position.x - _playerPositionX.value)) == 4 || Mathf.Abs(Mathf.Ceil(_playerPositionX.value - transform.position.x)) == 4 )
-                        _enemyIsDodged = true;
+                else {
+                    if (transform.position.z - _playerPositionZ.value < _distanceToDodge) {
+                        if (Mathf.Abs(Mathf.Ceil(transform.position.x - _playerPositionX.value)) == 4 ||
+                            Mathf.Abs(Mathf.Ceil(_playerPositionX.value - transform.position.x)) == 4){
+                            _enemyIsDodged = true;
+                            _mathShouldBeDone = false;
+                        }
+                    }
                 }
             }
         }
         
         private void AddScore() {
             if(_enemyIsDodged) _currentScoreAsset.value += _carSettings.dodgeScore;
-            _enemyIsDodged = false;
         }
         
         protected override void Move() {
