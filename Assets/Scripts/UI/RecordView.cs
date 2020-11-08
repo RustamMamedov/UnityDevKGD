3e315077143ed8dc155467c913cac497ebd95440
Ilya <elya5911@mail.ru>
2020-11-08 23:51:06 +0200

    Create ResultView class

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private List<Text> _textLables;
+
+        public void SetData(int pos, string date, string score) {
+            _textLables[0].text=pos.ToString();
+            _textLables[1].text = date;
+            _textLables[2].text = score;
+        }
+    }
+}
