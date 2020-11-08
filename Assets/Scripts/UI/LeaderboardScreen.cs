b37345bb922852592b4fe325d4923594e8cf2b0d
Denis <denis.zhuravkov@gmail.com>
2020-11-08 21:36:25 +0200

    Logic of generating RecordView on LeaderboardScreen depending on Data
    Also:
    - added new refernces to LeaderBoardScreen prefab
    - removed temporary RecordView fillers

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Game;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,9 +11,57 @@ namespace UI {
 
         [SerializeField] 
         private Button _menuButton;
-        
+
+        [SerializeField] 
+        private GameObject _recordViewPrefab;
+
+        [SerializeField] 
+        private GameObject _recordsHolder;
+
+        private List<GameObject> _recordsViewList;
+
         void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnEnable() {
+            HandleRecordsData();
+        }
+
+        private void OnDisable() {
+            DestroyRecordsView();
+        }
+
+        private void DestroyRecordsView() {
+            for (int i = _recordsViewList.Count - 1; i > -1; --i) {
+                Destroy(_recordsViewList[i]);
+            }
+            
+            _recordsViewList.Clear();
+        }
+
+        private void HandleRecordsData () {
+            _recordsViewList = new List<GameObject>();
+            
+            var records = Save.SaveDatas;
+
+            if (records.Count == 0) {
+                return;
+            }
+
+            for (int i = 0; i < records.Count; ++i) {
+                GenerateRecordView(i + 1, records[i]);
+            }
+        }
+
+        private void GenerateRecordView(int position, Save.Record record) {
+            var recordViewObject = Instantiate(_recordViewPrefab, _recordsHolder.transform);
+            recordViewObject.transform.SetParent(_recordsHolder.transform);
+            
+            var recordView = recordViewObject.GetComponent<RecordView>();
+            recordView.SetData(position, record.date, record.Score);
+            
+            _recordsViewList.Add(recordViewObject);
         }
 
         private void OnDestroy() {
