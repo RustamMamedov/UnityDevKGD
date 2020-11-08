8ca058482c8234ac81860ba22dca05574267e5be
Alexandr <navacar@mail.ru>
2020-11-08 15:47:36 +0200

    Added new class RecordView and  public method SetData

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+        
+        [SerializeField]
+        private Text _placeLabel;
+        
+        [SerializeField]
+        private Text _dateLabel;
+
+        [SerializeField]
+        private Text _scoreLabel;
+
+        [SerializeField]
+        public GameObject FlowerImage;
+
+        public void SetData(int place, string date, string score) {
+            _placeLabel.text = $"{place}";
+            _dateLabel.text = $"{date}";
+            _scoreLabel.text = $"{score}";
+        }
+    }
+}
