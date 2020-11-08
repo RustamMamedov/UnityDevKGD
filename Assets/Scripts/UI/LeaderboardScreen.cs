997ba0ea3f468aa35dbdedaac20b3b02223d18f2
TechesDePainture <kirillzelen00@gmail.com>
2020-11-08 17:29:43 +0200

    To OnEnable added creation 10 best Records

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Game;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,8 +12,22 @@ namespace UI {
 
         [SerializeField]
         private Button _menuButtom;
+
+        [SerializeField]
+        private ResoltView _resoltViewPrefab;
+
+        [SerializeField]
+        private GameObject _table;
+
         private void Awake() {
             _menuButtom.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnEnable() {
+            for (int i = 0; i < Save.SavedDatas.Count; i++) {
+                var Resolt =Instantiate(_resoltViewPrefab,_table.gameObject.transform);
+                Resolt.SetData(i+1, Save.SavedDatas[i].date, Save.SavedDatas[i].score);
+            }
         }
 
         private void OnMenuButtonClick() {
