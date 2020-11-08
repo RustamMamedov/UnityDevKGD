d9ab5bf2d0c8d7d28afdcb91c1911cefb16cb666
SaraJess <sarycheva.00@mail.ru>
2020-11-08 21:35:30 +0200

    invoice display

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
+using Game;
+using System.Collections.Generic;
 
 namespace UI {
     public class LeaderboardScreen : MonoBehaviour {
@@ -10,12 +12,37 @@ namespace UI {
         [SerializeField]
         private UIManager _uimanager;
 
+        [SerializeField]
+        private RecordView _recordView;
+
+        [SerializeField]
+        private GameObject _table;
+
+        private List<GameObject> _saveRecord;
+
         private void OnMenuButtonClick() {
             _uimanager.LoadMenu();
         }
 
         private void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
+            _saveRecord = new List<GameObject>();
+        }
+
+        private void OnEnable() {
+            for (var i = 0; Save.SavedDatas.Count > i; i++) {
+                var Record = Instantiate(_recordView, _table.transform);
+                Record.SetData(i+1, Save.SavedDatas[i].date, Save.SavedDatas[i].score);
+                _saveRecord.Add(Record.gameObject);
+            }
+        }
+
+        private void OnDisable() {
+            for (var i = 0; _saveRecord.Count > i; i++) {
+                var temp = _saveRecord[i];
+                _saveRecord.RemoveAt(i);
+                Destroy(temp);
+            }
         }
     }
 }
