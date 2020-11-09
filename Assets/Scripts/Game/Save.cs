7d3b5cb41bdd2ef4ea6bd334f9abd9e4487e6306
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-09 15:43:30 +0200

    save script (some data offline)

---
+++ Assets/Scripts/Game/Save.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+using System;
+
+namespace Game {
+    public class Save : MonoBehaviour {
+
+        [SerializeField]
+        public class SaveData {
+            public string date;
+            public string score;
+        }
+
+        [SerializeField]
+        private class SavedDataWrapper {
+            public List<SaveData> saveDatas;
+        }
+
+        [SerializeField]
+        private EventListeners _carCollisionEventListener;
+
+        [SerializeField]
+        private ScriptableIntValue _currentScore;
+
+        private List<SaveData> _saveDatas;
+        public List<SaveData> SavedDatas => _saveDatas;
+        private const string RECORDS_KEY = "records";
+
+        private void Awake() {
+            _saveDatas = new List<SaveData>();
+            LoadFromPlayerPrefs();
+        }
+        private void OnEnable() {
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+        private void OnDisable() {
+            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+        }
+        private void OnCarCollision() {
+            var newRecord = new SaveData {
+                date = DateTime.Now.ToString("MM/dd/yyyy HH:mm"),
+                score = _currentScore.value.ToString()
+            };
+            Debug.Log($"new record:{newRecord.date} {newRecord.score}");
+            _saveDatas.Add(newRecord);
+            SaveToPlayerPrefs();
+        }
+
+        private void LoadFromPlayerPrefs() {
+            if (!PlayerPrefs.HasKey(RECORDS_KEY)) {
+                return;
+            }
+            var wrapper = JsonUtility.FromJson<SavedDataWrapper>(PlayerPrefs.GetString(RECORDS_KEY));
+            _saveDatas = wrapper.saveDatas;
+        }
+        private void SaveToPlayerPrefs() {
+            var wrapper = new SavedDataWrapper {
+                saveDatas = _saveDatas
+            };
+            var json = JsonUtility.ToJson(wrapper);
+            PlayerPrefs.SetString(RECORDS_KEY, json);
+        }
+    }
+}
