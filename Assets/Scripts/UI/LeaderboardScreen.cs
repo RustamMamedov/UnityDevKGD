01a89a1f42b0d7bdff9387017b285818673c036e
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-09 17:56:53 +0200

    changed logic on leaderboard

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Game;
 
 namespace UI {
     public class LeaderboardScreen : MonoBehaviour {
@@ -9,9 +10,51 @@ namespace UI {
         [SerializeField]
         private Button _menuButton;
 
+        [SerializeField]
+        private GameObject _leaderboardTable;
+
+        [SerializeField]
+        private GameObject _resultView;
+
         private void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
         }
+
+        private void OnEnable() {
+            UpdateRecords();
+        }
+
+        private void OnDisable() {
+            ClearRecords();
+        }
+
+        private void UpdateRecords() {
+            var place = 1;
+
+            if (Save.SavedDatas != null) {
+                foreach (Save.SaveData save in Save.SavedDatas) {
+                    var recordView = Instantiate(_resultView, _leaderboardTable.transform);
+                    var recordViewComponent = recordView.GetComponent<RecordView>();
+                    recordViewComponent.SetData(place, save.date, save.score);
+
+                    if (save.newScore) {
+                        recordViewComponent.MarkRecord();
+                    } else {
+                        recordViewComponent.UnmarkRecord();
+                    }
+                    place++;
+                }
+            }
+        }
+
+        private void ClearRecords() {
+            for (int i = _leaderboardTable.transform.childCount - 1; i >= 0; i--) {
+                var recordView = _leaderboardTable.transform.GetChild(i);
+                recordView.SetParent(null, false);
+                Destroy(recordView.gameObject);
+            }
+        }
+
         private void OnMenuButtonClick() {
             UIManager.Instance.LoadMenu();
         }
