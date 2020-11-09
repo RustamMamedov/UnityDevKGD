bab5c1b3c76e47988e22d786fd68c99d8169291c
Andrey <stanok99@bk.ru>
2020-11-09 09:08:13 +0200

    Implementation OnEnable and OnDisable

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Game;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI {
@@ -8,8 +9,28 @@ namespace UI {
         [SerializeField]
         private Button _menuButton;
 
+        [SerializeField]
+        private GameObject _bestRideRecord;
+
+        [SerializeField]
+        private Transform _records;
+
         private void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnEnable() {
+            for (int i = 0; i < Save.SavedDatas.Count; i++) {
+                var record = Save.SavedDatas[i];
+                Instantiate(_bestRideRecord, _records).GetComponent<RecordView>().SetData(i + 1, record.date, record.score, record.newRec);
+            }
+        }
+
+        private void OnDisable() {
+            for (int i = 0; i < Save.SavedDatas.Count; i++) {
+                Save.SavedDatas[i].newRec = false;
+                Destroy(_records.GetChild(i).gameObject);
+            }
         }
 
         private void OnMenuButtonClick() {
