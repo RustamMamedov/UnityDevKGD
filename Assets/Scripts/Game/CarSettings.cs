8bf831d82deb5933c37d250e737c8f303ca35c37
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-09 20:06:39 +0200

    Add field headers in inspector for CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -8,10 +8,11 @@ namespace Game {
     [CreateAssetMenu(fileName="CarSettings", menuName="Game/CarSettings")]
     public class CarSettings : ScriptableObject {
 
+        [Header("Score settings")]
         public int dodgeScore;
 
+        [Header("Motion settings")]
         public float maxSpeed;
-
         public float acceleration;
         
 
