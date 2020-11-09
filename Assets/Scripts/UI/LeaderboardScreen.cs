bc08d291ff0e8bdf9adfbb5c54c60d45981d1809
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-09 15:58:15 +0200

    Leaderboard: update and clear records

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Game;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI {
@@ -8,8 +9,43 @@ namespace UI {
         [SerializeField]
         private Button _menuButton;
 
+        [SerializeField]
+        private GameObject _leaderboardTable;
+
+        [SerializeField]
+        private GameObject _resultView;
+
         private void Awake() {
             _menuButton.onClick.AddListener(OnClickMenuButton);
+        }
+
+        private void OnEnable() {
+            UpdateRecords();
+        }
+
+        private void OnDisable() {
+            ClearRecords();
+        }
+
+        private void UpdateRecords() {
+            var place = 1;
+
+            if (Save.SavedData != null) {
+                foreach (Save.SaveData save in Save.SavedData) {
+                    var recordView = Instantiate(_resultView, _leaderboardTable.transform);
+                    recordView.GetComponent<RecordView>().SetData(place, save.date, save.score);
+
+                    place++;
+                }
+            }
+        }
+
+        private void ClearRecords() {
+            for (int i = _leaderboardTable.transform.childCount - 1; i >= 0; i--) {
+                var recordView = (_leaderboardTable.transform.GetChild(i));
+                recordView.SetParent(null, false);
+                Destroy(recordView.gameObject);
+            }
         }
 
         private void OnClickMenuButton() {
