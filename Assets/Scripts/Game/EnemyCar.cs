72df9bfcd27a8004c67dc6f68ba25fd26df46510
Alex Osi <noemail>
2020-11-09 20:31:40 +0500

    EnemyCar script changed

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -1,14 +1,15 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using Events;
 
 namespace Game {
     public class EnemyCar : Car {
+
         [SerializeField]
         private EventDispatcher _carCollisionEventDispatcher;
 
         [SerializeField]
         private ScriptableIntValue _currentScore;
-
         [SerializeField]
         private ScriptableFloatValue _playerPositionZ;
 
@@ -17,7 +18,6 @@ namespace Game {
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carCollisionEventDispatcher.Dispatch();
-                Debug.Log("CarCollision with " + transform.name);
             }
         }
 
@@ -25,9 +25,11 @@ namespace Game {
             if (_playerPositionZ.value >= gameObject.transform.position.z && !_dodged) {
                 _dodged = true;
                 _currentScore.value += _carSettings.dodgeScore;
+
             }
             base.UpdateBehaviour();
         }
+
     }
 }
 
