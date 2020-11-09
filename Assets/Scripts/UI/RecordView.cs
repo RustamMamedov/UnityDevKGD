0730e2c5382325dfd446b4e8c428f1321c1268e4
Oleg <oleg.kalyugin2001@mail.ru>
2020-11-09 16:56:15 +0200

    RecordView scripts

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordView : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bestRideRecords;
+
+    [SerializeField]
+    private GameObject _recordView;
+
+
+
+    public void SetData(int place,string date, string score) {
+        var records = _bestRideRecords;
+        records.transform.GetChild(0).GetComponent<Text>().text = place.ToString();
+        records.transform.GetChild(1).GetComponent<Text>().text = date;
+        records.transform.GetChild(2).GetComponent<Text>().text = score;
+        
+        Instantiate(records,_recordView.transform);
+        
+    }
+}
