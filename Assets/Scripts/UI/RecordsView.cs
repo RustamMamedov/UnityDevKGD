60266b596186861b9cd07fcde076b3332ffdf95b
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-09 17:36:32 +0200

    RecordView script added

---
+++ Assets/Scripts/UI/RecordsView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordsView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _score, _data, _place;
+
+        public void SetData(int place, string date, string score) {
+
+            _data.text = date;
+            _score.text = score;
+            _place.text = place.ToString();
+
+        }
+    }
+}
+
