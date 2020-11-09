8b337f860f50816043787d5703ebce3e55e043ad
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-09 17:55:45 +0200

    created recordView + logic

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _place;
+
+        [SerializeField]
+        private Text _date;
+
+        [SerializeField]
+        private Text _score;
+
+        [SerializeField]
+        private Image _imageNewScore;
+
+        public void SetData(int place, string date, string score) {
+            _place.text = place.ToString();
+            _date.text = date;
+            _score.text = score;
+        }
+
+        public void MarkRecord() {
+            _imageNewScore.gameObject.SetActive(true);
+        }
+
+        public void UnmarkRecord() {
+            _imageNewScore.gameObject.SetActive(false);
+        }
+    }
+}
