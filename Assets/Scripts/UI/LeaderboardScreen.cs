f62cddda34c37a8313436ce943ac53e12f742bfa
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-09 17:44:19 +0200

    Added OnEnable, OnDisable methods to LeaderboardScreen script

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
+using Game;
+using System;
 
 namespace UI {
 
@@ -8,9 +11,58 @@ namespace UI {
         [SerializeField]
         private Button _menuButton;
 
+        [SerializeField]
+        private GameObject _scorePrefab;
+
+        private Save _saveObject;
+        private List<GameObject> _scores;
+
+       
+
+
         private void Awake() {
 
             _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnEnable() {
+            _saveObject = GameObject.FindGameObjectWithTag("Save").GetComponent<Save>();
+            var listForCreatingScoreTable = SortList(_saveObject);
+            for (int i = 0; i < listForCreatingScoreTable.Count; i++) {
+
+                var resultBlank = Instantiate(_scorePrefab);
+                resultBlank.GetComponent<RecordsView>().SetData(i+1,listForCreatingScoreTable[i].data,listForCreatingScoreTable[i].score);
+                _scores.Add(resultBlank);
+            }
+        }
+
+        private List<Save.SaveData>  SortList(Save save) {
+            var sortedList = new List<Save.SaveData>();
+            sortedList = save.SavedDatas;
+            for (int i = 0; i < sortedList.Count-1; i++) {
+                for (int j = 1; j < sortedList.Count; j++) {
+                    if (Int32.Parse(sortedList[i].score) < Int32.Parse(sortedList[j].score)) {
+
+                        var scoreCash = sortedList[i].score;
+                        var dataCash = sortedList[i].data;
+                        sortedList[i].score = sortedList[j].score;
+                        sortedList[i].data = sortedList[j].data;
+                        sortedList[j].score = scoreCash;
+                        sortedList[j].data = dataCash;
+
+                    }
+                }
+            }
+            return sortedList;
+        }
+
+        private void OnDisable() {
+
+            for (int i = _scores.Count - 1; i < 0; i--) {
+
+                Destroy(_scores[i]);
+                _scores.RemoveAt(i);
+            }
         }
 
         private void OnMenuButtonClick() {
