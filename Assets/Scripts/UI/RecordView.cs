e1b8c2da00f139f5143da20964e6848958a7c342
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-09 15:55:57 +0200

    RecordView class created and added to ResultView prefab

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _scoreLabel;
+
+        [SerializeField]
+        private Text _dateLabel;
+
+        [SerializeField]
+        private Text _placeLabel;
+
+        public void SetData(int place, string date, string score) {
+            _placeLabel.text = place.ToString();
+            _dateLabel.text = date;
+            _scoreLabel.text = score;
+        }
+    }
+
+}
