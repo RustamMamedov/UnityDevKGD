52edaa1405422ff7080766f29b14de72f37f1752
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-09 20:00:19 +0200

    Fix text in record views

--- Assets/Scripts/UI/RecordView.cs
+++ Assets/Scripts/UI/RecordView.cs
@@ -25,7 +25,7 @@ namespace UI {
 
         public void SetData(int position, DateTime dateTime, int score, bool distinguish = false) {
             _positionText.text = position.ToString();
-            _dateText.text = dateTime.ToString("dd.MM.yyyy HH.mm");
+            _dateText.text = dateTime.ToString("dd.MM.yyyy HH:mm");
             _scoreText.text = score.ToString();
             _distinguishedObject.SetActive(distinguish);
         }
