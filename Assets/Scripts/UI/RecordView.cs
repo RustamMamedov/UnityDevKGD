d842bc1d1174195725482596571647a591ba1af0
Andrey <DrEMartstud@gmail.com>
2020-11-09 11:48:42 +0200

    Created RecordView and added class to prefab

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField] 
+        private Text _placeNumberLabel;
+        
+        [SerializeField] 
+        private Text _dateLabel; 
+        
+        [SerializeField] 
+        private Text _scoreNumberLabel;
+        
+        [SerializeField] 
+        private GameObject _currentScoreHighlighter;
+
+        public void SetData(int place, string date, string score, bool isCurrent) {
+            _placeNumberLabel.text = place.ToString();
+            _dateLabel.text = date;
+            _scoreNumberLabel.text = score;
+            _currentScoreHighlighter.SetActive(isCurrent);
+        }
+    }
+}
