0f9a770d0224a7010c1683fca093dcd87bb9533a
Potapenko_Ivan <van.van123@list.ru>
2020-11-09 04:58:24 +0200

    LeaderbordScreen changed

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -2,16 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using Game;
 
 namespace UI {
     public class LeaderboardScreen : MonoBehaviour {
 
         [SerializeField]
         private Button _menuButton;
+        [SerializeField]
+        private GameObject _bestRideRecordPrefab;
+
+        [SerializeField]
+        private GameObject _recordsGameObject;
+
+        [SerializeField]
+        private ScriptableIntValue _playerScorePosition;
+
+        [SerializeField]
+        private List<GameObject> _bestRideRecord;
 
         private void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnEnable() {
+            AddBestRideRecordPrefab();
+        }
+
+        private void dis() {
+            AddBestRideRecordPrefab();
+        }
+
+
+        private void AddBestRideRecordPrefab() {
+            if (_bestRideRecord == null) {
+                _bestRideRecord = new List<GameObject>();
+            }
+            else {
+                for (int i = 0; i < _bestRideRecord.Count; i++) {
+                    Destroy(_bestRideRecord[i].gameObject);
+                }
+                _bestRideRecord.Clear();
+            }
+            var recordsList = Save.SavedDatas;
+            for (int i = 0; i < recordsList.Count; i++) {
+
+                _bestRideRecord.Add(Instantiate(_bestRideRecordPrefab, _recordsGameObject.transform));
+
+                var recordView = _bestRideRecord[i].GetComponent<RecordView>();
+                recordView.SetData(i + 1, recordsList[i].date, recordsList[i].score);
+
+                if (_playerScorePosition.value == i) {
+                    recordView.ShowFlower();
+                }
+            }
+
         }
 
         private void OnMenuButtonClick() {
