4c9bdd0230f470f6fd9d9ee7df98520227580a2c
Alex Osi <noemail>
2020-11-09 20:29:30 +0500

    RecordView script added

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeText;
+
+        [SerializeField]
+        private Text _dateText;
+
+        [SerializeField]
+        private Text _scoreText;
+
+        [SerializeField]
+        private GameObject _flower;
+
+        public void SetData(int place, string date, string score) {
+            _placeText.text = place.ToString();
+            _dateText.text = date;
+            _scoreText.text = score;
+        }
+        public void ShowFlower() {
+            _flower.SetActive(true);
+        }
+    }
+
+}
