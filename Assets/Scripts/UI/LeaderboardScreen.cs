7214fd438f9cc07206a1a24afc7fbcd55746fcca
Nikita <Nkanunov@gmail.com>
2020-11-09 17:06:37 +0200

    Added Record prefab in Leaderboarb

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,17 +1,41 @@
-﻿using UI;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using Game;
 
-public class LeaderboardScreen : MonoBehaviour {
-   
-   [SerializeField]
-   private Button _menuButton;
+namespace UI {
+    public class LeaderboardScreen : MonoBehaviour {
 
-   private void Awake() {
-      _menuButton.onClick.AddListener(OnPlayButtonClick);
-   }
+        [SerializeField]
+        private Button _menuButton;
 
-   private void OnPlayButtonClick() {
-      UIManager.Instance.LoadMenu();
-   }
+        [SerializeField]
+        private GameObject _resultViewPrefab;
+
+        [SerializeField]
+        private Transform _resultsKeeper;
+
+        private int _recordCount;
+
+        private void OnEnable() {
+            _recordCount = Save.SaveDatas.Count;
+            for (int i = 0; i < _recordCount; i++) {
+                var tmp = Instantiate(_resultViewPrefab, _resultsKeeper);
+                tmp.GetComponent<RecordView>().SetData(i + 1, Save.SaveDatas[i].date, Save.SaveDatas[i].score, i + 1 == Save.CurrentRecordPos);
+            }
+        }
+
+        private void OnDisable() {
+            for (int i = 0; i < _recordCount; i++) {
+                Destroy(_resultsKeeper.GetChild(i).gameObject);
+            }
+        }
+
+        private void Awake() {
+            _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnMenuButtonClick() {
+            UIManager.Instance.LoadMenu();
+        }
+    }
 }
