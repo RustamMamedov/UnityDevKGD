26a69e9e5e2046937144eb5e36271c541eb7a8a2
Ilya <elya5911@mail.ru>
2020-11-09 21:01:56 +0200

    Create EnemyCarHandle.cs

---
+++ Assets/Scripts/Editor/EnemyCarHandle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Game;
+
+namespace GameEditor {
+
+    [CustomEditor(typeof(EnemyCar))]
+    public class EnemyCarHandle : Editor {
+
+        private EnemyCar _enemyCar;
+
+        private void OnEnable() {
+            _enemyCar = (EnemyCar)target;
+        }
+
+        private void OnSceneGUI() {
+            
+            if (Event.current.type == EventType.Repaint) {
+                Handles.color = Color.red;
+
+                
+                var angle = 45f;
+                var rotation = Quaternion.Euler(0f, -angle / 2f, 0f);
+                Handles.DrawSolidArc(_enemyCar.transform.position, Vector3.up, rotation * Vector3.forward, angle, 5f);
+
+                _enemyCar.GetCarSettings();
+                Handles.Label(_enemyCar.transform.position + Vector3.up * 5f, "Hi");
+            }
+        }
+    }
+}
