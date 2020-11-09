c007fb2aa84b45243fdb5c1e041f1a211a09b96b
vladislav <vladislavnikwork@gmail.com>
2020-11-09 05:02:41 +0200

    Controler RecordView cteated and added into ResultView prefab

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeT;
+
+        [SerializeField]
+        private Text _dateT;
+
+        [SerializeField]
+        private Text _scoreT;
+
+        public void SetData(int position, string date, string score) {
+            _placeT.text = position.ToString();
+            _dateT.text = date;
+            _scoreT.text = score;
+        }
+    }
+}
