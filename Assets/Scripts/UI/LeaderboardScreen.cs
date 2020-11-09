5905da2ba03a980a84604593e3e09824b8c4d1b1
Oleg <oleg.kalyugin2001@mail.ru>
2020-11-09 16:55:31 +0200

    GameScreen and LeaderboardScreen scripts

---
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -0,0 +1,46 @@
+﻿using Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class LeaderboardScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _menuButton;
+
+        [SerializeField]
+        private RecordView recordView;
+
+
+
+        private void Awake() {
+            _menuButton.onClick.AddListener(OnMenuButtonClick);
+            
+            
+        }
+
+        private void OnEnable() {
+            var savesDate = Save.SavedDatas;
+            for (int i = 0; i < savesDate.Count; i++) {
+                recordView.SetData(i,savesDate[i].date, savesDate[i].score);
+            }
+            
+        }
+        private void OnDisable() {
+            int i = recordView.transform.GetChildCount();
+            while (--i >= 0) {
+                
+                Destroy(recordView.transform.GetChild(i).gameObject);
+                
+            }
+        }
+
+        private void OnMenuButtonClick() {
+            
+            UIManager.Instance.LoadMenu();
+            
+
+        }
+    }
+}
