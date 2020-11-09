90b91512f15e3b953a90086d1e49d9e3d22dfdfc
BepeckoB <BepeckoB@gmail.com>
2020-11-10 00:22:39 +0200

    created RecordView script

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeLabel;
+
+        [SerializeField]
+        private Text _dateLabel;
+
+        [SerializeField]
+        private Text _scoreLabel;
+
+        public void SetData(int place, string date, string score) {
+            _placeLabel.text = place.ToString();
+            _dateLabel.text = date;
+            _scoreLabel.text = score;
+        }
+    }
+}
