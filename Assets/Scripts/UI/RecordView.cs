490a91a6caa226c98bde3c2d30f53c5c9ce7725e
gg <71404862+gqggqg@users.noreply.github.com>
2020-11-09 17:45:48 +0200

    RecordView created

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeLabel;
+
+        [SerializeField]
+        private Text _dataLabel;
+
+        [SerializeField]
+        private Text _scoreLabel;
+
+        public void SetData(int place, string data, string score) {
+            _placeLabel.text = place.ToString();
+            _dataLabel.text = data;
+            _scoreLabel.text = score;
+        }
+    }
+}
