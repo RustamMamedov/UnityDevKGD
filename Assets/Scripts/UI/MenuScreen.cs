d3c61977b70d25b3c79fef8e78802661c5a6e62b
Oleg <oleg.kalyugin2001@mail.ru>
2020-11-09 16:55:53 +0200

    MenuScreen scripts

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -1,34 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 namespace UI {
 
     public class MenuScreen : MonoBehaviour {
-        [SerializeField]
-        private Button _button;
-
-    
 
         [SerializeField]
-        private UIManager _uiManager;
-
-     
+        private Button _playButton;
 
         private void Awake() {
-            _button.onClick.AddListener(OnPlayButtonClick);
-
+            _playButton.onClick.AddListener(OnPlayButtonClick);
         }
-
-      
 
         private void OnPlayButtonClick() {
-            _uiManager.LoadGameplay();
-            
+            UIManager.Instance.LoadGameplay();
         }
-
-
     }
 }
