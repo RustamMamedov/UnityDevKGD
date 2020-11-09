60a688cb420d24ec4a718224432cfae8e7257ee0
Nikita <Nkanunov@gmail.com>
2020-11-09 16:17:25 +0200

    Created RecordView script

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeLabel;
+
+        [SerializeField]
+        private Text _dateLabel;
+
+        [SerializeField]
+        private Text _scoreLabel;
+
+        [SerializeField]
+        private Image _flower;
+        public void SetData(int place, string date, string score, bool isNewRecord) {
+            _placeLabel.text = $"{place}";
+            _dateLabel.text = date;
+            _scoreLabel.text = score;
+            _flower.enabled = isNewRecord;
+        }
+    }
+}
