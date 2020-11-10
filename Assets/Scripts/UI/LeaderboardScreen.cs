92f85b19b95841665b114c87b575554cc2773b02
Rfnt <kvotua.job@gmail.com>
2020-11-10 21:02:07 +0200

    onenable add

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -3,21 +3,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using UI;
 using Game;
+using System.Collections.Generic;
 
-namespace UI {
-    public class LeaderboardScreen : MonoBehaviour {
+public class LeaderboardScreen : MonoBehaviour {
+    [SerializeField]
+    private Button _menuButton;
 
-        [SerializeField]
-        private Button _menuButton;
+    [SerializeField]
+    private GameObject _results;
 
-        private void Awake() {
-            _menuButton.onClick.AddListener(OnMenuButtonClick);
+    [SerializeField]
+    private GameObject _resultView;
 
+    private void Awake() {
+        _menuButton.onClick.AddListener(OnMenuButtonClick);
+    }
+
+    private void OnEnable() {
+        InitializeLeaderboard();
+    }
+    public void OnMenuButtonClick() {
+        UIManager.Instance.LoadMenu();
+    }
+
+    private void InitializeLeaderboard() {
+        List<Save.SaveData> saves = Save.SavedDatas;
+        for (int i = 0; i < saves.Count; i++) {
+            GameObject newResultView = Instantiate(_resultView, _results.transform);
+
+            if (saves[i].isHighlighted) {
+                newResultView.transform.GetChild(0).gameObject.SetActive(true);
+            }
+
+            var recordView = newResultView.GetComponent<RecordView>();
+            recordView.SetData(i + 1, saves[i].date, saves[i].score);
         }
-
-        public void OnMenuButtonClick() {
-            UIManager.Instance.LoadMenu();
-        }
-
     }
 }
