a6068153974d6d5bb58bc2853b7e5d02d1f7e45f
Dan <kdanil01@mail.ru>
2020-11-10 15:26:33 +0200

    LeaderboardScreen now loads saves

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Game;
+using System.Collections.Generic;
 
 namespace UI {
     
@@ -8,12 +10,44 @@ namespace UI {
         [SerializeField]
         private Button _menuButton;
 
+        [SerializeField]
+        private GameObject _resultViewPrefab;
+
+        [SerializeField]
+        private GameObject _playersField;
+
+        private List<GameObject> _scoreBoard = new List<GameObject>{};
+
         private void Awake() {
             _menuButton.onClick.AddListener(OnMenuButtonClick);
         }
 
+        private void OnEnable() {
+            var saveDatas = Save.SavedDatas;
+            int place = 1;
+            for (int i = saveDatas.Count - 1; i >= 0; i--) {
+                var resultView = Instantiate(_resultViewPrefab, _playersField.transform);
+                resultView.GetComponent<ResultView>().SetData(place, saveDatas[i].date, saveDatas[i].score);
+
+                if (Save.CurrentPlace == i) {
+                    resultView.GetComponent<ResultView>().SetNewScoreImage();
+                }
+                
+                _scoreBoard.Add(resultView);
+                place++;
+            }
+        }
+
+        private void OnDisable() {
+            for (int i = _scoreBoard.Count - 1; i >= 0; i--) {
+                Destroy(_scoreBoard[i]);
+                _scoreBoard.RemoveAt(i);
+            }
+        }
+
         private void OnMenuButtonClick() {
             UIManager.Instance.LoadMenu();
+            _menuButton.onClick.RemoveAllListeners();
         }
     }
 }
