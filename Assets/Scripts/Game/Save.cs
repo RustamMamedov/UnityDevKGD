5ff0b4044c57f9579e92f397a1c240167f41052b
Rfnt <kvotua.job@gmail.com>
2020-11-10 19:10:20 +0200

    add from save ten liders

--- Assets/Scripts/Game/Save.cs
+++ Assets/Scripts/Game/Save.cs
@@ -1,24 +1,36 @@
-﻿using System;
+﻿using Events;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
-using Events;
+using UI;
 
 namespace Game {
+
     public class Save : MonoBehaviour {
 
-        [SerializeField]
+        public static Save Instance;
+
+        [Serializable]
         public class SaveData {
 
             public string date;
             public string score;
+            public bool isHighlighted;
+
         }
 
-        [SerializeField]
+        [Serializable]
         private class SavedDataWrapper {
 
             public List<SaveData> saveDatas;
+        }
+
+        private enum SaveType {
+
+            PlayerPrefs,
+            File
         }
 
         [SerializeField]
@@ -27,36 +39,85 @@ namespace Game {
         [SerializeField]
         private ScriptableIntValue _currentScore;
 
-        private List<SaveData> _saveDatas;
-        public List<SaveData> SavedDatas => _saveDatas;
+        [SerializeField]
+        private SaveType _saveType;
+
+        private static List<SaveData> _saveDatas;
+        public static List<SaveData> SavedDatas => _saveDatas;
 
         private const string RECORDS_KEY = "records";
-        private string _filepath;
+        private string _filePath;
 
         private void Awake() {
             _saveDatas = new List<SaveData>();
-            _filepath = Path.Combine(Application.persistentDataPath, "data.txt");
-            // LoadFromPlayerPrefs();
-            LoadFromFile();
-        }
-        private void OnEnable() {
-            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+            _filePath = Path.Combine(Application.persistentDataPath, "data.txt");
+
+            if (_saveType == SaveType.PlayerPrefs) {
+                LoadFromPlayerPrefs();
+            } else {
+                LoadFromFile();
+            }
+
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
         }
 
-        private void OnDisable() {
-            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
-        }
-
-        private void OnCarCollision() {
+        public void StartSaveProcess() {
             var newRecord = new SaveData {
                 date = DateTime.Now.ToString("MM/dd/yyyy HH:mm"),
-                score = _currentScore.value.ToString()
+                score = _currentScore.value.ToString(),
+                isHighlighted = true
             };
-            Debug.Log($" new record: {newRecord.date} {newRecord.score}");
-            _saveDatas.Add(newRecord);
+            _currentScore.value = 0;
 
-            //SaveToPlayerPrefs();
-            SaveToFile();
+            Turn_Highlights_Off_In_savedDatas();
+            _saveDatas.Add(newRecord);
+            if (!EditRecords()) {
+                UIManager.Instance.ShowLeaderboardsScreen();
+                return;
+            }
+
+            if (_saveType == SaveType.PlayerPrefs) {
+                SaveDataToPlayerPrefs();
+            } else {
+                SaveToFile();
+            }
+            UIManager.Instance.ShowLeaderboardsScreen();
+        }
+
+        private void Turn_Highlights_Off_In_savedDatas() {
+            foreach (var it in _saveDatas) {
+                it.isHighlighted = false;
+            }
+        }
+
+        private bool EditRecords() {
+            int size = _saveDatas.Count - 1;
+            if (size > 9 && Int32.Parse(_saveDatas[size - 1].score) > Int32.Parse(_saveDatas[size].score)) {
+                return false;
+            }
+
+            for (int i = size; i > 0; i--) {
+                if (Int32.Parse(_saveDatas[i].score) <= Int32.Parse(_saveDatas[i - 1].score)) {
+                    break;
+                }
+                SwapInSaveDatas(i, i - 1);
+            }
+            if (size > 9) {
+                _saveDatas.RemoveAt(size);
+            }
+
+            return true;
+        }
+
+        private void SwapInSaveDatas(int x, int y) {
+            SaveData temp = _saveDatas[x];
+            _saveDatas[x] = _saveDatas[y];
+            _saveDatas[y] = temp;
         }
 
         private void LoadFromPlayerPrefs() {
@@ -75,31 +136,32 @@ namespace Game {
             return wrapper;
         }
 
-        private void SaveToPlayerPrefs() {
+        private void SaveDataToPlayerPrefs() {
             var wrapper = GetWrapper();
             var json = JsonUtility.ToJson(wrapper);
-            PlayerPrefs.SetString(RECORDS_KEY,json );
+            PlayerPrefs.SetString(RECORDS_KEY, json);
         }
 
         private void LoadFromFile() {
-            if (!File.Exists(_filepath)) {
+            if (!File.Exists(_filePath)) {
                 return;
             }
-            
+
             var binaryFormatter = new BinaryFormatter();
-            using (FileStream fileStream = File.Open(_filepath, FileMode.Open)) {
-                var wrapper = (SavedDataWrapper) binaryFormatter.Deserialize(fileStream);
+            using (FileStream fileStream = File.Open(_filePath, FileMode.Open)) {
+                var wrapper = (SavedDataWrapper)binaryFormatter.Deserialize(fileStream);
                 _saveDatas = wrapper.saveDatas;
             }
-            Debug.Log(_saveDatas.Count);
         }
 
         private void SaveToFile() {
             var wrapper = GetWrapper();
+
             var binaryFormatter = new BinaryFormatter();
-            using (FileStream fileStream = File.Open(_filepath, FileMode.OpenOrCreate)) {
-                binaryFormatter.Serialize(fileStream,wrapper);
+            using (FileStream fileStream = File.Open(_filePath, FileMode.OpenOrCreate)) {
+                binaryFormatter.Serialize(fileStream, wrapper);
             }
         }
+
     }
 }
