524ea932375f5051b0703ca80c1b4742163c858c
BarbadosP <var0720@gmail.com>
2020-11-11 01:33:44 +0200

    new .cs for Record dates

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class RecordView : MonoBehaviour {
+
+        [SerializeField]
+        private List<Text> _lablesRecordView;
+
+        [SerializeField]
+        private Image _image;
+
+        public void SetData(int place, string date, string score) {
+            _lablesRecordView[0].text = place.ToString();
+            _lablesRecordView[1].text = date;
+            _lablesRecordView[2].text = score;
+        }
+
+        public void Symbol() {
+            //_lablesRecordView[0].text = "aaa";
+            _image.enabled = true;
+        }
+    }
+}
