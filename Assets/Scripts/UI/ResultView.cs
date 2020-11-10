da33da5f538ec626258fd32c4a5cf49095ebc8c9
Dan <kdanil01@mail.ru>
2020-11-10 15:20:22 +0200

    Created the ResultView script

---
+++ Assets/Scripts/UI/ResultView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+        
+    public class ResultView : MonoBehaviour {
+
+        [SerializeField]
+        private Text _placeLabel;
+
+        [SerializeField]
+        private Text _dateLabel;
+
+        [SerializeField]
+        private Text _scoreLabel;
+
+        [SerializeField]
+        private Image _lineImage;
+
+        public void SetData(int place, string date, string score) {
+            _placeLabel.text = place.ToString() + '.';
+            _dateLabel.text = date;
+            _scoreLabel.text = score;
+        }
+
+        public void SetNewScoreImage() {
+            _lineImage.sprite = Resources.Load <Sprite>("Sprites/CHERRY BLOSSOM/3.IMAGE/item_0_image");
+        }
+    }
+}
