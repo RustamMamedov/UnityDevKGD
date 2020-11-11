a0b27d26c4e579b21bd8e123283d139c6e4f282d
Dan <kdanil01@mail.ru>
2020-11-11 11:39:43 +0200

    Created CarLisght script, its prefab and CarLight asset

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    
+    public class CarLight : MonoBehaviour {
+        
+        [SerializeField]
+        private Color _gizmosColor = Color.white;
+        
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        private void OnDrawGizmosSelected() {
+            //Debug.Log(transform.position);
+            Gizmos.DrawFrustum(transform.position, 45f, _carSettings.carLightLength, 0.5f, 1f);
+        }
+    }
+
+}
