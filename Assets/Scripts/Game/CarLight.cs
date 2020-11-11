4fb09360148ee46fe30a5d01272db23ed82c38c5
Nikita Shpulin <shpulinn@mail.ru>
2020-11-11 13:33:16 +0300

    CarLight class created, gizmos drawed

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Game {
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private Color _gizmosColor = Color.white;
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = _gizmosColor;
+            Gizmos.DrawFrustum(transform.position, 20f, _carSettings.carLightLenght, 0.5f, 3f);
+        }
+
+
+    }
+}
