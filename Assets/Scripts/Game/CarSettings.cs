6309ced4056b4d586ff10050b3471ecf717a03c5
Dan <kdanil01@mail.ru>
2020-11-11 11:40:41 +0200

    Added carLightLength parameter to CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -9,12 +9,17 @@ namespace Game {
 
         [BoxGroup("Score")]
         [ValidateInput(nameof(ValidateDodgeScore))]
+
         public int dodgeScore;
         [FoldoutGroup("Speed")]
         public float maxSpeed;
+
         [FoldoutGroup("Speed")]
         [InfoBox("Speed is increased by acceleration every frame.", InfoMessageType.Warning)]
         public float acceleration;
+
+        [Range(1, 5)]
+        public float carLightLength;
 
         private bool ValidateDodgeScore(int score) {
             return (score > 0);
