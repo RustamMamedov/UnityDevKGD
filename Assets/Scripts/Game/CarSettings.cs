3f3e41bf4634c399674ba4059c82514ed45e0107
Valeriy <mr.robariii@gmail.com>
2020-11-12 19:05:15 +0200

    CarSettings script update

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Game {
 
     [CreateAssetMenu(fileName = "New Car", menuName = "Car")]
     public class CarSettings : ScriptableObject {
         
+        [BoxGroup("Score")]
+        [ValidateInput(nameof(ValidateDodgeScore))]
         public int dodgeScore;
+        [BoxGroup("Score")]
+        public int dodgeScore2;
+        [FoldoutGroup("Speed")]
         public float maxSpeed;
+        [FoldoutGroup("Speed")]
+        [InfoBox("Speed is beeing increased by this value every frame")]
         public float acceleration;
 
+        private bool ValidateDodgeScore(int score) {
+            return score >= 0;
+        }
     }
 }
 
