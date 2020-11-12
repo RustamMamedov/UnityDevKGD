e79464fe9b78a63a8bf4066a53c5912b6a9671c8
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-12 18:37:27 +0200

    correct save cs

--- Assets/Scripts/Game/Save.cs
+++ Assets/Scripts/Game/Save.cs
@@ -1,66 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Events;
-using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Game {
-    public class Save : MonoBehaviour {
 
+    public class Save : MonoBehaviour {
         [SerializeField]
+        private EventDispatcher _saveResultEventDispatcher;
+
+        [Serializable]
         public class SaveData {
+
             public string date;
             public string score;
         }
 
+        public static int last;
+
+        //for Sort
         private class ScoreComparer : IComparer<SaveData> {
-            public int Compare(SaveData d1, SaveData d2) {
-                var val1 = Int32.Parse(d1.score);
-                var val2 = Int32.Parse(d2.score);
-                if (val1 > val2) {
+
+            public int Compare(SaveData a1, SaveData a2) {
+                var value1 = Int32.Parse(a1.score);
+                var value2 = Int32.Parse(a2.score);
+                if (value1 > value2) {
                     return -1;
                 }
-                else if (val1 < val2) {
+                else if (value1 < value2) {
                     return 1;
                 }
+
                 return 0;
             }
         }
 
-        private ScoreComparer _comparer;
+        private ScoreComparer comparer = new ScoreComparer();
 
         [SerializeField]
+        private int _countBestResults;
+
+        [Serializable]
         private class SavedDataWrapper {
+
             public List<SaveData> saveDatas;
         }
 
-        private enum SaveType {
-            PlayerPrefs,
-            File
-        }
-
         [SerializeField]
-        private EventListeners _carCollisionEventListener;
+        private EventListeners _carCollisionEventListeners;
 
         [SerializeField]
         private ScriptableIntValue _currentScore;
 
-        [SerializeField]
-        private SaveType _saveType;
-        [SerializeField]
-        private int _countBestResult;
+        private static List<SaveData> _savedData;
 
-        private static List<SaveData> _saveDatas;
-        public static List<SaveData> SavedDatas => _saveDatas;
+        public static List<SaveData> SavedDatas => _savedData;
 
-        private const string RECORDS_KEY = "records";
+        private const string Records_Key = "records";
+
         private string _filePath;
 
+        private enum SaveType {
+            PlayerPrefs,
+            FromFile
+        }
+
+        [SerializeField]
+        private SaveType _saveType;
+
         private void Awake() {
-            _saveDatas = new List<SaveData>();
-            _filePath = Path.Combine(Application.persistentDataPath,"data.txt");
+            _savedData = new List<SaveData>();
+            //Debug.Log(Application.persistentDataPath);
+            _filePath = Path.Combine(Application.persistentDataPath, "data.txt");
             if (_saveType == SaveType.PlayerPrefs) {
                 LoadFromPlayerPrefs();
             }
@@ -70,73 +84,93 @@ namespace Game {
         }
 
         private void OnEnable() {
-            _carCollisionEventListener.OnEventHappened += OnCarCollision;
-        }
-        private void OnDisable() {
-            _carCollisionEventListener.OnEventHappened -= OnCarCollision;
-            _saveDatas.Clear();
+            _carCollisionEventListeners.OnEventHappened += OnCarCollison;
         }
 
-        private void BestResults() {
-            _saveDatas.Sort(_comparer);
-            for(int i = _saveDatas.Count - 1; i >= _countBestResult; i--) {
-                _saveDatas.RemoveAt(i);
-            }
+        private void OnDisable() {
+            _carCollisionEventListeners.OnEventHappened -= OnCarCollison;   
+            _savedData.Clear();
         }
-        private void OnCarCollision() {
+
+        private void OnCarCollison() {
             var newRecord = new SaveData {
                 date = DateTime.Now.ToString("MM/dd/yyyy HH:mm"),
                 score = _currentScore.value.ToString()
-            };
-            _saveDatas.Add(newRecord);
 
+            };
+            //Debug.Log($"new record:{newRecord.date} {newRecord.score}");
+            _savedData.Add(newRecord);
             if (_saveType == SaveType.PlayerPrefs) {
-                SaveToPlayerPrefs();
+                SaveFromPlayerPrefs();
             }
             else {
-                SaveToFile();
+                SaveFromFile();
             }
-            BestResults();
+            //Debug.Log("SaveData");
+            //Debug.Log($"{_savedData[0].score}   {_savedData.Count}");
+            Sort10BestResult(newRecord);
+            //Debug.Log($"{_savedData[0].score}   {_savedData.Count}");
+            _saveResultEventDispatcher.Dispatch();
+        }
+
+        private void Sort10BestResult(SaveData newResult) {
+            _savedData.Sort(comparer);
+            var count = _savedData.Count;
+            for (int i = count - 1; i >= _countBestResults; i--) {
+                _savedData.RemoveAt(i);
+
+            }
+            for (int i = 0; i < _savedData.Count; i++) {
+                if (newResult == _savedData[i]) {
+                    last = i;
+                }
+                else last = -1;
+            }
+        }
+
+        private void LoadFromPlayerPrefs() {
+            if (!PlayerPrefs.HasKey(Records_Key)) {
+                return;
+            }
+            var wrapper = JsonUtility.FromJson<SavedDataWrapper>(PlayerPrefs.GetString(Records_Key));
+            _savedData = wrapper.saveDatas;
 
         }
 
-        private SavedDataWrapper GetWrapper() {
+        private SavedDataWrapper GetDataWrapper() {
             var wrapper = new SavedDataWrapper {
-                saveDatas = _saveDatas
+                saveDatas = _savedData
             };
             return wrapper;
         }
 
-        private void LoadFromPlayerPrefs() {
-            if (!PlayerPrefs.HasKey(RECORDS_KEY)) {
-                return;
-            }
-            var wrapper = JsonUtility.FromJson<SavedDataWrapper>(PlayerPrefs.GetString(RECORDS_KEY));
-            _saveDatas = wrapper.saveDatas;
-        }
-        private void SaveToPlayerPrefs() {
-            var wrapper = GetWrapper();
+        private void SaveFromPlayerPrefs() {
+            var wrapper = GetDataWrapper();
             var json = JsonUtility.ToJson(wrapper);
-            PlayerPrefs.SetString(RECORDS_KEY, json);
+            //Debug.Log(json);
+            PlayerPrefs.SetString(Records_Key, json);
         }
 
-        private void LoadFromFile() {
-            if (File.Exists(_filePath)) {
-                return;
-            }
-            var binaryFormatter = new BinaryFormatter();
-            using (FileStream fileStream = File.Open(_filePath, FileMode.OpenOrCreate)) {
-                var wrapper = (SavedDataWrapper)binaryFormatter.Deserialize(fileStream);
-                _saveDatas = wrapper.saveDatas;
-            }
-            Debug.Log(_saveDatas.Count);
-        }
-        private void SaveToFile() {
-            var wrapper = GetWrapper();
+        private void SaveFromFile() {
+            var wrapper = GetDataWrapper();
+
             var binaryFormatter = new BinaryFormatter();
             using (FileStream fileStream = File.Open(_filePath, FileMode.OpenOrCreate)) {
                 binaryFormatter.Serialize(fileStream, wrapper);
             }
+        }
+
+        private void LoadFromFile() {
+            if (!File.Exists(_filePath)) {
+                return;
+            }
+
+            var binaryFormatter = new BinaryFormatter();
+            using (FileStream fileStream = File.Open(_filePath, FileMode.OpenOrCreate)) {
+                var wrapper = (SavedDataWrapper)binaryFormatter.Deserialize(fileStream);
+                _savedData = wrapper.saveDatas;
+            }
+            //Debug.Log(_savedData.Count);
         }
     }
 }
