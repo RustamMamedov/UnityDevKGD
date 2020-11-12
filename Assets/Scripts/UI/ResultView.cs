98d1df5c82be03a624de46b5e6ef961068cb5063
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-12 17:16:09 +0200

    create resultView prefab and cs

---
+++ Assets/Scripts/UI/ResultView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class ResultView : MonoBehaviour {
+
+        [SerializeField]
+        private List<Text> _lableRecordView;
+
+        [SerializeField]
+        private Image _image;
+
+        public void SetData(int place, string date, string score) {
+            _lableRecordView[0].text = place.ToString();
+            _lableRecordView[1].text = date;
+            _lableRecordView[2].text = score;
+        }
+
+        public void Symbol(){
+            _image.enabled = true;
+        }
+    }
+}
