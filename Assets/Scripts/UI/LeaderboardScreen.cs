faa27ade12cd3e8df6df97cb59fa0f12b91f6105
Nikita <nikitadedov66@gmail.com>
2020-11-14 00:51:39 +0200

    records visualise

--- Assets/Scripts/UI/LeaderboardScreen.cs
+++ Assets/Scripts/UI/LeaderboardScreen.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game;
 
-public class LeaderboardScreen : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class LeaderboardScreen : MonoBehaviour {
+
+    private List<GameObject> _recordViewOnScene = new List<GameObject>();
+
+    [SerializeField]
+    private GameObject _recordViewPrefab;
+
+    [SerializeField]
+    private GameObject _instantiatePosition;
+
+    private void OnEnable() {
+        int i = 1;
+        foreach (var obj in Save.SaveDatas) {
+            var record = Instantiate(_recordViewPrefab, _instantiatePosition.transform);
+            _recordViewOnScene.Add(record);
+            record.GetComponent<RecordView>().SetData(i, obj.date, obj.score);
+            i++;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDisable() {
+        for (int i = 1; i <= _instantiatePosition.transform.childCount; i++) {
+            Destroy(_instantiatePosition.transform.GetChild(i).gameObject);
+        }
     }
+
 }
