98b9133b53a8fce4b320a46251e1d2dfbf5ff615
Nikita <nikitadedov66@gmail.com>
2020-11-14 00:48:53 +0200

    setData method realized

---
+++ Assets/Scripts/UI/RecordView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordView : MonoBehaviour {
+
+    [SerializeField]
+    private Text _placeText;
+
+    [SerializeField]
+    private Text _dateText;
+
+    [SerializeField]
+    private Text _scoreText;
+
+    public void SetData(int place, string date, string score) {
+        _placeText.text = place.ToString();
+        _dateText.text = date;
+        _scoreText.text = score.ToString();
+
+    }
+
+}
