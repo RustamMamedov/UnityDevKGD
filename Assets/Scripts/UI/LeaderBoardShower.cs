c3ceae72affa257feda90a2f2e1ea4c5f39e42ae
Valeriy <mr.robariii@gmail.com>
2020-11-14 20:24:23 +0200

    LeaderBoardShower script created
    This script shows Leadeboard screen after each car collision

---
+++ Assets/Scripts/UI/LeaderBoardShower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Events;
+
+namespace UI {
+
+    public class LeaderBoardShower : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _carCollisionListener;
+
+        private void OnEnable() {
+            _carCollisionListener.OnEventHappened += ShowLeadderBoard;
+        }
+
+        private void ShowLeadderBoard() {
+            UIManager.instance.ShowLeaderboardScreen();
+        }
+    }
+}
+
