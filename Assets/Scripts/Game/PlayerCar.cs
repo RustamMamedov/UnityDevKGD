d80769b445cad422a749b87b45649958a0770119
BepeckoB <BepeckoB@gmail.com>
2020-11-14 14:06:31 +0200

    gizmos

--- Assets/Scripts/Game/PlayerCar.cs
+++ Assets/Scripts/Game/PlayerCar.cs
@@ -22,6 +22,9 @@ namespace Game {
         [SerializeField]
         private ScriptableFloatValue _playerPositionZ;
 
+        [SerializeField]
+        private Color _gizmosColor;
+
         private int _currentRoad;
 
         private bool _inDodge;
@@ -63,6 +66,20 @@ namespace Game {
             }
             _inDodge = false;
             _currentRoad = nextRoad;
+        }
+
+        private void OnDrawGizmos() {
+            
+        }
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = _gizmosColor;
+
+            Gizmos.DrawWireSphere(transform.position, 5f);
+            Gizmos.DrawIcon(transform.position + Vector3.up * 4f, "heart_icon");
+            Gizmos.DrawFrustum(transform.position + transform.forward * 2, 45f, 15f, 50f, .5f);
+            var mesh = GetComponent<MeshFilter>().sharedMesh;
+            Gizmos.DrawWireMesh(mesh, 0, transform.position + transform.forward * 5);
         }
 
     }
