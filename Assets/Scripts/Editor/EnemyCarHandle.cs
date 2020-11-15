46163fb6d72d05c03fc7cee4c3a56b9c51c80dd6
Nikita <Nkanunov@gmail.com>
2020-11-15 15:01:52 +0200

    Created Editor folder and EnemyCarHandle script

---
+++ Assets/Scripts/Editor/EnemyCarHandle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Game;
+using UnityEngine;
+using UnityEditor;
+
+namespace GameEditor {
+
+    [CustomEditor(typeof(EnemyCar))]
+    public class EnemyCarHandle : Editor {
+
+        private EnemyCar _enemyCar;
+
+        private void OnEnable() {
+            _enemyCar = (EnemyCar) target;
+        }
+
+        private void OnSceneGUI() {
+            if (Event.current.type == EventType.Repaint) {
+                Handles.color = Color.red;
+
+                var angle = 45f;
+                var rotation = Quaternion.Euler(0f, -angle / 2, 0f);
+                Handles.DrawSolidArc(_enemyCar.transform.position, Vector3.up, rotation * Vector3.forward, angle, 5f);
+
+                var style = new GUIStyle();
+                style.fontSize = 30;
+                style.normal.textColor = Color.red;
+                var text = $"{_enemyCar.name}. Dodge score {_enemyCar.CarSettings.dodgeScore}";
+                Handles.Label(_enemyCar.transform.position + Vector3.up * 5f, text, style);
+            }
+        }
+    }
+}
