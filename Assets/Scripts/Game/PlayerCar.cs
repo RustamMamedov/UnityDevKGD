2d8a55ee64d339ff86310021eb305310e971b096
Denis <denis.zhuravkov@gmail.com>
2020-11-16 00:51:56 +0200

    Some temporary gizmos code commented for PlayerCar

--- Assets/Scripts/Game/PlayerCar.cs
+++ Assets/Scripts/Game/PlayerCar.cs
@@ -82,12 +82,12 @@ namespace Game {
         private void OnDrawGizmosSelected() {
             Gizmos.color = _gizmosColor;
 
-            var mesh = GetComponent<MeshFilter>().mesh;
-            Gizmos.DrawWireMesh(mesh, 0, transform.position + transform.forward * 5f);
-            
-            Gizmos.DrawWireSphere(transform.position, 5f);
+            // var mesh = GetComponent<MeshFilter>().mesh;
+            // Gizmos.DrawWireMesh(mesh, 0, transform.position + transform.forward * 5f);
+            //
+            // Gizmos.DrawWireSphere(transform.position, 5f);
             Gizmos.DrawIcon(transform.position + Vector3.up * 4f, "car_gizmo", false); 
-            Gizmos.DrawFrustum(transform.position + transform.forward * 2f, 45f, 15f, 50f, 1);
+            // Gizmos.DrawFrustum(transform.position + transform.forward * 2f, 45f, 15f, 50f, 1);
            
         }
     }
