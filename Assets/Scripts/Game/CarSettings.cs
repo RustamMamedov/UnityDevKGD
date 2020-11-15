08ba54a80da4f9faadcd80a7ebc6dcefab9e5587
Denis <denis.zhuravkov@gmail.com>
2020-11-15 17:33:04 +0200

    Added OdinInspector attributes to CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Game {
 
     [CreateAssetMenu(fileName = "CarSettings", menuName = "CarSettings", order = 0)]
     public class CarSettings : ScriptableObject {
         
-        [Header("Score")]
-        public int dodgeScore = 0;
-        [Header("Speed")]
+        [FoldoutGroup("Speed", false)]
+        public float acceleration;
+        [FoldoutGroup("Speed")]
+        [InfoBox("Speed is beeing increased by acceleration every frame", InfoMessageType.Warning)]
         public float maxSpeed;
         
-        [Space]
-        [Header("Acceleration")]
-        public float acceleration;
+        [BoxGroup("Speed/Score")]
+        [ValidateInput(nameof(ValidateDodgeScore))]
+        public int dodgeScore = 0;
+
+        private bool ValidateDodgeScore(int score) {
+            return score >= 0;
+        }
+        
     }
 }
