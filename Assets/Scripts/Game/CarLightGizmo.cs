9cf818b26239078cde40d334ae435e9774b95de1
Nikita <Nkanunov@gmail.com>
2020-11-16 01:49:59 +0200

    Changed min and max values on DrawFrustum method

--- Assets/Scripts/Game/CarLightGizmo.cs
+++ Assets/Scripts/Game/CarLightGizmo.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,7 +12,7 @@ namespace Game {
         private void OnDrawGizmos() {
             Gizmos.color = _color;
             Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.DrawFrustum(Vector3.zero, 45f, 0f, _carSettings.lightDistance, 2f);
+            Gizmos.DrawFrustum(Vector3.zero, 45f, _carSettings.lightDistance, 0f, 2f);
         }
     }
 }
