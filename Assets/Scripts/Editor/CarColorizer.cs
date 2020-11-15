ad7764555f1e5bf674f6fd82797347d3e2930919
BepeckoB <BepeckoB@gmail.com>
2020-11-15 22:55:33 +0200

    custom editor

---
+++ Assets/Scripts/Editor/CarColorizer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace GameEditor {
+
+    public class CarColorizer : EditorWindow {
+
+        private Color _color = Color.white;
+        
+        [MenuItem("Tools/Car colorizer")]
+        public static void OpenWindow() {
+            GetWindow<CarColorizer>("Car colorizer");
+        }
+
+        private void OnGUI() {
+            GUILayout.Label("Select color", EditorStyles.boldLabel);
+
+            _color = EditorGUILayout.ColorField("Color", _color);
+
+            if (GUILayout.Button("Press me")) {
+                var gameObjects = Selection.gameObjects;
+                for (int i = 0; i < gameObjects.Length; i++) {
+
+                    var mechRenderer = gameObjects[i].GetComponent<MeshRenderer>();
+                    if (gameObjects[i].TryGetComponent<MeshRenderer>(out var meshRenderer)) {
+                        //meshRenderer.sharedMaterial.color = _color; -- for every game object
+                        //only for one
+                        var material = new Material(meshRenderer.sharedMaterial);
+                        material.color = _color;
+                        meshRenderer.sharedMaterial = material;
+                    }
+                }
+            }
+        }
+
+    }
+}
