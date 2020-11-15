9f37f082d1a83baf1baca5878dcb26f15a7bcd4e
Denis <denis.zhuravkov@gmail.com>
2020-11-16 00:48:56 +0200

    CarLights script created
    For first time, it handles gizmos drawing for Car Lights

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Game {
+    
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField] 
+        private Color _gizmoColor = Color.yellow;
+
+        [SerializeField] 
+        private CarSettings _carSettings;
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = _gizmoColor;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(transform.position, 45f, 0f, _carSettings.lightDistance, 1f);
+        }
+    }
+}
+
