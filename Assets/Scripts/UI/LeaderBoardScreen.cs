38679f0f4b1921de58c60c0cea79c4a3c8487bf3
VysogotaOC <kamaeff.petya@gmail.com>
2020-11-15 07:02:20 +0200

    LeaderBoardScreen script added

---
+++ Assets/Scripts/UI/LeaderBoardScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace UI {
+    public class LeaderBoardScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _menuButton;
+
+        private void Awake() {
+            _menuButton.onClick.AddListener(OnMenuButtonClick);
+        }
+
+        private void OnDestroy() {
+            _menuButton.onClick.RemoveListener(OnMenuButtonClick);
+        }
+
+        private void OnMenuButtonClick() {
+            UIManager.Instance.LoadMenu();
+        }
+    }
+}
