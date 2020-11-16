218a04e495173088766c9725452df69f4040333d
Nikita Shpulin <shpulinn@mail.ru>
2020-11-16 20:02:04 +0300

    CarDodgeView class created

---
+++ Assets/Scripts/UI/CarDodgeView.cs
@@ -0,0 +1,24 @@
+﻿using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class CarDodgeView : MonoBehaviour {
+
+        [SerializeField]
+        private RawImage _carImage;
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        private void OnEnable() {
+            Init();
+        }
+
+        public void Init() {
+            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab);
+        }
+    }
+}
