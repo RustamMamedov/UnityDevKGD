549b1a515256afdf1f0eb5c442cb9d13b1083861
Nikita <nikitadedov66@gmail.com>
2020-11-16 17:19:51 +0200

    car light script created

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game {
+
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private Color _color = Color.white;
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+
+        private void OnDrawGizmos() {
+            Gizmos.color = _color;
+
+            Gizmos.DrawFrustum(transform.position, 25f, _carSettings.lightDistance, 0.5f, 2f);
+        }
+
+
+
+    }
+}
