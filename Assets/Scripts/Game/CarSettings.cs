4ce9559a937da4f45b953d9cb36d3a5397a0e2f9
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-16 15:12:38 +0200

    More readable inspector for CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -4,8 +4,13 @@ namespace Game {
 
     [CreateAssetMenu(fileName = "CarSettings", menuName = "Car/CarSettings")]
     public class CarSettings : ScriptableObject {
+
+        [Header("Score")]
         public int dodgeScore;
+
+        [Header("Speed")]
         public float maxSpeed;
+        [Space]
         public float acceleration;
     }
 }
