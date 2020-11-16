9de95c06207ec4dea8a9d4712ed563e72b33f3f1
Nikita Shpulin <shpulinn@mail.ru>
2020-11-16 20:10:56 +0300

    renderCarPrefab field added to CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -20,6 +20,8 @@ namespace Game {
         [Range(1, 5)]
         public int carLightLenght;
 
+        public GameObject renderCarPrefab;
+
         private bool ValidateDodgeScore(int score) {
             return score >= 0;
         }
