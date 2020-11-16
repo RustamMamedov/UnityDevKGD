81a7fe62bedd717d69c918ba32c133f5eef32aa8
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-16 15:10:43 +0200

    Car custom inspector buttons to change color

---
+++ Assets/Scripts/Editor/CarInspector.cs
@@ -0,0 +1,36 @@
+﻿using Game;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameEditor {
+
+    [CustomEditor(typeof(EnemyCar))]
+    public class CarInspector : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            var enemyCar = (EnemyCar) target;
+
+            GUILayout.BeginHorizontal();
+            GUI.backgroundColor = Color.red;
+            if (GUILayout.Button("Red")) {
+                if (enemyCar.TryGetComponent<MeshRenderer>(out var meshRenderer)) {
+                    var material = new Material(meshRenderer.sharedMaterial);
+                    material.color = Color.red;
+                    meshRenderer.material = material;
+                }
+            }
+
+            GUI.backgroundColor = Color.green;
+            if (GUILayout.Button("Green")) {
+                if (enemyCar.TryGetComponent<MeshRenderer>(out var meshRenderer)) {
+                    var material = new Material(meshRenderer.sharedMaterial);
+                    material.color = Color.green;
+                    meshRenderer.material = material;
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+
+}
