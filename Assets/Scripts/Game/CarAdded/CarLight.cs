c73a165a7bfba7485f4232cca47d1841181bf778
Ilya <elya5911@mail.ru>
2020-11-16 18:18:06 +0200

    Create CarLight cs

---
+++ Assets/Scripts/Game/CarAdded/CarLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private CarSettings _settings;
+
+        private void OnDrawGizmos() {
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(transform.forward, 30f, _settings.lenghLightCar + 1, 1f, 1f);
+        }
+
+    }
+}
