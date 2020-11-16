c5eff226342e98df4720f2b5e28b94bf247fce37
Andrey <stanok99@bk.ru>
2020-11-16 05:41:54 +0200

    handles

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -15,6 +15,10 @@ namespace Game {
         [SerializeField]
         private EventListener _carCollisionEventListener;
 
+#if UNITY_EDITOR
+        public CarSettings CarSettings => _carSettings;
+#endif
+
         protected float _currentSpeed;
 
 #endregion  1
