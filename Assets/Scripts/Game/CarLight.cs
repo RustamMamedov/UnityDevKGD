a53e34eb6df1d75c915f5a39047e0f3f9dbaf366
Potapenko_Ivan <van.van123@list.ru>
2020-11-16 13:11:30 +0200

    gizmo car lights realized

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Game;
+
+namespace GameEditor {
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private Color _gizmosColor;
+
+        [SerializeField]
+        private CarSettings _carSettings;
+    
+        private void OnDrawGizmos() {
+            Gizmos.color = _gizmosColor;
+            Gizmos.matrix = gameObject.transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(Vector3.zero, 15f,_carSettings.lightLength,0.1f,0.5f);
+            
+        }
+    }
+}
