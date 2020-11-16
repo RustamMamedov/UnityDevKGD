c202249b69d3d4c517389bd2bd1e6f3337495b6f
BepeckoB <BepeckoB@gmail.com>
2020-11-16 15:16:05 +0200

    attributes

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -7,8 +7,11 @@ namespace Game {
     [CreateAssetMenu(fileName = "Car", menuName = "Car")]
     public class CarSettings : ScriptableObject {
 
+        [Header("Score")]
         public int dodgeScore;
+        [Header("Speed")]
         public float maxSpeed;
+        [Space]
         public float acceleration;
     }
 }
