8a6f81db2dfb28b1288143abf6005a403ff9f9f1
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-16 16:29:35 +0200

    car light prefab + logic

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private Color _gizmosColor = Color.white;
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        private void OnDrawGizmosSelected() {
+
+            Gizmos.color = _gizmosColor;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(Vector3.zero, 15f, _carSettings.carLight, .1f, 0.5f);
+        }
+    }
+}
