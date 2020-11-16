ceefdca92989f8f4e32ca9ca97e1c9b6f17f3b68
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-16 04:14:15 +0200

    Headers in CarSettings script

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -5,8 +5,11 @@ namespace Game {
     [CreateAssetMenu(fileName ="Car Settings",menuName = "Game/Car Settings")]
     public class CarSettings : ScriptableObject {
 
+        [Header("Score")]
         public int dodgeScore;
+        [Header("Speed")]
         public float maxSpeed;
+        [Space]
         public float acceleration;
     }
 }
