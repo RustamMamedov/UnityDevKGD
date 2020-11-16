915f5967aa258b5a29d51f4c807b5718b6698675
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-16 16:56:37 +0200

    CarLight prefab created and added to PlayerCar prefab

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,44 @@
+﻿using Game;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class CarLight : MonoBehaviour {
+
+    [FoldoutGroup("Car")]
+    [SerializeField]
+    private Car _car;
+
+    [FoldoutGroup("Light settings")]
+    [BoxGroup("Light settings/Color")]
+    [SerializeField]
+    private Color _color = Color.white;
+
+    [SerializeField]
+    [Range(0, 5)]
+    private float _maxRange;
+
+    [FoldoutGroup("Light settings")]
+    [BoxGroup("Light settings/Width and height")]
+    [SerializeField]
+    [Range(0, 100)]
+    private float _fov;
+
+    [FoldoutGroup("Light settings")]
+    [BoxGroup("Light settings/Width and height")]
+    [SerializeField]
+    [Range(0, 1)]
+    private float _aspect;
+
+    private float _minRange = 0f;
+
+    private void OnEnable() {
+
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = _color;
+
+        var difference = transform.position.z - _car.transform.position.z;
+        Gizmos.DrawFrustum(transform.position, _fov, _car.CarSettings.lightLength + difference, _minRange, _aspect);
+    }
+}
