ff73fd0fbb45856245a3a084e588b0682fa17f0c
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-16 13:38:01 +0200

    CarLight class created

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game {
+
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        [SerializeField]
+        private Color _gizmosColor = Color.red;
+
+        private void OnDrawGizmos() {
+
+            Gizmos.color = _gizmosColor;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(Vector3.zero, 45f, _carSettings.lightRange,0f,2f);
+        }
+    }
+}
+
