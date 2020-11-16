bed687fac7595c44379a0d3093b0abe1cbad9cbf
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-16 15:31:59 +0200

    Add CarLight script

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Game {
+    
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private Car _car;
+
+#if UNITY_EDITOR
+
+        private void OnDrawGizmosSelected() {
+            if (_car == null) {
+                return;
+            }
+
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Gizmos.DrawFrustum(
+                center: Vector3.zero,
+                fov: 20,
+                maxRange: _car.CarSettings.carLightDistance,
+                minRange: Mathf.Min(0.5f, _car.CarSettings.carLightDistance),
+                aspect: 1
+            );
+
+        }
+
+#endif
+
+
+    }
+
+}
