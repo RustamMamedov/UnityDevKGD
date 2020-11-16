1c1d68e2672621332810fe4d6d73afa994b945c2
Alexandr <navacar@mail.ru>
2020-11-16 21:19:30 +0200

    Music

---
+++ Assets/Scripts/Audio/MusicController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Audio {
+
+    public class MusicController : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _menuMusicPlayer;
+
+        public void PlayMenuMusic() {
+            _menuMusicPlayer.Play();
+        }
+
+    }
+
+}
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Audio;
 
 namespace UI {
 
@@ -20,6 +21,9 @@ namespace UI {
         [SerializeField]
         private GameObject _leaderboardsScreen;
 
+        [SerializeField]
+        private MusicController  _musicManager;
+
         private void Awake() {
             if (Instance != null) {
                 Destroy(gameObject);
@@ -28,6 +32,10 @@ namespace UI {
 
             Instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start() {
+            ShowMenuScreen();
         }
 
         public void LoadMenu() {
@@ -63,6 +71,7 @@ namespace UI {
         public void ShowMenuScreen() {
             HideAllScreens();
             _menuScreen.SetActive(true);
+            _musicManager.PlayMenuMusic();
         }
 
         public void ShowGameScreen() {
