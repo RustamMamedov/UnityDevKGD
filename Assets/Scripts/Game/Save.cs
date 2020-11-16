656c226cb5c55837462f8967123c4b4bcffee178
Oleg <oleg.kalyugin2001@mail.ru>
2020-11-16 13:58:53 +0200

    Save changes

--- Assets/Scripts/Game/Save.cs
+++ Assets/Scripts/Game/Save.cs
@@ -4,30 +4,27 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Events;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Game {
 
     public class Save : MonoBehaviour {
 
         [Serializable]
-        public class SaveData{
+        public class SaveData {
 
             public string date;
             public string score;
-            static public int CompareScore(SaveData sc1, SaveData sc2) {
-                return sc2.score.CompareTo(sc1.score);
-            }
         }
 
         [Serializable]
         private class SavedDataWrapper {
 
             public List<SaveData> saveDatas;
-
         }
 
         private enum SaveType {
-
+            
             PlayerPrefs,
             File,
         }
@@ -36,26 +33,61 @@ namespace Game {
         private EventListener _carCollisionEventListener;
 
         [SerializeField]
-        private ScriptableIntValue _currentScore;
+        private EventDispatcher _saveDispatcher;
 
         [SerializeField]
+        private ScriptableIntValue _currentScore;
+
+        
+        [InfoBox("PlayerPrefs", "saveTypePlayerPrefs")]
+        [InfoBox("@ getFilePath()", "saveTypePlayerFile")]
+        [SerializeField]
         private SaveType _saveType;
+        
+        [SerializeField]
+        private int _maxRecords = 10;
 
         private static List<SaveData> _saveDatas;
         public static List<SaveData> SavedDatas => _saveDatas;
 
+        private static int _currentRideInd = -1;
+        public static int CurrentRideInd => _currentRideInd;
+
         private const string RECORDS_KEY = "records";
         private string _filePath;
+        
+
+        private bool saveTypePlayerPrefs() {
+            if (_saveType == SaveType.PlayerPrefs) {
+                return true;
+            }
+            return false;
+        }
+
+        private bool saveTypePlayerFile() {
+            if (_saveType == SaveType.File) {
+                return true;
+            }
+            return false;
+        }
+
+        private string getFilePath() {
+            _filePath = Path.Combine(Application.persistentDataPath, "data.txt");
+            return _filePath;
+        }
+
 
         private void Awake() {
             _saveDatas = new List<SaveData>();
             _filePath = Path.Combine(Application.persistentDataPath, "data.txt");
+            
             if (_saveType == SaveType.PlayerPrefs) {
                 LoadFromPlayerPrefs();
+                
             } else {
                 LoadFromFile();
+                
             }
-            _saveDatas.Sort(SaveData.CompareScore);
         }
 
         private void OnEnable() {
@@ -72,26 +104,27 @@ namespace Game {
                 score = _currentScore.value.ToString()
             };
 
-            if (_saveDatas.Count < 10) {
-                _saveDatas.Add(newRecord);   
-            }
-            else {
+            _currentRideInd = -1;
+            if (_saveDatas.Count < _maxRecords) {
+                _saveDatas.Add(newRecord);
+                _currentRideInd = _saveDatas.Count - 1;
+            } else {
                 for (int i = 0; i < _saveDatas.Count; i++) {
-                    if (Convert.ToInt32(_saveDatas[i].score) < Convert.ToInt32(newRecord.score)) {
-                        _saveDatas.Add(newRecord);
-                        _saveDatas.RemoveAt(_saveDatas.Count - 2);
-                    }  
+                    if (Int32.Parse(newRecord.score) >= Int32.Parse(_saveDatas[i].score)) {
+                        _saveDatas[i] = newRecord;
+                        _currentRideInd = i;
+                        break;
+                    }
                 }
-                
             }
 
-
-            _saveDatas.Sort(SaveData.CompareScore);
             if (_saveType == SaveType.PlayerPrefs) {
                 SaveToPlayerPrefs();
             } else {
                 SaveToFile();
             }
+
+            _saveDispatcher.Dispatch();
         }
 
         private void LoadFromPlayerPrefs() {
@@ -111,7 +144,6 @@ namespace Game {
         }
 
         private void SaveToPlayerPrefs() {
-
             var wrapper = GetWrapper();
             var json = JsonUtility.ToJson(wrapper);
             PlayerPrefs.SetString(RECORDS_KEY, json);
@@ -119,7 +151,6 @@ namespace Game {
 
         private void LoadFromFile() {
             if (!File.Exists(_filePath)) {
-                
                 return;
             }
 
@@ -128,6 +159,7 @@ namespace Game {
                 var wrapper = (SavedDataWrapper) binaryFormatter.Deserialize(fileStream);
                 _saveDatas = wrapper.saveDatas;
             }
+            Debug.Log(_saveDatas.Count);
         }
 
         private void SaveToFile() {
@@ -136,7 +168,6 @@ namespace Game {
             using(FileStream fileStream = File.Open(_filePath, FileMode.OpenOrCreate)) {
                 binaryFormatter.Serialize(fileStream, wrapper);
             }
-            
         }
     }
 }
