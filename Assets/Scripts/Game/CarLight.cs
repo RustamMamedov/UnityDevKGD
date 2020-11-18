af894987fbf902292fbfe0778357b22cd761ba40
Andrey <stanok99@bk.ru>
2020-11-18 15:10:53 +0200

    fix car light

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -13,8 +13,10 @@ namespace Game {
         private CarSettings _carSettings;
 
         private void OnDrawGizmosSelected() {
-            Gizmos.color = _gizmosColor;
-            Gizmos.DrawFrustum(transform.position, 20f, _carSettings.carLightLenght, 0.5f, 3f);
+            var tempMatrix = Gizmos.matrix;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(Vector3.zero, 20f, _carSettings.carLightLenght, 0.5f, 3f);
+            Gizmos.matrix = tempMatrix;
         }
     }
 }
