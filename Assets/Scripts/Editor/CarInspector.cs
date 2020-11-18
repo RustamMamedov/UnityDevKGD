b16cbbea2b60059d49e715ed5ad91feda658c2e3
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-18 23:56:20 +0200

    car inspector

---
+++ Assets/Scripts/Editor/CarInspector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Game;
+
+namespace GameEditor {
+
+    [CustomEditor(typeof(EnemyCar))]
+    public class CarInspector : Editor {
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            var enemyCar = (EnemyCar) target;
+
+            GUI.backgroundColor = Color.red;
+            if (GUILayout.Button("Red")) {
+                if (enemyCar.TryGetComponent<MeshRenderer>(out var meshRenderer)) {
+                    meshRenderer.material.color = Color.red;
+                }
+            }
+
+            GUI.backgroundColor = Color.green;
+            if (GUILayout.Button("Green")) {
+                if (enemyCar.TryGetComponent<MeshRenderer>(out var meshRenderer)) {
+                    meshRenderer.material.color = Color.green;
+                }
+            }
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}
