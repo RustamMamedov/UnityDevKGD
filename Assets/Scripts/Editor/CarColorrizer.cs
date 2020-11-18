dc5349b63155a559a7401af0fa5940c1c670d3e4
Rfnt <kvotua.job@gmail.com>
2020-11-18 13:50:20 +0200

    custom editor

---
+++ Assets/Scripts/Editor/CarColorrizer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GameEditor {
+
+    public class CarColorrizer : EditorWindow {
+
+        private Color _color= Color.white;
+
+        [MenuItem("Tools/Car colorrizer")]
+        public static void OpenWindow() {
+            GetWindow<CarColorrizer>("Car colorrizer");
+        }
+
+        private void OnGUI() {
+            GUILayout.Label("Select color", EditorStyles.boldLabel);
+
+            _color =EditorGUILayout.ColorField("Color",_color);
+
+            if (GUILayout.Button("Press me")) {
+                var gameObject = Selection.gameObjects;
+                for (int i = 0; i < gameObject.Length; i++) {
+                    if (gameObject[i].TryGetComponent<MeshRenderer>(out var meshRenderer)) {
+                        meshRenderer.sharedMaterial.color = _color;
+                    }
+                }
+            }
+        }
+    }
+}
