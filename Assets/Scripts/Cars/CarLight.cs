cb7179ea2836eb733bd3e57e3a4fbd6bd0714eb2
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-18 22:26:22 +0200

    HW create CarLight

---
+++ Assets/Scripts/Cars/CarLight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Game {
+    public class CarLight : MonoBehaviour {
+        [SerializeField]
+        private CarsSettings _settings;
+        private void OnDrawGizmos() {
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _settings.lenghLightCar, 1f, 1f);
+        }
+    }
+}
--- Assets/Scripts/Cars/CarsSettings.cs
+++ Assets/Scripts/Cars/CarsSettings.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 namespace Game {
     [CreateAssetMenu(fileName = "CarsSettings", menuName = "CarsSettings")]
     public class CarsSettings : ScriptableObject {
+        [Header("Score")]
         public int dodgedScore;
+        [Header("Speed")]
         public float maxSpeed;
+        [Space]
         public float acceleration;
+        [Range(1f,5f)]
+        public float lenghLightCar;
     }
 }
