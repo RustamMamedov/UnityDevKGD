27c1b544bffef57e334842124cff2ed187acd7d7
Rfnt <kvotua.job@gmail.com>
2020-11-18 18:54:07 +0200

    added into enemyspawn validation

--- Assets/Scripts/Game/EnemySpawner.cs
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Events;
-using System.Linq;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
 
 namespace Game {
+
     public class EnemySpawner : MonoBehaviour {
+
         [SerializeField]
         private EventListener _updateEventListener;
 
@@ -13,19 +14,20 @@ namespace Game {
         private EventListener _carCollisionListener;
 
         [SerializeField]
+        [ValidateInput(nameof(ValidateCarPrefs), "Was added two equal car prefabs")]
         private List<GameObject> _carPrefabs = new List<GameObject>();
 
         [SerializeField]
-        private List<CarSettings> _carSettingsPrefabs = new List<CarSettings>();
+        private List<CarSettings> _carSettings = new List<CarSettings>();
 
         [SerializeField]
         private float _spawnCooldown;
 
         [SerializeField]
-        private float _dictanceToPlayerToSpawn;
+        private float _distanceToPlayerToSpawn;
 
         [SerializeField]
-        private float _distanceToPlayerDestroy;
+        private float _distanceToPlayerToDestroy;
 
         [SerializeField]
         private ScriptableFloatValue _playerPositionZ;
@@ -33,15 +35,19 @@ namespace Game {
         [SerializeField]
         private ScriptableFloatValue _roadWidth;
 
-        [SerializeField] ScriptableIntValue _currentScore;
+        [SerializeField]
+        private ScriptableIntValue _currentScore;
 
         private float _currentTimer;
 
-        private List<GameObject> _cars= new List<GameObject>();
+        private List<GameObject> _cars = new List<GameObject>();
+
+        private List<int> _carsIndexs = new List<int>();
 
         private void OnEnable() {
             SubscribeToEvents();
         }
+
         private void OnDisable() {
             UnsubscribeToEvents();
         }
@@ -49,7 +55,6 @@ namespace Game {
         private void SubscribeToEvents() {
             _updateEventListener.OnEventHappened += UpdateBehaviour;
             _carCollisionListener.OnEventHappened += OnCarCollision;
-            
         }
 
         private void UnsubscribeToEvents() {
@@ -60,12 +65,12 @@ namespace Game {
         private void OnCarCollision() {
             UnsubscribeToEvents();
         }
-        private void UpdateBehaviour() {
 
+        private void UpdateBehaviour() {
             HandleCarsBehindPlayer();
 
             _currentTimer += Time.deltaTime;
-            if (_currentTimer< _spawnCooldown) {
+            if (_currentTimer < _spawnCooldown) {
                 return;
             }
             _currentTimer = 0f;
@@ -74,33 +79,40 @@ namespace Game {
         }
 
         private void SpawnCar() {
-            
             var randomRoad = Random.Range(-1, 2);
-            var randomCar = Random.Range(0, 3);
-            var position = new Vector3(1f*randomRoad* _roadWidth.value,0f,_playerPositionZ.value + _dictanceToPlayerToSpawn );
-            var car = Instantiate(_carPrefabs[randomCar], position, Quaternion.Euler(0f,180f,0f));
-            _cars.Add(car);
+            int randomEnemy = Random.Range(0, 3);
 
+            _carsIndexs.Add(randomEnemy);
+
+            var position = new Vector3(1f * randomRoad * _roadWidth.value, 0f, _playerPositionZ.value + _distanceToPlayerToSpawn);
+            var car = Instantiate(_carPrefabs[randomEnemy], position, Quaternion.Euler(0f, 180f, 0f));
+            _cars.Add(car);
         }
 
         private void HandleCarsBehindPlayer() {
-
-            for(int i = _cars.Count-1; i >-1; i--) {
-                if (_playerPositionZ.value - _cars[i].transform.position.z > _distanceToPlayerDestroy) {
+            for (int i = _cars.Count - 1; i > -1; i--) {
+                if (_playerPositionZ.value - _cars[i].transform.position.z > _distanceToPlayerToDestroy) {
+                    _currentScore.value += _carSettings[_carsIndexs[i]].dodgeScore;
                     Destroy(_cars[i]);
-                    if (_cars[i].name == "SUV(Clone)") {
-                        _currentScore.value += _carSettingsPrefabs[1].dodgeScore;
-                    }
-                    if (_cars[i].name == "FamilyCar(Clone)") {
-                        _currentScore.value += _carSettingsPrefabs[0].dodgeScore;
-                    }
-                    if (_cars[i].name == "Truck(Clone)") {
-                        _currentScore.value += _carSettingsPrefabs[2].dodgeScore;
-                    }
                     _cars.RemoveAt(i);
-                    
+                    _carsIndexs.RemoveAt(i);
                 }
             }
         }
+
+        private bool ValidateCarPrefs(List<GameObject> prefs) {
+
+            foreach (var item in prefs) {
+                List<GameObject> tempList = prefs.FindAll(
+                    delegate (GameObject it) {
+                        return it.name == item.name;
+                    });
+                if (tempList.Count > 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
     }
 }
