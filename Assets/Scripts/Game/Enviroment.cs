2b5558f01960716c3e23044172b4ee0407b2166d
Evgenii <jekaweek1@gmail.com>
2020-11-20 20:56:23 +0200

    fix Enviroment

--- Assets/Scripts/Game/Enviroment.cs
+++ Assets/Scripts/Game/Enviroment.cs
@@ -40,8 +40,6 @@ namespace Game {
                 var road = Instantiate(_roadPrefab, position, Quaternion.identity);
                 _roadTransforms.Add(road.transform);
             }
-
-            _roadPrefab.gameObject.SetActive(false);
         }
 
         private void HandleRoadCollision() {
