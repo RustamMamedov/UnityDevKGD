d4d8891c08c5d90e121073e1491e6bf828b37ced
Nikita <Nkanunov@gmail.com>
2020-11-21 16:27:53 +0200

    Created OdinWindow to change cars assets

---
+++ Assets/Scripts/Editor/OdinWindow.cs
@@ -0,0 +1,26 @@
+using Game;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+
+namespace GameEditor {
+    
+    public class OdinWindow : OdinMenuEditorWindow {
+          
+        [MenuItem("My Game/My Window")]
+        private static void OpenWindow()
+        {
+            GetWindow<OdinWindow>().Show();
+        }
+        
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            var tree = new OdinMenuTree();
+            tree.Selection.SupportsMultiSelect = false;
+            tree.AddAllAssetsAtPath("Car assets", "Assets/Resourses/Cars assets", typeof(CarSettings));
+            return tree;
+        }
+    }
+}
