fa831b23c5972706c0dc99208a30a7ee23b31b07
SaraJess <sarycheva.00@mail.ru>
2020-11-21 21:14:13 +0200

    Fixed CarLight

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -9,7 +9,7 @@ namespace Game {
         [SerializeField]
         private Car _car;
 
-        private void OnDrawGizmos() {
+        private void OnDrawGizmosSelected() {
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _car.CarSettings.lightDistance, 1f, 1f);
         }
