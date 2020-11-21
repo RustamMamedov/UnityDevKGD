8c0245ef3ba6a3ede1ebfec4c0fc033f80e35b98
TechesDePainture <kirillzelen00@gmail.com>
2020-11-21 14:32:07 +0200

    AudioSoursePlayer created

---
+++ Assets/Scripts/Audio/AudioSoursePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Audio {
+
+    public class AudioSoursePlayer : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSource _audioSource;
+
+        [Button]
+        public void Play() {
+            _audioSource.Play();
+        }
+
+        [Button]
+        public void Stop() {
+            _audioSource.Stop();
+        }
+
+    }
+}
