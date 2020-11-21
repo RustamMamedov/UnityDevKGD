aa1bd8aa4f4584c5b4021582dd493288935ab0a2
Evgenii <jekaweek1@gmail.com>
2020-11-21 15:15:01 +0200

    add Audio Scripts

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Audio {
+
+    public class MusicManager : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _menuMusicPlayer;
+
+        public void PlayerMenuMusic() {
+            _menuMusicPlayer.Play();
+        }
+    }
+}
