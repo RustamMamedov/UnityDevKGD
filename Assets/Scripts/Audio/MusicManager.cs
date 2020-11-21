e27176ef0a2d35406df4d4bdbbc4cbcc32899b59
TechesDePainture <kirillzelen00@gmail.com>
2020-11-21 14:31:47 +0200

    Music manager created

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Audio {
+
+    public class MusicManager : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSoursePlayer _menuMusicPlayer;
+
+        public void PlayMenuMusic() {
+            _menuMusicPlayer.Play();
+        }
+    }
+}
