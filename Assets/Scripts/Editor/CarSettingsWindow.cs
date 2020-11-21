4319b096a75078529d579fa5fad4705cad7cf552
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-22 01:25:50 +0200

    Created CarSettingsWindow class
    Window for editor which helps to work with cars' settings

---
+++ Assets/Scripts/Editor/CarSettingsWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+using Game;
+
+
+namespace GameEditor {
+
+    public class CarSettingsWindow : OdinMenuEditorWindow {
+
+        [MenuItem("Tools/Car Settings")]
+        private static void OpenWindow() {
+            GetWindow<CarSettingsWindow>().Show();
+        }
+
+        protected override OdinMenuTree BuildMenuTree() {
+
+            var menuTree = new OdinMenuTree(supportsMultiSelect: false);
+            menuTree.AddAllAssetsAtPath("Car Settings", "Assets/Resources/Cars Settings",typeof(CarSettings));
+            return menuTree;
+        }
+    }
+}
+
