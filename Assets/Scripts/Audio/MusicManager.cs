ede07e9d61abfc1f288038eb41c7f666d18e22f4
Nikita <Nkanunov@gmail.com>
2020-11-22 22:44:53 +0200

    Created MusicManager script

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Audio {
+
+    public class MusicManager : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _menuMusicPlayer;
+
+        public void PlayMenuMusic() {
+            _menuMusicPlayer.Play();
+        }
+
+
+
+    }
+}
