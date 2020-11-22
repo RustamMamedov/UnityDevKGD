0cce4e171620ab7e9ab9f28447f22ab26ed1d4c6
Evgenii <jekaweek1@gmail.com>
2020-11-22 15:20:35 +0200

    Create ScriptableStringValue DodgeScoreName

---
+++ Assets/Scripts/Game/ScriptableStringValue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "New Int Value", menuName = "Data/StringValue")]
+    public class ScriptableStringValue : ScriptableObject {
+
+        public string value;
+    }
+}
