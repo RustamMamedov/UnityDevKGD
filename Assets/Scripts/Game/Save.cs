afebaddae09b7a9421e7d640245c886695f02e5b
Valeriy <mr.robariii@gmail.com>
2020-11-22 16:28:39 +0200

    Save script update

--- Assets/Scripts/Game/Save.cs
+++ Assets/Scripts/Game/Save.cs
@@ -81,9 +81,6 @@ namespace Game {
 
             InsertNewRecord(NewRecord);
             CheckTail();
-            for (int i = 0; i < _savedDatas.Count; i++) {
-                Debug.Log($" record: {i + 1} {_savedDatas[i].date} {_savedDatas[i].score} ");
-            }
 
             if (_saveType == SaveType.PlayerPrefs) {
                 SaveToPlayerPrefs();
