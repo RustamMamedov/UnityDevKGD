609ddfcbead8521bd2d1c641cf5f238a403016ff
Nikita <Nkanunov@gmail.com>
2020-11-22 16:45:56 +0200

    Created CarDodgetView script

---
+++ Assets/Scripts/UI/CarDodgedView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Game;
+namespace UI {
+
+    public class CarDodgedView : MonoBehaviour {
+
+        [SerializeField]
+        private RawImage _carImage;
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+
+        private void OnEnable() {
+            Init();
+        }
+
+        public void Init() {
+            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab);
+        }
+    }
+}
