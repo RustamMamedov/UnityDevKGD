0f1f5f02b4d7db23fa69f56855854244b587dd17
Valeriy <mr.robariii@gmail.com>
2020-11-22 16:32:41 +0200

    CarSettings script update

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -5,24 +5,27 @@ namespace Game {
 
     [CreateAssetMenu(fileName = "New Car", menuName = "Car")]
     public class CarSettings : ScriptableObject {
-        
+
         [BoxGroup("Score")]
         [ValidateInput(nameof(ValidateDodgeScore))]
         public int dodgeScore;
-        [BoxGroup("Score")]
 
-        public int dodgeScore2;
         [FoldoutGroup("Speed")]
         public float maxSpeed;
         [FoldoutGroup("Speed")]
-        [InfoBox("Speed is beeing increased by this value every frame")]
+        [InfoBox("Speed is beeing increased by acceleration every frame")]
         public float acceleration;
 
-        [BoxGroup("LightSettings")]
+        [FoldoutGroup("Distance")]
         [Range(1f, 5f)]
-        public int lightDistance;
+        public float lightDistance;
 
+        [FoldoutGroup("RenderParameters")]
         public GameObject renderCarPrefab;
+        [FoldoutGroup("RenderParameters")]
+        public Vector3 renderCameraPosition;
+        [FoldoutGroup("RenderParameters")]
+        public Quaternion renderCameraRotation;
 
         private bool ValidateDodgeScore(int score) {
             return score >= 0;
