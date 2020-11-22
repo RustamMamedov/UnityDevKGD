6bbde6c867c5555e57aa45b60821f20fb1e7a5bb
Valeriy <mr.robariii@gmail.com>
2020-11-22 10:31:50 +0200

    GameScreen script added

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI {
+
+    public class GameScreen : MonoBehaviour {
+
+
+        private void OnDisable() {
+
+            RenderManager.instance.ReleaseTextures();
+        }
+    }
+}
+
