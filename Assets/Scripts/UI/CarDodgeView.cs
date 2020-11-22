8eb40003a025783cbd66cf8ee488b2d347933e18
Denis <denis.zhuravkov@gmail.com>
2020-11-22 16:56:16 +0200

    CarDodgeView prefab and controller created

---
+++ Assets/Scripts/UI/CarDodgeView.cs
@@ -0,0 +1,23 @@
+﻿using Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    
+    public class CarDodgeView : MonoBehaviour {
+
+        [SerializeField] 
+        private RawImage _carImage;
+
+        [SerializeField] 
+        private CarSettings _carSettings;
+
+        private void OnEnable() {
+            Init();
+        }
+
+        public void Init() {
+            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab);
+        }
+    }
+}
