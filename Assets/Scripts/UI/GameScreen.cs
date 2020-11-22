a768ca2bc13a0d3507e979173c08513de10e0a2d
SaraJess <sarycheva.00@mail.ru>
2020-11-23 01:33:14 +0200

    Coroutine for CarDodgeViews

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,9 +1,25 @@
 ﻿using Events;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace UI {
 
     public class GameScreen : MonoBehaviour {
+
+        [SerializeField]
+        private List<CarDodgeView> _carDodgeViewsList;
+
+        public IEnumerator CarDodgeViewsCoroutine() {
+            for (var i = 0; _carDodgeViewsList.Count > i; i++) {
+                _carDodgeViewsList[i].Init();
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        private void OnEnable() {
+            StartCoroutine(CarDodgeViewsCoroutine());
+        }
 
         private void OnDisable() {
             RenderManager.Instance.ReleaseTextures();
