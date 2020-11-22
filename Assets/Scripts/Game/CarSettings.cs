41d371c152a97ee341cf8028fe3ddde2006bce0e
gg <71404862+gqggqg@users.noreply.github.com>
2020-11-22 23:20:02 +0200

    setting odin attributes in CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -6,21 +6,29 @@ namespace Game {
     [CreateAssetMenu(fileName = "CarSettings", menuName = "CarSettings")]
     public class CarSettings : ScriptableObject {
 
+        public enum CarType {
+            Enemy,
+            Player,
+        }
+
+        public CarType carType;
+
         [FoldoutGroup("Speed", false)]
         public float maxSpeed;
         [FoldoutGroup("Speed")]
         [InfoBox("Speed is beeing increased by acceleration every frame", InfoMessageType.Info)]
         public float acceleration;
 
-        [BoxGroup("Score")]
         [ValidateInput(nameof(ValidateDodgeScore))]
+        [ShowIf("carType", CarType.Enemy)]
         public int dodgeScore;
 
-        [BoxGroup("Headlights")]
-        [Range(1f, 5f)]
-        public int headlightRange;
-
+        [ShowIf("carType", CarType.Enemy)]
         public GameObject renderCarPrefab;
+
+        [Range(1f, 5f)]
+        [ShowIf("carType", CarType.Player)]
+        public int headlightRange;
 
         private bool ValidateDodgeScore(int score) {
             return score >= 0;
