b5cb2a91ad152a34bed2f9be806d9bc0201d30ee
Denis <denis.zhuravkov@gmail.com>
2020-11-22 19:49:49 +0200

    Created "ScriptableStringValue" scriptable object

---
+++ Assets/Scripts/Game/ScriptableValues/ScriptableStringValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game {
+    
+    [CreateAssetMenu(fileName = "ScriptableStringValue", menuName = "ScriptableStringValue")]
+    public class ScriptableStringValue : ScriptableObject {
+    
+        public string value = String.Empty;
+    }
+}
