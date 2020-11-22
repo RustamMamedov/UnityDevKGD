85655b11f3b95f2ab280f5e31f4a18fecc08fbd2
Dan <kdanil01@mail.ru>
2020-11-22 20:07:26 +0200

    Changed CarSettings

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -9,8 +9,8 @@ namespace Game {
 
         [BoxGroup("Score")]
         [ValidateInput(nameof(ValidateDodgeScore))]
-
         public int dodgeScore;
+
         [FoldoutGroup("Speed")]
         public float maxSpeed;
 
@@ -18,8 +18,15 @@ namespace Game {
         [InfoBox("Speed is increased by acceleration every frame.", InfoMessageType.Warning)]
         public float acceleration;
 
-        [Range(1, 5)]
+        [Range(1, 40)]
         public float carLightLength;
+
+        [BoxGroup("UI Icon Utilities")]
+        public GameObject renderCarPrefab;
+        [BoxGroup("UI Icon Utilities")]
+        public Vector3 cameraPosition;
+        [BoxGroup("UI Icon Utilities")]
+        public Quaternion cameraRotation;
 
         private bool ValidateDodgeScore(int score) {
             return (score > 0);
