999a4157df5b2cd3c4287d560c85203c5b771376
Evgenii <jekaweek1@gmail.com>
2020-11-22 15:24:08 +0200

    change GameScreen class

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -2,21 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Events;
+using Game;
 namespace UI {
     public class GameScreen : MonoBehaviour {
 
         [SerializeField]
         private EventListener _saveEventListener;
 
-        private void Awake() {
+        [SerializeField]
+        private EventListener _carDodgeEventListener;
+
+        [SerializeField]
+        private ScriptableStringValue _carDodgeName;
+
+        [SerializeField]
+        private List<CarSettings> _carSettings = new List<CarSettings>();
+
+        [SerializeField]
+        private List<CarDodgeView> _carDodgeViews = new List<CarDodgeView>();
+
+        private void OnEnable() {
             _saveEventListener.OnEventHappened += ShowLeaderboardScreen;
+            _carDodgeEventListener.OnEventHappened += DodgeCounter;
+            StartCoroutine(InitCarDodgeViewCoroutine());
+        }
+
+        IEnumerator InitCarDodgeViewCoroutine() {
+            for(int i = 0; i < _carDodgeViews.Count; i++) {
+                _carDodgeViews[i].Init(_carSettings[i].renderCarPrefab, _carSettings[i].renderCameraPosition, _carSettings[i].renderCameraRotation);
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         private void ShowLeaderboardScreen() {
             UIManager.Instance.ShowLeaderboardScreen();
         }
 
+        private void DodgeCounter() {
+            for(int i = 0; i < _carSettings.Count; i++) {
+                if(_carSettings[i].name == _carDodgeName.value) {
+                    _carDodgeViews[i].DodgeCounter();
+                    break;
+                }
+            }
+        }
+
         private void OnDisable() {
+            _saveEventListener.OnEventHappened -= ShowLeaderboardScreen;
+            _carDodgeEventListener.OnEventHappened -= DodgeCounter;
             RenderManager.Instance.ReleaseTextures();
         }
     }
