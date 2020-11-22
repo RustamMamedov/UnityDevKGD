082d1e76eaa59b951c67a2cdadb8fdca196117a5
Denis <denis.zhuravkov@gmail.com>
2020-11-22 16:54:46 +0200

    RenderManager created and added to UI prefab

---
+++ Assets/Scripts/UI/RenderManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI {
+    
+    public class RenderManager : MonoBehaviour {
+    
+        public static RenderManager Instance;
+
+        [SerializeField]
+        private Camera _renderCamera;
+
+        private RenderTexture _texture;
+
+        [SerializeField] 
+        private Transform _carRootTransform;
+
+        private void Awake() {
+            Instance = this;
+        }
+
+        public RenderTexture Render(GameObject prefab) {
+            var carInstance = Instantiate(prefab, _carRootTransform);
+            
+            _texture = RenderTexture.GetTemporary(64, 64, 16);
+            _texture.antiAliasing = 8;
+            _texture.Create();
+
+            _renderCamera.targetTexture = _texture;
+            _renderCamera.Render();
+            _renderCamera.targetTexture = null;
+
+            Destroy(carInstance);
+            return _texture;
+        }
+
+        public void ReleaseTextures() {
+            RenderTexture.ReleaseTemporary(_texture);
+        }
+    }
+}
+
