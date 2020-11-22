33354264e515afc4781e113365af62322bc5ae24
gg <71404862+gqggqg@users.noreply.github.com>
2020-11-22 20:26:28 +0200

    render camera

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -6,22 +6,18 @@ namespace UI {
     public class GameScreen : MonoBehaviour {
 
         [SerializeField]
-        private EventListener _carCollisionEventListener;
-
-        [SerializeField]
         private EventListener _gameSavedEventListener;
 
         private void Awake() {
-            _carCollisionEventListener.OnEventHappened += OnCarCollision;
             _gameSavedEventListener.OnEventHappened += ShowLeaderboard;
-        }
-
-        private void OnCarCollision() {
-
         }
 
         private void ShowLeaderboard() {
             UIManager.Instance.ShowLeaderboardsScreen();
+        }
+
+        private void OnDisable() {
+            RenderManager.Instance.ReleaseTextures();
         }
     }
 }
