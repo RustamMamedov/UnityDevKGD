142057dfb23cbc9957e0a0fd8e283d26658969bc
Denis <denis.zhuravkov@gmail.com>
2020-11-22 20:01:21 +0200

    CarDodged event handling by CarDodgeView

--- Assets/Scripts/UI/CarDodgeView.cs
+++ Assets/Scripts/UI/CarDodgeView.cs
@@ -1,4 +1,6 @@
-﻿using Game;
+﻿using System;
+using Events;
+using Game;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,18 +8,48 @@ namespace UI {
     
     public class CarDodgeView : MonoBehaviour {
 
+        private int _carCounter = 0;
+
         [SerializeField] 
         private RawImage _carImage;
 
         [SerializeField] 
+        private Text _carCounterText;
+
+        [SerializeField] 
         private CarSettings _carSettings;
 
+        [SerializeField] 
+        private ScriptableStringValue _carDodgedName;
+
+        [SerializeField] 
+        private EventListener _carDodgedEventListener;
+
         private void OnEnable() {
-            Init();
+            _carDodgedEventListener.OnEventHappened += OnCarDodgedBehaviour;
+        }
+
+        private void OnDisable() {
+            _carDodgedEventListener.OnEventHappened -= OnCarDodgedBehaviour;
         }
 
         public void Init() {
-            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab);
+            _carCounter = 0;
+            _carCounterText.text = "0";
+            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab, 
+                                 _carSettings.cameraPosition, _carSettings.cameraRotation);
+        }
+
+        private void OnCarDodgedBehaviour() {
+            if (String.IsNullOrEmpty(_carDodgedName.value)) {
+                return;
+            }
+            Debug.Log(_carDodgedName.value);
+
+            if (_carSettings.name == _carDodgedName.value) {
+                _carCounterText.text = $"{++_carCounter}";
+                _carDodgedName.value = String.Empty;
+            }
         }
     }
 }
