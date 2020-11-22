b759e417075a12ba2a04df16f893c8de0a057491
Valeriy <mr.robariii@gmail.com>
2020-11-22 10:32:49 +0200

    CarSettings script updated

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -10,15 +10,19 @@ namespace Game {
         [ValidateInput(nameof(ValidateDodgeScore))]
         public int dodgeScore;
         [BoxGroup("Score")]
+
         public int dodgeScore2;
         [FoldoutGroup("Speed")]
         public float maxSpeed;
         [FoldoutGroup("Speed")]
         [InfoBox("Speed is beeing increased by this value every frame")]
         public float acceleration;
+
         [BoxGroup("LightSettings")]
         [Range(1f, 5f)]
-        public int lightDistance; 
+        public int lightDistance;
+
+        public GameObject renderCarPrefab;
 
         private bool ValidateDodgeScore(int score) {
             return score >= 0;
