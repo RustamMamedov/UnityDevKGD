90fbca5278ce4a178a5d6eccbe23ff0d64a9e782
Denis <denis.zhuravkov@gmail.com>
2020-11-22 20:23:18 +0200

    "AudioEvent" script created
    It plays audio when event it's being listening to is happend

---
+++ Assets/Scripts/Audio/AudioEvent.cs
@@ -0,0 +1,28 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Audio {
+    
+    [RequireComponent(typeof(EventListener))]
+    public class AudioEvent : MonoBehaviour {
+
+        [SerializeField] 
+        private AudioSourcePlayer _audioSourcePlayer;
+        
+        private EventListener _eventListener;
+
+        private void Awake() {
+            _eventListener = GetComponent<EventListener>();
+            _eventListener.OnEventHappened += OnEventBehaviour;
+        }
+        
+        private void OnDestroy() {
+            _audioSourcePlayer.Stop();
+            _eventListener.OnEventHappened -= OnEventBehaviour;
+        }
+
+        private void OnEventBehaviour() {
+            _audioSourcePlayer.Play();
+        }
+    }
+}
