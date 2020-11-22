d659c9c5819ad8d29d95f06f87c7e634afea63a4
SaraJess <sarycheva.00@mail.ru>
2020-11-22 21:35:30 +0200

    Light use Range

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -9,9 +9,13 @@ namespace Game {
         [SerializeField]
         private Car _car;
 
-        private void OnDrawGizmosSelected() {
+        [SerializeField]
+        private Light _light;
+
+        private void OnDrawGizmos() {
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _car.CarSettings.lightDistance, 1f, 1f);
+            _light.range = _car.CarSettings.lightDistance * 10;
         }
     }
 }
