aea4a8df9a68572e3f156cb5f2b121a15971c5d4
vladislav <vladislavnikwork@gmail.com>
2020-11-23 16:14:52 +0200

    Added sounds for all buttons, dodge and crash of player's car

---
+++ Assets/Scripts/Audio/PlayerAudioHandler.cs
@@ -0,0 +1,40 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Audio {
+
+    public class PlayerAudioHandler : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _audioDodgePlayer;
+
+        [SerializeField]
+        private AudioSourcePlayer _audioCrashPlayer;
+
+        [SerializeField]
+        private EventListener _playerDodgeCar;
+
+        [SerializeField]
+        private EventListener _playerCrashCar;
+
+        private void Awake() {
+            _playerDodgeCar.OnEventHappened += OnEventDodge;
+            _playerCrashCar.OnEventHappened += OnEventCrash;
+        }
+
+        private void OnDestroy() {
+            _audioDodgePlayer.Stop();
+            _audioCrashPlayer.Stop();
+            _playerDodgeCar.OnEventHappened -= OnEventDodge;
+            _playerCrashCar.OnEventHappened -= OnEventCrash;
+        }
+
+        private void OnEventDodge() {
+            _audioDodgePlayer.Play();
+        }
+
+        private void OnEventCrash() {
+            _audioCrashPlayer.Play();
+        }
+    }
+}
