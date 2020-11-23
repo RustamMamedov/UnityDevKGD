77ad760a155446f087bc0617d82c03ee3d65eca0
vladislav <vladislavnikwork@gmail.com>
2020-11-23 08:49:59 +0200

    render camera

--- Assets/Scripts/Game/CarSettings.cs
+++ Assets/Scripts/Game/CarSettings.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using Sirenix.OdinInspector;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace Game {
 
@@ -8,20 +8,21 @@ namespace Game {
 
         [FoldoutGroup("Speed", false)]
         public float maxSpeed;
-
         [FoldoutGroup("Speed")]
         [InfoBox("Speed is beeing increased by acceleration every frame", InfoMessageType.Warning)]
         public float acceleration;
-
-        [BoxGroup("Lights")]
-        [Range(1f, 5f)]
-        public float lightDistance;
 
         [BoxGroup("Speed/Score")]
         [ValidateInput(nameof(ValidateDodgeScore))]
         public int dodgeScore;
         [BoxGroup("Speed/Score")]
         public int dodgeScore2;
+
+        [BoxGroup("Lights")]
+        [Range(1f, 5f)]
+        public float lightDistance;
+
+        public GameObject renderCarPrefab;
 
         private bool ValidateDodgeScore(int score) {
             return score >= 0;
---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,23 @@
+﻿using Events;
+using UnityEngine;
+
+namespace UI {
+
+    public class GameScreen : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _saveEventListener;
+
+        private void Awake() {
+            _saveEventListener.OnEventHappened += OnGameSaved;
+        }
+
+        private void OnGameSaved() {
+            UIManager.Instance.ShowLeaderboardScreen();
+        }
+
+        private void OnDisable() {
+            RenderManager.Instance.ReleaseTextures();
+        }
+    }
+}
