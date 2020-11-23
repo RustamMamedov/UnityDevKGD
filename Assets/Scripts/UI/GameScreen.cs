d37f7e32e755d58b85d4f67ed4bd071667612bdc
Andrey <DrEMartstud@gmail.com>
2020-11-23 16:25:33 +0200

    Added CarDodged logic to prefabs and GameSreen

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,12 +1,28 @@
 ﻿using Events;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace UI {
 
     public class GameScreen : MonoBehaviour {
         
+        [SerializeField]
+        private List<CarDodgedView> _carsDodgedViews = new List<CarDodgedView>();
+
+        private void OnEnable() {
+            StartCoroutine(CarRendererCoroutine());
+        }
+
         private void OnDisable() {
             RenderManager.Instance.ReleaseTextures();
+        }
+
+        private IEnumerator CarRendererCoroutine() {
+            foreach (var carsDodgedView in _carsDodgedViews) {
+                carsDodgedView.Init();
+                yield return null;
+            }
         }
     }
 }
