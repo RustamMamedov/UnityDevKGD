d6eb8116a0f5eafa72bbcaefdc68cf270cd4420b
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-23 16:43:36 +0200

    Modified CarLight script and connected it to spot light

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -10,11 +10,19 @@ namespace Game {
         [SerializeField]
         private Color _gizmosColor = Color.red;
 
+        [SerializeField]
+        private Light _light;
+
+        private float multyplier = 10f;
+
+        private void Awake() {
+            _light.range = _carSettings.lightRange * multyplier;
+        }
         private void OnDrawGizmos() {
 
             Gizmos.color = _gizmosColor;
             Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.DrawFrustum(Vector3.zero, 45f, _carSettings.lightRange,0f,2f);
+            Gizmos.DrawFrustum(Vector3.zero, 45f, _carSettings.lightRange, 0f, 2f);
         }
     }
 }
