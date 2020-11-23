6042c2e85cf3e3e1fc04a90510196f8782447951
Valeriy <mr.robariii@gmail.com>
2020-11-23 10:59:19 +0200

    GameScreen script update

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,16 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Events;
+using Game;
+using System.Collections;
 
 namespace UI {
 
     public class GameScreen : MonoBehaviour {
 
+        [SerializeField]
+        private EventListener _carDodgeEventListener;
+
+        [SerializeField]
+        private EnemyCarToDodge _carDodged;
+
+        [SerializeField]
+        private List<CarSettings> _carSettings = new List<CarSettings>();
+
+        [SerializeField]
+        private List<CarDodgeView> _carDodgeViews = new List<CarDodgeView>();
+
+        private bool _canSetScore = true;
+
+        private void OnEnable() {
+            _carDodgeEventListener.OnEventHappened += DodgeCounter;
+            StartCoroutine(InitCarDodgeView());
+        }
+
+        private void ShowLeaderboardScreen() {
+            UIManager.instance.ShowLeaderboardScreen();
+        }
+
+        private void DodgeCounter() {
+            if(_canSetScore == true) {
+                for (int i = 0; i < _carSettings.Count; i++) {
+                    var name = _carDodged.currentCar.name.Trim(new char[] { '(', 'C', 'l', 'o', 'n', 'e', ')' });
+                    if (_carSettings[i].name == name) {
+                        Debug.Log(name);
+                        _carDodgeViews[i].DodgeCounter();
+                        StartCoroutine(CoolDown());
+                        
+                    }
+                }
+            }
+        }
 
         private void OnDisable() {
-
+            _carDodgeEventListener.OnEventHappened -= DodgeCounter;
             RenderManager.instance.ReleaseTextures();
+        }
+
+        IEnumerator InitCarDodgeView() {
+            for (int i = 0; i < _carDodgeViews.Count; i++) {
+                _carDodgeViews[i].Init(_carSettings[i].renderCarPrefab, _carSettings[i].cameraPos);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        IEnumerator CoolDown() {
+            _canSetScore = false;
+            yield return new WaitForSeconds(1f);
+            _canSetScore = true;
+
         }
     }
 }
-
