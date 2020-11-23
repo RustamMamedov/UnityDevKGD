d588e4accb4f2b118cee3ccc5fddc0b11cfe09b5
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-23 17:43:56 +0200

    Add DodgeCountView script

---
+++ Assets/Scripts/UI/DodgeCountView.cs
@@ -0,0 +1,39 @@
+﻿using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class DodgeCountView : MonoBehaviour {
+
+        // Fields.
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        [SerializeField]
+        private RawImage _rawImage;
+
+        private RenderTexture _renderTexture;
+
+
+        // Life cycle.
+
+        public void Start() {
+            _renderTexture = RenderManager.Instance.Render(_carSettings.renderableCarPrefab);
+            _rawImage.texture = _renderTexture;
+        }
+
+        private void OnDestroy() {
+            _rawImage.texture = null;
+            if (_renderTexture != null) {
+                _renderTexture.Release();
+            }
+        }
+
+
+    }
+
+}
