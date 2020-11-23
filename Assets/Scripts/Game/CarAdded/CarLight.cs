c9aed25bd293649cdd24af17e4c85775a9e41c51
Ilya <elya5911@mail.ru>
2020-11-23 02:44:57 +0200

    Logic corrected LightCar

--- Assets/Scripts/Game/CarAdded/CarLight.cs
+++ Assets/Scripts/Game/CarAdded/CarLight.cs
@@ -9,10 +9,23 @@ namespace Game {
         [SerializeField]
         private CarSettings _settings;
 
-        private void OnDrawGizmos() {
-            Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.DrawFrustum(new Vector3(0f,0f,-1f), 30f, _settings.lenghLightCar, 1f, 1f);
+        [SerializeField]
+        private Light _light;
+
+        [SerializeField]
+        private float _constantLenghCar;
+
+        private void OnEnable() {
+            _light.range = _settings.lenghLightCar * _constantLenghCar;
         }
+
+        private void OnDrawGizmos() {
+            _light.range = _settings.lenghLightCar * _constantLenghCar;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawFrustum(new Vector3(0f,0f,-1f), 30f, _settings.lenghLightCar* _constantLenghCar, 1f, 1f);
+        }
+
+
 
     }
 }
