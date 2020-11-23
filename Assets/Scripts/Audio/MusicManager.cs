673c3312bb24aab66bc8cecb0edb0519c7664013
Oleg <oleg.kalyugin2001@mail.ru>
2020-11-23 17:16:26 +0200

    Audio files

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Audio {
+
+    public class MusicManager : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _menuMusicPlayer;
+
+        [SerializeField]
+        private AudioSourcePlayer _gameMusicPlayer;
+
+        [SerializeField]
+        private AudioSourcePlayer _dodged;
+
+        [SerializeField]
+        private AudioSourcePlayer _carCollision;
+
+        [SerializeField]
+        private EventListener _carDodgedEventListener;
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        private void OnEnable() {
+            _carDodgedEventListener.OnEventHappened += OnCarDodged;
+            _carCollisionEventListener.OnEventHappened += OnCarCollision;
+        }
+
+        private void OnCarDodged() {
+            _dodged.Play();
+        }
+
+        private void OnCarCollision() {
+            _carCollision.Play();
+        }
+
+        public void PlayMenuMusic() {
+            PauseMusic();
+            _menuMusicPlayer.Play();
+        }
+        public void PlayGameMusic() {
+            PauseMusic();
+            _gameMusicPlayer.Play();
+
+        }
+
+        private void PauseMusic() {
+            _gameMusicPlayer.Stop();
+            _menuMusicPlayer.Stop();
+        }
+    }
+}
