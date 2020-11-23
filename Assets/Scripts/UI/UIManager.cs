4afadeb17ee853f4e59d329da4c956a0a867835e
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-23 12:42:46 +0200

    Added MusicManager logic to UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Audio;
 
 namespace UI {
 
@@ -16,6 +17,9 @@ namespace UI {
 
         [SerializeField] private GameObject _leaderboardScreen;
 
+        [SerializeField]
+        private MusicManager _musicManager;
+
         private void Awake() {
             if (Instansce != null) {
                 Destroy(gameObject);
@@ -24,6 +28,10 @@ namespace UI {
 
             Instansce = this;
             DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start() {
+            ShowMenuScreen();
         }
 
 
@@ -62,6 +70,7 @@ namespace UI {
         public void ShowMenuScreen() {
             HideAllScreens();
             _menuScreen.SetActive(true);
+            _musicManager.PlayMenuMusic();
         }
 
         public void ShowGameScreen() {
