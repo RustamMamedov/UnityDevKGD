922c63b6ba2ea728fda0dcca1b41d27978a182d1
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-23 16:49:41 +0200

    Cars' icons rendering

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,13 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game;
 
 namespace UI {
 
     public class GameScreen : MonoBehaviour {
 
+        [SerializeField]
+        private List<CarSettings> _carSettings = new List<CarSettings>();
+        [SerializeField]
+        private List<CarDodgedView> _carsDodgedViews = new List<CarDodgedView>();
+
+        private bool _isCoolDownEnded = true;
+
+        private void OnEnable() {
+            StartCoroutine(InitOfCarDodgeView());
+        }
         private void OnDisable() {
             RenderManager.Instance.ReleaseTexture();
+        }
+
+        private void DodgeCounter() {
+            if (_isCoolDownEnded) {
+                for (int i = 0; i < _carSettings.Count;i++) {
+                    
+                }
+            }
+        }
+
+        IEnumerator InitOfCarDodgeView() {
+            for (int i = 0; i < _carsDodgedViews.Count; i++) {
+                _carsDodgedViews[i].Init(_carSettings[i].renderCarPrefab,_carSettings[i].renderCameraPosition);
+            }
+            yield return new WaitForEndOfFrame();
         }
     }
 }
