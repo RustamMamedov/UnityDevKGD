adfac281beb01cab513980943f9c07ef2dbe0d6f
Valeriy <mr.robariii@gmail.com>
2020-11-23 10:58:02 +0200

    RequiredEventSoundPlay script created

---
+++ Assets/Scripts/Audio/RequiredEventSoundPlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+using Audio;
+
+namespace Game {
+
+    public class RequiredEventSoundPlay : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _requiredEventListener;
+
+        [SerializeField]
+        private AudioSourcePlayer _source;
+
+        private bool _canPlay = true;
+
+        private void OnEnable() {
+            _requiredEventListener.OnEventHappened += PlaySound;
+        }
+        private void OnDisable() {
+            _requiredEventListener.OnEventHappened -= PlaySound;
+        }
+
+
+        private void PlaySound() {
+            if(_canPlay == true) {
+                _source.Play();
+                StartCoroutine(CoolDown());
+            }
+        }
+
+        IEnumerator CoolDown() {
+            _canPlay = false;
+            yield return new WaitForSeconds(1f);
+            _canPlay = true;
+        }
+
+    }
+}
