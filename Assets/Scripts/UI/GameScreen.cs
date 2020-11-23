8df5645bd828b476702556c17eaf75b8a7e41b01
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-23 13:11:11 +0200

    created GameScreen

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace UI {
+
+    public class GameScreen : MonoBehaviour {
+
+
+	[SerializeField]
+	private EventListener _saveEventListener;
+
+	private void Awake() {
+	    _saveEventListener.OnEventHappend += OnGameSaved;
+	}
+
+	private void OnGameSaved() {
+	    UIManager.Instance.ShowLeaderboardScreen();
+	}
+
+    }
+}
