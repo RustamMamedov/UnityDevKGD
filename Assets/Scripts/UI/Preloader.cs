acf75fe7290e1a72c976486590ff773a67328693
Alexandr <navacar@mail.ru>
2020-11-23 18:58:45 +0200

    fixes

--- Assets/Scripts/UI/Preloader.cs
+++ Assets/Scripts/UI/Preloader.cs
@@ -17,14 +17,10 @@ namespace UI {
         private IEnumerator LoadMenuScene() {
             var asyncOperation = SceneManager.LoadSceneAsync("Menu");
             asyncOperation.allowSceneActivation = false;
-            while (asyncOperation.progress < .9f) {
-                _sceneLoadingValue.value = asyncOperation.progress;
+            while (!asyncOperation.isDone) {
+                _sceneLoadingValue.value = asyncOperation.progress / .9f;
                 yield return null;
             }
-            _sceneLoadingValue.value = 1f;
-
-            yield return new WaitForSeconds(2f);
-            asyncOperation.allowSceneActivation = true;
         }
 
     }
