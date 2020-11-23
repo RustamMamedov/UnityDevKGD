0cbeca1cf2db8b3e6e784c22f2df494ce9cde792
Nikita <Nkanunov@gmail.com>
2020-11-23 13:45:19 +0200

    Added car dodged sound

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Game;
 using UnityEngine;
-
+using Audio;
 namespace UI {
 
     public class ScoreManager : MonoBehaviour {
@@ -13,11 +13,15 @@ namespace UI {
         [SerializeField]
         private List<CarSettings> _carSettings;
 
+        [SerializeField]
+        private AudioSourcePlayer _audioSoursePlayer;
         private void OnTriggerEnter(Collider other) {
             for (int i = 0; i < _enemyCars.Count; i++) {
                 if (_enemyCars[i].tag.Equals(other.tag)) {
                     _currentScore.value += _enemyCars[i].DodgeScore.dodgeScore;
                     _carSettings[i].differentCarCount++;
+                    _audioSoursePlayer.Play();
+
                 }
             }
         }
