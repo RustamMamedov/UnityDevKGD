0f715457a0e843acf8e09f543332a485dedc72dc
KrupinMA <krupin732@yandex.ru>
2020-11-23 16:45:57 +0200

    RenderManager

---
+++ Assets/Scripts/UI/RenderManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace UI {
+
+    public class RenderManager : MonoBehaviour {
+
+        public static RenderManager Instance;
+
+        [SerializeField]
+        private Camera _renderCamera;
+
+        [SerializeField]
+        private Transform _rootTransform;
+
+        private RenderTexture _texture;
+
+        private void Awake() {
+            Instance = this;
+        }
+
+        public RenderTexture Render(GameObject prefab,float FOV) {
+            var carInstance = Instantiate(prefab, _rootTransform);
+            _texture = RenderTexture.GetTemporary(64, 64, 16);
+            _texture.antiAliasing = 8;
+            _texture.Create();
+            _renderCamera.fieldOfView = FOV;
+            _renderCamera.targetTexture = _texture;
+            _renderCamera.Render();
+            _renderCamera.targetTexture = null;
+            
+            Destroy(carInstance);
+            return _texture;
+        }
+
+        public void ReleaseTextures() {
+            RenderTexture.ReleaseTemporary(_texture);
+        }
+    }
+}
