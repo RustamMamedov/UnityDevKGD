51d8a80ad9560ea2a96bc397c0ca036b6a8116c4
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-24 01:04:46 +0200

    Add DodgedCar scriptable reference

---
+++ Assets/Scripts/Values/ScriptableCarSettingsReference.cs
@@ -0,0 +1,14 @@
+﻿using Game;
+using UnityEngine;
+
+namespace Values {
+
+    [CreateAssetMenu(fileName = "CarSettings Reference", menuName = "Values/CarSettings Reference")]
+    public class ScriptableCarSettingsReference : ScriptableObject {
+
+        public CarSettings reference;
+
+
+    }
+
+}
