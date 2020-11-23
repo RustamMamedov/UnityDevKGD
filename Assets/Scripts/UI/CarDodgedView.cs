bb5b0820d31e6d84ffc1fc8f0dde30c8dee115fe
Andrey <DrEMartstud@gmail.com>
2020-11-23 16:03:12 +0200

    Created counter system for specific dodged cars

--- Assets/Scripts/UI/CarDodgedView.cs
+++ Assets/Scripts/UI/CarDodgedView.cs
@@ -1,6 +1,8 @@
-﻿using Game;
+﻿using Events;
+using Game;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 namespace UI {
 
@@ -12,12 +14,49 @@ namespace UI {
         [SerializeField]
         private CarSettings _carSettings;
 
+        [SerializeField] 
+        private EventListener _carDodgedEventListener;
+        
+        [SerializeField] 
+        private Text _carsDodgedLabel;
+       
+        private int _carsDodgedCount;
+
         private void OnEnable() {
-            Init();
+            SupscribeToEvents();
+            SetToZeroLabelAndCount();
         }
 
-        public void Init() {
-            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab);
+        private void OnDisable() {
+            UnsupscribeFromEvents();
+            SetToZeroLabelAndCount();
         }
+
+        private void SupscribeToEvents() {
+            _carDodgedEventListener.OnEventHappened += OnCarDodged;
+        }
+        
+        public void Init() {
+            _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab, _carSettings.cameraPosition, _carSettings.cameraRotation);
+        }
+
+        private void SetToZeroLabelAndCount() {
+            _carsDodgedCount = 0;
+            _carsDodgedLabel.text = $"{_carsDodgedCount}";
+        }
+        
+        private void UnsupscribeFromEvents() {
+            _carDodgedEventListener.OnEventHappened -= OnCarDodged;
+        }
+        
+        private void OnCarDodged() {
+            StartCoroutine(IncrementDodgedCarsCoroutine());
+        }
+
+        private IEnumerator IncrementDodgedCarsCoroutine() {
+                _carsDodgedCount++;
+                _carsDodgedLabel.text = $"{_carsDodgedCount}";
+                yield return null;
+        } 
     }
 }
