b7069affe4b3385926a270e455710ebe0c8d4e19
Nikita Shpulin <shpulinn@mail.ru>
2020-11-23 18:10:22 +0300

    render coroutine added

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,4 +1,7 @@
 ﻿using Events;
+using Game;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UI {
@@ -7,6 +10,16 @@ namespace UI {
 
         [SerializeField]
         private EventListener _carCollisionEventListener;
+
+        [SerializeField]
+        private List<CarSettings> _carSettings = new List<CarSettings>();
+
+        [SerializeField]
+        private List<CarDodgeView> _carsDodgedViews = new List<CarDodgeView>();
+
+        private void OnEnable() {
+            StartCoroutine(CarRendererCoroutine());
+        }
 
         private void Awake() {
             _carCollisionEventListener.OnEventHappened += OnCarCollision;
@@ -18,6 +31,12 @@ namespace UI {
 
         private void OnDisable() {
             RenderManager.Instance.ReleaseTextures();
+        }
+        private IEnumerator CarRendererCoroutine() {
+            for (int i = 0; i < _carsDodgedViews.Count; i++) {
+                _carsDodgedViews[i].Init(_carSettings[i].renderCarPrefab, _carSettings[i].cameraPosition, _carSettings[i].cameraRotation);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
