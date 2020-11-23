df33b7ef0d12c26b61a79c3a6ddf14c0c4e658be
Evgenii <jekaweek1@gmail.com>
2020-11-23 12:42:25 +0200

    add CarLight logic

---
+++ Assets/Scripts/Game/CarLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    public class CarLight : MonoBehaviour {
+
+        [SerializeField]
+        private CarSettings _carSettings;
+
+        [SerializeField]
+        private Light _light;
+
+        private float _offset = 10f;
+
+        private void Awake() {
+            _light.range = _carSettings.lightDistance * _offset;
+        }
+    }
+}
