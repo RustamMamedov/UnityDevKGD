78e77fbf558d3a3ecc57931e3ccd680b5778e193
Valeriy <mr.robariii@gmail.com>
2020-11-23 10:59:27 +0200

    RenderManager script update

--- Assets/Scripts/UI/RenderManager.cs
+++ Assets/Scripts/UI/RenderManager.cs
@@ -24,11 +24,12 @@ namespace UI {
             instance = this;
         }
 
-        public RenderTexture Render(GameObject prefab) {
+        public RenderTexture Render(GameObject prefab, Vector3 cameraPos) {
             var carInstance = Instantiate(prefab, _carRootTransform);
             var _texture = RenderTexture.GetTemporary(64, 64, 16);
             _texture.antiAliasing = 8;
             _texture.Create();
+            _renderCamera.transform.position = cameraPos;
             _renderCamera.targetTexture = _texture;
             _renderCamera.Render();
             _renderCamera.targetTexture = null;
