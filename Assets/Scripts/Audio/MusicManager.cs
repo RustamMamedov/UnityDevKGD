d9d97e5c01e392fcb9658403a16eeeb12c94b3f6
gg <71404862+gqggqg@users.noreply.github.com>
2020-11-23 12:34:20 +0200

    dodge and collision sounds added

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Events;
 
 namespace Audio {
 
@@ -6,6 +7,31 @@ namespace Audio {
 
         [SerializeField]
         private AudioSourcePlayer _menuMusicPlayer;
+
+        [SerializeField]
+        private AudioSourcePlayer _carDodgeSoundPlayer;
+
+        [SerializeField]
+        private AudioSourcePlayer _carCollisionSoundPlayer;
+
+        [SerializeField]
+        private EventListener _carDodgeEventListener;
+
+        [SerializeField]
+        private EventListener _carCollisionEventListener;
+
+        private void Awake() {
+            _carDodgeEventListener.OnEventHappened += PlayCarDodgeSound;
+            _carCollisionEventListener.OnEventHappened += PlayCarCollisionSound;
+        }
+
+        private void PlayCarDodgeSound() {
+            _carDodgeSoundPlayer.Play();
+        }
+
+        private void PlayCarCollisionSound() {
+            _carCollisionSoundPlayer.Play();
+        }
 
         public void PlayrMenuMusic() {
             _menuMusicPlayer.Play();
