57ea5b64868d40b8b9def2d92111925cef03a816
Evgenii <jekaweek1@gmail.com>
2020-11-23 14:55:13 +0200

    add Sound Logic in Gameplay

---
+++ Assets/Scripts/Audio/RequiredEventSoundPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Audio {
+
+    public class RequiredEventSoundPlay : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _eventListener;
+
+        [SerializeField]
+        private AudioSourcePlayer _audioSource;
+
+        private void OnEnable() {
+            _eventListener.OnEventHappened += _audioSource.Play;
+        }
+
+        private void OnDisable() {
+            _eventListener.OnEventHappened -= _audioSource.Play;
+        }
+    }
+}
