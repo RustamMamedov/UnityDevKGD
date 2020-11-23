b50d5cdf7dd7a133bd3543f285df65b9b2e4eccf
vladislav <vladislavnikwork@gmail.com>
2020-11-23 13:18:37 +0200

    Writed a coroutine that will initialize CarDodgeView one by one per frame

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,12 +1,28 @@
-﻿using Events;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace UI {
 
     public class GameScreen : MonoBehaviour {
 
+        [SerializeField]
+        private List<CarDodgeView> _carDodgeViews = new List<CarDodgeView>();
+
+        private void OnEnable() {
+            StartCoroutine(CarDodgeCoroutine());
+        }
+
         private void OnDisable() {
             RenderManager.Instance.ReleaseTextures();
+        }
+
+        private IEnumerator CarDodgeCoroutine() {
+
+            for (var numView = 0; numView < _carDodgeViews.Count; numView++) {
+                _carDodgeViews[numView].Init();
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
