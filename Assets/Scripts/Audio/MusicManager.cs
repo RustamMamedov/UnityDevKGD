afafa9beaddbe49d27ace5b56f701a7f6339eb33
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-11-23 16:14:29 +0200

    add Audio logic

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio {
+
+    public class MusicManager : MonoBehaviour {
+
+	[SerializeField]
+	private AudioSourcePlayer _menuMusicPlayer;
+
+	public void PlayMenuMusic() {
+	    _menuMusicPlayer.Play();
+	}
+    }
+}
