b2b23e02a45c415ab3b15190ad442741539f1a54
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-23 17:45:11 +0200

    Sounds of dodge and collision were added

---
+++ Assets/Scripts/Audio/AudioCar.cs
@@ -0,0 +1,38 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Audio {
+
+    public class AudioCar : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _dodgeSound;
+
+        [SerializeField]
+        private AudioSourcePlayer _collisionSound;
+
+        [SerializeField]
+        private EventListener _carDodgedEvent;
+
+        [SerializeField]
+        private EventListener _carCollisionEvent;
+
+        private void OnEnable() {
+            _carCollisionEvent.OnEventHappened += OnCarCollision;
+            _carDodgedEvent.OnEventHappened += OnCarDodged;
+        }
+
+        private void OnDisable() {
+            _carCollisionEvent.OnEventHappened -= OnCarCollision;
+            _carDodgedEvent.OnEventHappened -= OnCarDodged;
+        }
+
+        private void OnCarDodged() {
+            _dodgeSound.Play();
+        }
+
+        private void OnCarCollision() {
+            _collisionSound.Play();
+        }
+    }
+}
