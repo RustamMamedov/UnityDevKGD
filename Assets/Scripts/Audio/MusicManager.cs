4935cc38132ed683bd772f54cf3b86d1ea3c9be1
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-23 22:04:19 +0200

    Improve music control in MusicManager script

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -12,11 +12,22 @@ namespace Audio {
         [SerializeField]
         private AudioSourcePlayer _menuMusicPlayer;
 
+        private AudioSourcePlayer _currentlyPlaying = null;
+
 
         // Methods.
 
+        public void StopMusic() {
+            if (_currentlyPlaying != null) {
+                _currentlyPlaying.Stop();
+                _currentlyPlaying = null;
+            }
+        }
+
         public void PlayMenuMusic() {
+            StopMusic();
             _menuMusicPlayer.Play();
+            _currentlyPlaying = _menuMusicPlayer;
         }
 
 
