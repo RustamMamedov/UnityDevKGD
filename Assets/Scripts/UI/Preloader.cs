657a396d3a45a0625c008b163ddf6ba67e910c96
Ilya <elya5911@mail.ru>
2020-11-23 18:59:02 +0200

    Correct Some Changes

--- Assets/Scripts/UI/Preloader.cs
+++ Assets/Scripts/UI/Preloader.cs
@@ -17,14 +17,11 @@ namespace UI {
         private IEnumerator LoadMenuScene() {//карутина - работает как поток из-за IEnumerator.
             var acyncOperation=SceneManager.LoadSceneAsync("Menu");
             acyncOperation.allowSceneActivation = false;
-            while (acyncOperation.progress<.9f) {
-                _sceneLoadValue.Value = acyncOperation.progress;
+            while (!acyncOperation.isDone) {
+                _sceneLoadValue.Value = acyncOperation.progress/.9f;
                 yield return null;// yield -
                 //если напишем new waitforfixedupdate - 
             }
-            _sceneLoadValue.Value = 1f;
-            yield return new WaitForSeconds(2f);
-            acyncOperation.allowSceneActivation = true;
         }
     }
 }
