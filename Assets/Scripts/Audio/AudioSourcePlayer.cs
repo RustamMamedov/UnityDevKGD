40a67110aa520d4778c4e4b01d159323c920c7c3
Valeriy <mr.robariii@gmail.com>
2020-11-23 14:19:11 +0200

    AudioSourcePlayer script update

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -1,4 +1,5 @@
 ﻿using Sirenix.OdinInspector;
+using System.Collections;
 using UnityEngine;
 
 namespace Audio {
@@ -16,6 +17,35 @@ namespace Audio {
         [Button]
         public void Stop() {
             _audioSource.Stop();
+        }
+
+        public void PlayMusic(float requiredTime) {
+            StartCoroutine(PlayGradually(requiredTime));
+        }
+
+        public void StopMusic(float requiredTime) {
+            StartCoroutine(StopGradually(requiredTime));
+        }
+
+        IEnumerator PlayGradually(float requiredTime) {
+            Play();
+            yield return StartCoroutine(CoroutineMusicVolume(0f, 1f, requiredTime));
+        }
+
+        IEnumerator StopGradually(float requiredTime) {
+            yield return StartCoroutine(CoroutineMusicVolume(1f, 0f, requiredTime));
+            Stop();
+        }
+
+        private IEnumerator CoroutineMusicVolume(float from, float to, float requiredTime) {
+            var time = 0f;
+            _audioSource.volume = from;
+
+            while (time < requiredTime) {
+                time += Time.deltaTime;
+                _audioSource.volume = Mathf.Lerp(_audioSource.volume, to, time / requiredTime);
+                yield return null;
+            }
         }
     }
 }
