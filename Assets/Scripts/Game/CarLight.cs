5a97f227d558799a615317ec0ba4bb2ed214ea07
vladislav <vladislavnikwork@gmail.com>
2020-11-23 15:03:44 +0200

    Change the Range parameter of the Light component depending on PlayerCar

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -10,6 +10,14 @@ namespace Game {
         [SerializeField]
         private CarSettings _carSettings;
 
+        private Light _lightCar;
+        private int _rangeLightCarRatio = 8;
+
+        private void Awake() {
+            _lightCar = GetComponent<Light>();
+            _lightCar.range = _carSettings.lightDistance * _rangeLightCarRatio;
+        }
+
         private void OnDrawGizmosSelected() {
             Gizmos.color = _colorLight;
             Gizmos.matrix = transform.localToWorldMatrix;
