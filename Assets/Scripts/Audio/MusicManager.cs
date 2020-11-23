fc5b8d2915a13eaac97ceec5129d5eb4c949d108
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-23 16:48:45 +0200

    Added music methods for crash,dodge and gameplay

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -9,8 +9,32 @@ namespace Audio {
         [SerializeField]
         private AudioSourcePlayer _menuMusicPlayer;
 
+        [SerializeField]
+        private AudioSourcePlayer _gameplayMusic;
+
+        [SerializeField]
+        private AudioSourcePlayer _dodgeSound;
+
+        [SerializeField]
+        private AudioSourcePlayer _crachSound;
+
         public void PlayMenuMusic() {
+            _gameplayMusic.Stop();
             _menuMusicPlayer.Play();
+        }
+
+        public void PlayGameplayMusic() {
+           _menuMusicPlayer.Stop();
+           _gameplayMusic.Play();
+            
+        }
+
+        public void PlayDodgeSound() {
+            _dodgeSound.Play();
+        }
+
+        public void PlayCrashSound() {
+            _crachSound.Play();
         }
     }
 }
