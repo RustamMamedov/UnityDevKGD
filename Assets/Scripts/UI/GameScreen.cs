94cfaff608ce951c55001ab2160ab152e876c9e0
Alexandr <navacar@mail.ru>
2020-11-23 17:14:19 +0200

    One render per frame

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
+﻿using Events;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace UI {
 
     public class GameScreen : MonoBehaviour {
+        
+        [SerializeField]
+        private List<CarDodgeVeiw> _carsDodgedViews = new List<CarDodgeVeiw>();
+
+        private void OnEnable() {
+            StartCoroutine(CarRendererCoroutine());
+        }
 
         private void OnDisable() {
             RenderManager.Instance.ReleaseTexture();
         }
 
+        private IEnumerator CarRendererCoroutine() {
+            foreach (var carsDodgedView in _carsDodgedViews) {
+                carsDodgedView.Init();
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
