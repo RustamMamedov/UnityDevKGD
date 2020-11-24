993f6d05168cf0aa1c5fb37b48a41dd758f83a58
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-24 16:10:29 +0200

    Add AudioEventHandler script

---
+++ Assets/Scripts/Audio/AudioEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Audio {
+    
+    public class AudioEventHandler : MonoBehaviour {
+    
+        // Fields.
+
+        [SerializeField]
+        private EventListener _eventListener;
+
+        [SerializeField]
+        private AudioSourcePlayer _audioSourcePlayer;
+
+
+        // Life cycle.
+
+        private void OnEnable() {
+            _eventListener.OnEventHappened += PlaySound;
+        }
+
+        private void OnDisable() {
+            _eventListener.OnEventHappened -= PlaySound;
+        }
+
+
+        // Event handling.
+
+        private void PlaySound() {
+            _audioSourcePlayer.Play();
+        }
+
+
+    }
+
+}
