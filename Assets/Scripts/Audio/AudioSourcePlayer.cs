90cd2b5ce00de3916e97beb25a9cdeafb8f4c116
Nikita Shpulin <shpulinn@mail.ru>
2020-11-24 15:00:37 +0300

    AudioSourcePlayer edited
    Menu & Gameplay music play coroutines added

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -20,5 +20,22 @@ namespace Audio {
             _audioSource.Stop();
         }
 
+        public void PlayMusic(float time) {
+            Play();
+            StartCoroutine(MusicVolumeCoroutine(0.1f, 0.6f, time));
+        }
+        public IEnumerator StopMusic(float time) {
+            yield return StartCoroutine(MusicVolumeCoroutine(_audioSource.volume, 0f, time));
+            Stop();
+        }
+
+        private IEnumerator MusicVolumeCoroutine(float startVol, float finalVol, float time) {
+            var timer = 0f;
+            while (timer < time) {
+                timer += Time.deltaTime;
+                _audioSource.volume = Mathf.Lerp(startVol, finalVol, timer / time);
+                yield return null;
+            }
+        }
     }
 }
