6e3293dfc600c22bd43825f40d5c7ae97a2d5c22
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-24 16:16:28 +0200

    Expand AudioSourcePlayer script
    Expand AudioSourcePlayer script so it is able to change sound volume gradually with time.

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -10,17 +10,74 @@ namespace Audio {
         [SerializeField]
         private AudioSource _audioSource;
 
+        // Volume that set on play.
+        private float _defaultVolume;
+
+        // When not-null, volume is gradually changing to the value.
+        private float? _targetVolume;
+
+        // Volume change to target, per second.
+        private float _volumeChangeSpeed;
+
+
+        // Life cycle.
+
+        private void Awake() {
+            _defaultVolume = _audioSource.volume;
+            _audioSource.volume = 0;
+        }
+
+        private void Update() {
+            UpdateVolume();
+        }
+
 
         // Methods.
 
         [Button]
-        public void Play() {
-            _audioSource.Play();
-        }
+        public void Play() => SetVolume(_defaultVolume);
 
         [Button]
-        public void Stop() {
-            _audioSource.Stop();
+        public void Stop() => SetVolume(0);
+
+        public void Play(float changeDuration) => SetVolume(_defaultVolume, changeDuration);
+
+        public void Stop(float changeDuration) => SetVolume(0, changeDuration);
+
+
+        // Volume changing methods.
+
+        private void SetVolume(float newVolume) {
+            _targetVolume = null;
+            UpdateAudioSource(newVolume);
+        }
+
+        private void SetVolume(float newVolume, float changeDuration) {
+            _volumeChangeSpeed = _defaultVolume / changeDuration;
+            _targetVolume = newVolume;
+        }
+
+        private void UpdateVolume() {
+            if (_targetVolume.HasValue) {
+                var newVolume = Mathf.MoveTowards(_audioSource.volume, _targetVolume.Value, _volumeChangeSpeed * Time.deltaTime);
+                if (newVolume == _targetVolume.Value) {
+                    _targetVolume = null;
+                }
+                UpdateAudioSource(newVolume);
+            }
+        }
+
+        private void UpdateAudioSource(float newVolume) {
+            _audioSource.volume = newVolume;
+            if (_audioSource.volume == 0) {
+                if (_audioSource.isPlaying) {
+                    _audioSource.Stop();
+                }
+            } else {
+                if (!_audioSource.isPlaying) {
+                    _audioSource.Play();
+                }
+            }
         }
 
 
