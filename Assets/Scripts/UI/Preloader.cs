8167339b6532648e11548250c1a2f908d6ee9497
Dan <kdanil01@mail.ru>
2020-11-24 23:37:30 +0200

    Fixed build error in Preloader

--- Assets/Scripts/UI/Preloader.cs
+++ Assets/Scripts/UI/Preloader.cs
@@ -16,17 +16,12 @@ namespace UI {
 
         private IEnumerator LoadMenuScene() {
             var asyncOperation = SceneManager.LoadSceneAsync("Menu");
-            asyncOperation.allowSceneActivation = false;
             
-            while (asyncOperation.progress < .9f) {
-                Debug.Log(asyncOperation.progress);
+            while (!asyncOperation.isDone) {
+
+                _sceneLoadingValue.value = asyncOperation.progress / .9f;
                 yield return null;
             }
-
-            _sceneLoadingValue.value = 1f;
-
-            yield return new WaitForSeconds(2f);
-            asyncOperation.allowSceneActivation = true;
         }
     }
 }
