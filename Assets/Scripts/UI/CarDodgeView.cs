68df64f794390c27ee21fa9b2b18f5d1fcbf7e22
Maxim <korunets1901@gmail.com>
2020-11-25 13:12:07 +0200

    Dodge type car

--- Assets/Scripts/UI/CarDodgeView.cs
+++ Assets/Scripts/UI/CarDodgeView.cs
@@ -11,9 +11,28 @@ namespace UI {
 
         [SerializeField]
         private CarSettings _carSettings;
+        
+        [SerializeField]
+        private Text _text;
 
+        private int _family;
+        private int _suv;     
+        private int _truck;
         private void OnEnable() {
             Init();
+        }
+
+        private void Update() {
+            if (_carSettings.FamilyCar == true) {
+                _family++;
+                _text.text = _family.ToString();
+            }else if (_carSettings.SUV == true) {
+                _suv++;
+                _text.text = _suv.ToString();
+            }else if(_carSettings.Truck == true) {
+                _truck++;
+                _text.text = _truck.ToString();
+            }
         }
 
         public void Init() {
