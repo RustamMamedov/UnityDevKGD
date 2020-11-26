2c56edd38c9e3985d77caaa5f3af58511509de33
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-26 23:38:43 +0200

    Save: functional for saving settings

--- Assets/Scripts/Game/Save.cs
+++ Assets/Scripts/Game/Save.cs
@@ -30,6 +30,12 @@ namespace Game {
         private EventListener _carCollisionEventListener;
 
         [SerializeField]
+        private EventListener _gameSavedEvent;
+
+        [SerializeField]
+        private SettingsScreen _settingsScreen;
+
+        [SerializeField]
         private ScriptableIntValue _currentScore;
 
         [InfoBox("@\"Path: \" + Path.Combine(UnityEngine.Application.persistentDataPath, \"data.txt\")", "IsSaveTypeFile")]
@@ -37,21 +43,44 @@ namespace Game {
         [SerializeField]
         private SaveType _saveType;
 
+        public class SavedSettings {
+            public float volumeValue;
+            public Time time;
+            public Difficulty difficulty;
+
+            public enum Time {
+                Day,
+                Night,
+            }
+
+            public enum Difficulty {
+                Easy,
+                Hard,
+            }
+        }
+
         private enum SaveType {
             PlayerPrefs,
             File,
         }
 
+        private static SavedSettings _savedSettings;
+        public static SavedSettings Settings => _savedSettings;
+
         private static List<SaveData> _savedData;
         public static List<SaveData> SavedData => _savedData;
 
         private const string RECORDS_KEY = "records";
+        private const string SETTINGS_KEY = "settings";
 
         private string _filePath;
 
         private void Awake() {
             _filePath = Path.Combine(Application.persistentDataPath, "data.txt");
             _savedData = new List<SaveData>();
+            _savedSettings = new SavedSettings();
+
+            LoadSettings();
 
             if (_saveType == SaveType.PlayerPrefs) {
                 LoadFromPlayerPrefs();
@@ -63,10 +92,12 @@ namespace Game {
 
         private void OnEnable() {
             _carCollisionEventListener.OnEventHappened += OnCarCollision;
+            _gameSavedEvent.OnEventHappened += OnGameSaved;
         }
 
         private void OnDisable() {
             _carCollisionEventListener.OnEventHappened -= OnCarCollision;
+            _gameSavedEvent.OnEventHappened -= OnGameSaved;
         }
 
         private void OnCarCollision() {
@@ -85,6 +116,28 @@ namespace Game {
             }
 
             UIManager.instance.ShowLeaderBoardScreen();
+        }
+
+        private void OnGameSaved() {
+            var settings = new SavedSettings {
+                volumeValue = _settingsScreen.Volume,
+                time = _settingsScreen.Time > 0.5 ? SavedSettings.Time.Night : SavedSettings.Time.Day,
+                difficulty = _settingsScreen.Difficulty > 0.5 ? SavedSettings.Difficulty.Hard : SavedSettings.Difficulty.Easy,
+            };
+
+            var json = JsonUtility.ToJson(settings);
+            PlayerPrefs.SetString(SETTINGS_KEY, json);
+
+            _savedSettings = settings;
+        }
+
+        private void LoadSettings() {
+            if (!PlayerPrefs.HasKey(SETTINGS_KEY)) {
+                return;
+            }
+
+            var settings = JsonUtility.FromJson<SavedSettings>(PlayerPrefs.GetString(SETTINGS_KEY));
+            _savedSettings = settings;
         }
 
         private void AddNewRecord(SaveData newRecord) {
