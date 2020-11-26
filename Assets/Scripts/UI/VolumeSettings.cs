25ade47f362021014ac3318d8bffa34683836d39
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-26 21:48:37 +0200

    VolumeSettings: text representation of volume

---
+++ Assets/Scripts/UI/VolumeSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class VolumeSettings : MonoBehaviour {
+
+        [SerializeField]
+        private Text _volumeValueText;
+
+        [SerializeField]
+        private Slider _volumeSlider;
+
+        private void Start() {
+            _volumeSlider.onValueChanged.AddListener(delegate { OnVolumeChanged(); });
+        }
+
+        private void OnVolumeChanged() {
+            _volumeValueText.text = Mathf.Round((_volumeSlider.value * 100)).ToString() + "%";
+        }
+    }
+}
