f6880fdaaf9ab3d03043f31f4290dd100c4c1314
SaraJess <sarycheva.00@mail.ru>
2020-11-28 01:53:47 +0200

    Changing CarLight

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -17,5 +17,8 @@ namespace Game {
             Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _car.CarSettings.lightDistance, 1f, 1f);
             _light.range = _car.CarSettings.lightDistance * 10f;
         }
+        private void OnEnable() {
+            _light.range = _car.CarSettings.lightDistance * 10f;
+        }
     }
 }
