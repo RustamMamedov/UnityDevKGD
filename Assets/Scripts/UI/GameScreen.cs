c866e004fb38b7ab7d6d28722047590a472d9fe8
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-27 15:36:45 +0200

    cs RenderManager and GameScreen render texture

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace UI {
+    public class GameScreen : MonoBehaviour {
+        private void OnDisable() {
+            RenderManager.Instance.ReleaseTexture();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/RenderManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI {
+
+
+    public class RenderManager : MonoBehaviour {
+
+        public static RenderManager Instance;
+
+        [SerializeField]
+        private Camera _renderCamera;
+
+        [SerializeField]
+        private Transform _rootTransform;
+
+        private RenderTexture _texture;
+
+        private void Awake() {
+            Instance = this;
+        }
+
+        public RenderTexture Render(GameObject prefab) {
+            var carInstanse = Instantiate(prefab, _rootTransform);
+            _texture = RenderTexture.GetTemporary(64, 64, 16);
+            _texture.antiAliasing = 8;
+            _texture.Create();
+            _renderCamera.targetTexture = _texture;
+            _renderCamera.Render();
+            _renderCamera.targetTexture = null;
+            Destroy(carInstanse);
+            return _texture;
+        }
+
+        public void ReleaseTexture() {
+            RenderTexture.ReleaseTemporary(_texture);
+        }
+    }
+}
