1362c0f5579a167fbe44b07e09d324cf028a27a5
SaraJess <sarycheva.00@mail.ru>
2020-11-28 00:48:21 +0200

    Correct script CarLight

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -15,7 +15,7 @@ namespace Game {
         private void OnDrawGizmos() {
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _car.CarSettings.lightDistance, 1f, 1f);
-            _light.range = _car.CarSettings.lightDistance * 10;
+            _light.range = _car.CarSettings.lightDistance * 10f;
         }
     }
 }
