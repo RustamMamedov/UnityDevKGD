70555049aac8c51a07edf6440a602bd59274d547
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-27 06:21:39 +0200

    Added OnTriggerExit logic for EnemyCar script

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -8,12 +8,28 @@ namespace Game {
         [SerializeField]
         private EventDispatcher _carCollisionEventDispatcher;
 
+        [SerializeField]
+        private EventDispatcher _carDodgeEventDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _dodgeScore;
+
+        [SerializeField]
+        private ScriptableStringValue _dodgedCarName;
+
         private void OnTriggerEnter(Collider other) {
             
             if (other.CompareTag("Player")) {
 
                 _carCollisionEventDispatcher.Dispatch();
-                Debug.Log("CarCollision");
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (other.CompareTag("PlayerDodge")) {
+                _dodgeScore.value = _carSettings.dodgeScore;
+                _dodgedCarName.name = _carSettings.name;
+                _carDodgeEventDispatcher.Dispatch();
             }
         }
     }
