0505658fe4a3254163efaeb6f62fad79bd795af6
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-27 06:53:11 +0200

    Added count logic to CarDodgeView (fix)

--- Assets/Scripts/UI/CarDodgedView.cs
+++ Assets/Scripts/UI/CarDodgedView.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using Game;
+using Events;
+using System;
 
 namespace UI {
 
@@ -10,7 +12,35 @@ namespace UI {
         private RawImage _carImage;
 
         [SerializeField]
+        private Text _countLabel;
+
+        [SerializeField]
         private CarSettings _carSettings;
+
+        [SerializeField]
+        private EventListener _carDodgedEventListener;
+
+        [SerializeField]
+        private ScriptableStringValue _dodgedCarName;
+
+        private int _countOfCars;
+
+        private void OnEnable() {
+            _carDodgedEventListener.OnEventHappened += OnCarDodged;
+            _countOfCars = 0;
+            _countLabel.text = _countOfCars.ToString();
+        }
+
+        private void OnDisable() {
+            _carDodgedEventListener.OnEventHappened -= OnCarDodged;
+        }
+
+        private void OnCarDodged() {
+            if (_dodgedCarName.name == _carSettings.name) {
+                _countOfCars++;
+                _countLabel.text = _countOfCars.ToString();
+            }
+        }
 
         public void Init(GameObject renderCarPrefs, Vector3 renderCameraPos) {
             _carImage.texture = RenderManager.Instance.Render(_carSettings.renderCarPrefab,_carSettings.renderCameraPosition);
