81b2a5b664d30771f7742cbb347a40b984c8c990
BepeckoB <BepeckoB@gmail.com>
2020-11-27 19:24:04 +0200

    edited CarLight

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -7,20 +7,13 @@ namespace Game {
     public class CarLight : MonoBehaviour {
 
         [SerializeField]
-        private Color _gizmosColor;
-
-        [SerializeField]
         private PlayerCar _playerCar;
 
-        private void OnDrawGizmos() {
+        [SerializeField]
+        private Light _carLight;
 
-        }
-
-        private void OnDrawGizmosSelected() {
-            Gizmos.color = _gizmosColor;
-            Vector3 position = new Vector3(0f, 0f, 0f);
-            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
-            Gizmos.DrawFrustum(position, 25f, _playerCar.CarSettings.carLightDistance, 0f,1.5f);
+        private void Awake() {
+            _carLight.range = 15 * _playerCar.CarSettings.carLightDistance;
         }
     }
 }
