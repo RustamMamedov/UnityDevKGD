1e2452be8f4f54759723a30984d3241c274aa73b
Nikita <nikitadedov66@gmail.com>
2020-11-28 22:42:39 +0200

    score restarter object created and script created

---
+++ Assets/Scripts/Game/ScoreRestarter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+public class ScoreRestarter : MonoBehaviour
+{
+    [SerializeField]
+    private ScriptableIntValue _currentScore;
+    [SerializeField]
+    private CarSettings _suvDodgeScore;
+    [SerializeField]
+    private CarSettings _familyCarDodgeScore;
+    [SerializeField]
+    private CarSettings _truckDodgeScore;
+
+    private void OnEnable() {
+        _currentScore.value = 0;
+        _suvDodgeScore.currentDodgeScore = 0;
+        _familyCarDodgeScore.currentDodgeScore = 0;
+        _truckDodgeScore.currentDodgeScore = 0;
+    }
+}
