7e477279a0eea442aaf0f4b772659f837491e3f3
Andrey <DrEMartstud@gmail.com>
2020-11-28 22:58:29 +0200

    Reworked Menu's scene UI and UX
    Reworked every prefab in Menu scene
    + added menu's background, which will change according to day cycle settings
    + recolored and reshaped UI elements in scenes

--- Assets/Scripts/Game/SettingsScreen.cs
+++ Assets/Scripts/Game/SettingsScreen.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+using Events;
+using Sirenix.OdinInspector;
 using UI;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -10,20 +11,54 @@ namespace Game {
 
 		[SerializeField] 
 		private Button _cancelButton;
+		
+		[SerializeField] 
+		private Button _applyButton;
 
 		[SerializeField] 
 		private AudioMixer _audioMixer;
+
+		[SerializeField] 
+		private Toggle _isHardToggle;
 		
+		[SerializeField] 
+		private Toggle _isNightToggle;
+
+		[SerializeField] 
+		private EventDispatcher _settingsGotSavedEventDispatcher;
+		
+		private const string playerPrefsName = "settings";
+
+		private float _initialGameVolume;
+		private float _initialMusicVolume;
+
 		private void Awake() {
+			_applyButton.onClick.AddListener(OnApplyButtonClick);
 			_cancelButton.onClick.AddListener(OnCancelButtonClick);
 		}
 
+		private void OnEnable() {
+			_audioMixer.GetFloat("MusicVolume", out _initialGameVolume);
+			_audioMixer.GetFloat("GameVolume", out _initialMusicVolume);
+		}
+		
 		private void OnCancelButtonClick() {
-			UIManager.Instance.ShowMenuScreen();
+			SetMusicVolume(_initialMusicVolume);
+			SetGameVolume(_initialGameVolume);
+			ShowMenuScreen();
+		}
+		
+		private void OnApplyButtonClick() {
+			_settingsGotSavedEventDispatcher.Dispatch();
+			ShowMenuScreen();
 		}
 
 		public void SetMusicVolume(float volume) {
 			_audioMixer.SetFloat("MusicVolume", volume);
+		}
+		
+		public void SetGameVolume(float volume) {
+			_audioMixer.SetFloat("GameVolume", volume);
 		}
 
 		public void SetDifficultyLevel(bool isHard) {
@@ -31,6 +66,10 @@ namespace Game {
 		}
 		public void SetDayCycle(bool isNight) {
 			
+		}
+
+		private void ShowMenuScreen() {
+			UIManager.Instance.ShowMenuScreen();
 		}
 	}
 }
---
+++ Assets/Scripts/UI/MenusBackground.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+namespace UI {
+
+    public class MenusBackground : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _settingsGotSavedEventListener;
+
+        [SerializeField] 
+        private GameObject _dayLighting;
+        
+        [SerializeField] 
+        private GameObject _nightLighting;
+
+        [SerializeField] 
+        private bool _isNight;
+
+        private void Awake() {
+            SetMenusBackgroundLighting();
+        }
+        
+        private void OnEnable() {
+            _settingsGotSavedEventListener.OnEventHappened += SetMenusBackgroundLighting;
+        }
+        
+        private void OnDisable() {
+            _settingsGotSavedEventListener.OnEventHappened -= SetMenusBackgroundLighting;
+        }
+
+        private void SetMenusBackgroundLighting() {
+            _dayLighting.SetActive(!_isNight);
+            _nightLighting.SetActive(_isNight);
+        }
+    }
+}
