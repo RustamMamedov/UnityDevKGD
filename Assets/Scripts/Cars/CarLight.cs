da8273bcc4b5cb9b62b0e8e06f3718c28529e316
Anastasia <nastya_kalinina2000@mail.ru>
2020-11-28 03:04:37 +0200

    regulation carlight cs

--- Assets/Scripts/Cars/CarLight.cs
+++ Assets/Scripts/Cars/CarLight.cs
@@ -6,9 +6,17 @@ namespace Game {
     public class CarLight : MonoBehaviour {
         [SerializeField]
         private CarsSettings _settings;
+
+        [SerializeField]
+        private Light _light;
         private void OnDrawGizmos() {
             Gizmos.matrix = transform.localToWorldMatrix;
+            _light.range = _settings.lenghLightCar * 10f;
             Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _settings.lenghLightCar, 1f, 1f);
+        }
+
+        private void OnEnable() {
+            _light.range = _settings.lenghLightCar * 10f;
         }
     }
 }
