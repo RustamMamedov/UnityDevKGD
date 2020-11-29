d8c3a60f83cd71b830d09f23f0fc63b58f841df8
Denis <denis.zhuravkov@gmail.com>
2020-11-29 21:47:44 +0200

    Settings asset created

---
+++ Assets/Scripts/Game/ScriptableValues/ScriptableBoolValue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game {
+    
+    [CreateAssetMenu(fileName = "ScriptableBoolValue")]
+    public class ScriptableBoolValue : ScriptableObject {
+
+        public bool value;
+    }
+}
