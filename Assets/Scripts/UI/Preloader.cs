2a0b90bbed622469dbf957d377fcc91954d1026c
Denis <denis.zhuravkov@gmail.com>
2020-11-29 17:32:52 +0200

    Preloader fix

--- Assets/Scripts/UI/Preloader.cs
+++ Assets/Scripts/UI/Preloader.cs
@@ -16,16 +16,10 @@ namespace UI {
 
         private IEnumerator LoadMenuScene() {
             var asyncOperation = SceneManager.LoadSceneAsync("Menu");
-            asyncOperation.allowSceneActivation = false;
-            while (asyncOperation.progress < 0.9f) {
-                _sceneLoadingValue.value = asyncOperation.progress;
-                Debug.Log($"{_sceneLoadingValue}");
+            while (!asyncOperation.isDone) {
+                _sceneLoadingValue.value = asyncOperation.progress / 0.9f;
                 yield return null;
             }
-            _sceneLoadingValue.value = 1f;
-            
-            yield return new WaitForSeconds(3f);
-            asyncOperation.allowSceneActivation = true;
         }
     }
 }
