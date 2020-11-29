8b48a48526a2be883a936f56de2395064388a452
Nikita <nikitadedov66@gmail.com>
2020-11-29 22:11:21 +0200

    time state initialization created

---
+++ Assets/Scripts/UI/TimeStateInGameplaySceneInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+public class TimeStateInGameplaySceneInit : MonoBehaviour {
+
+    [SerializeField]
+    private ScriptableIntValue _timeState;
+
+    [SerializeField]
+    private Light _timeLightning;
+
+    void Start() {
+        switch (_timeState.value) {
+            case 0:
+                _timeLightning.intensity = 1;
+                break;
+
+            case 1:
+                _timeLightning.intensity = 0;
+                break;
+        }
+    }  
+}
