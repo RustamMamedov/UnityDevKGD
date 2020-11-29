2a6410dba11b300464abb5e3fcc98e7836491ef4
Denis <denis.zhuravkov@gmail.com>
2020-11-29 22:19:37 +0200

    DayTime script created
    It manages day or night time on Gameplay scene according to settings

---
+++ Assets/Scripts/Game/DayTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    
+    public class DayTime : MonoBehaviour {
+
+        [SerializeField] 
+        private ScriptableBoolValue _nightSetting;
+
+        [SerializeField] 
+        private GameObject _worldLight;
+
+        [SerializeField] 
+        private GameObject _carLights;
+
+        private void Awake() {
+            if (_nightSetting.value) {
+                SetNight();
+            } else {
+                SetDay();
+            }
+        }
+
+        private void SetDay() {
+            _carLights.SetActive(false);
+            _worldLight.SetActive(true);
+        }
+
+        private void SetNight() {
+            _carLights.SetActive(true);
+            _worldLight.SetActive(false);
+        }
+    }
+}
+
+
