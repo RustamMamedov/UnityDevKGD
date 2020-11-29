8b079b8217e55044cc05de328bd92e675b70477c
Denis <denis.zhuravkov@gmail.com>
2020-11-29 23:18:08 +0200

    MusicManager events handling

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using Events;
+using Game;
+using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Audio {
     
@@ -10,6 +14,30 @@ namespace Audio {
         [SerializeField]
         private AudioSourcePlayer _gameMusicPlayer;
 
+        [SerializeField] 
+        private EventListener _volumeChangedListener;
+        
+        [SerializeField] 
+        private EventListener _settingsLoadedListener;
+
+        [SerializeField] 
+        private ScriptableFloatValue _volumeAsset;
+
+        [SerializeField] 
+        private AudioMixer _audioMixer;
+
+        private void Awake() {
+            _volumeChangedListener.OnEventHappened += OnVolumeChangedBehaviour;
+            _settingsLoadedListener.OnEventHappened += OnSettingsLoaded;
+
+            UpdateMixerVolume(0.0001f);
+        }
+
+        private void OnDestroy() {
+            _volumeChangedListener.OnEventHappened -= OnVolumeChangedBehaviour;
+            _settingsLoadedListener.OnEventHappened -= OnSettingsLoaded;
+        }
+
         public void PlayMenuMusic() {
             _gameMusicPlayer.FadeStop();
             _menuMusicPlayer.FadePlay();
@@ -18,6 +46,18 @@ namespace Audio {
         public void PlayGameMusic() {
             _menuMusicPlayer.FadeStop();
             _gameMusicPlayer.FadePlay();
+        }
+
+        private void UpdateMixerVolume(float multiplier) {
+            _audioMixer.SetFloat("MasterVol",  Mathf.Log10(multiplier) * 20f);
+        }
+
+        private void OnVolumeChangedBehaviour() {
+            UpdateMixerVolume(_volumeAsset.value);
+        }
+
+        private void OnSettingsLoaded() {
+            UpdateMixerVolume(_volumeAsset.value);
         }
 
     }
