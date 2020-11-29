f743f817095a30719fe1338f7cf3331cca056b2e
Evgenii <jekaweek1@gmail.com>
2020-11-30 00:17:01 +0200

    add Settings button in MenuScreen class

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -9,12 +9,20 @@ namespace UI {
         [SerializeField]
         private Button _playButton;
 
+        [SerializeField]
+        private Button _settingsButton;
+
         private void Awake() {
             _playButton.onClick.AddListener(OnPlayButtonClick);
+            _settingsButton.onClick.AddListener(OnSettingsButtonClick);
         }
 
         private void OnPlayButtonClick() {
             UIManager.Instance.LoadGameplay();
+        }
+
+        private void OnSettingsButtonClick() {
+            UIManager.Instance.ShowSettingsScreen();
         }
     }
 }
