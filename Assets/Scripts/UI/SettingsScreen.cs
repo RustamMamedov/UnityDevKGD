5a9bf8a8c43fc8a2ae978d7335f5877d0160217a
Nikita <Nkanunov@gmail.com>
2020-11-30 00:57:01 +0200

    Added save on PlayerPrefs

--- Assets/Scripts/UI/SettingsScreen.cs
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -30,23 +31,83 @@ namespace UI {
 
         private const string RECORDS_KEY = "settings";
 
+        [Serializable]
+        public class SaveData {
+            public float volume;
+
+            public int difficult;
+
+            public int dayTime;
+
+        }
+
+        [Serializable]
+        private class SavedDataWrapper {
+            public SaveData savedData;
+        }
+
+        private static SaveData _saveData;
+
         private void Awake() {
+            LoadFromPlayerPrefs();
             _saveButton.onClick.AddListener(Save);
         }
 
         private void SetSoundVolume() {
-            _sliderVolumeValue = _volumeSlider.value;
-            for (int i = 0; i < _audioSources.Count; i++) {
-                _audioSources[i].volume = _sliderVolumeValue;
-            }
+
         }
 
-        private void Save(){
+        private SavedDataWrapper GetWrapper() {
+            return (new SavedDataWrapper {
+                savedData = _saveData
+            });
+        }
 
-            SetSoundVolume();
+        private void SaveToPlayerPrefs() {
+            var wrapper = GetWrapper();
+            var json = JsonUtility.ToJson(wrapper);
+            PlayerPrefs.SetString(RECORDS_KEY, json);
+        }
+
+        private void LoadFromPlayerPrefs() {
+            if (!PlayerPrefs.HasKey(RECORDS_KEY)) {
+                return;
+            }
+
+            var wrapper = JsonUtility.FromJson<SavedDataWrapper>(PlayerPrefs.GetString(RECORDS_KEY));
+            _saveData = wrapper.savedData;
+        }
+
+        private void Save() {
+
+            int isDifficultToggleOn = IsON(_difficultToggle);
+            int isLightToggleOn = IsON(_ligthToggle);
+
+            var SettingsRecord = new SaveData {
+                volume = _volumeSlider.value,
+                difficult = isDifficultToggleOn,
+                dayTime = isLightToggleOn
+            };
+
+            _saveData = SettingsRecord;
+            SaveToPlayerPrefs();
             UIManager.Instance.ShowMenuScreen();
         }
 
+        private void Cancel() {
+            _volumeSlider.value = _saveData.volume;
+            _difficultToggle.isOn = IntToBool(_saveData.difficult);
+            _ligthToggle.isOn = IntToBool(_saveData.dayTime);
+        }
+        private int IsON(Toggle toggle) {
+
+            return toggle.isOn ? 1 : 0;
+        }
+
+        private bool IntToBool(int value) {
+
+            return value == 1 ? true : false;
+        }
 
     }
 }
