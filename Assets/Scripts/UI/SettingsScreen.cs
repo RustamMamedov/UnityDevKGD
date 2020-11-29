2ad8f335c209c13f2bcb89e0a6d0eda326d9db88
Nikita <Nkanunov@gmail.com>
2020-11-29 23:04:48 +0200

    Created SettingsScreen script

---
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class SettingsScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Slider _volumeSlider;
+        [SerializeField]
+        private List<AudioSource> _audioSources;
+
+        [SerializeField]
+        private Toggle _difficultToggle;
+
+        [SerializeField]
+        private Toggle _ligthToggle;
+
+        [SerializeField]
+        private Button _saveButton;
+
+        [SerializeField]
+        private Button _cancelButton;
+
+        [SerializeField]
+        private GameObject _settingsScreen;
+
+        private float _sliderVolumeValue;
+
+        private const string RECORDS_KEY = "settings";
+
+        private void Awake() {
+            _saveButton.onClick.AddListener(Save);
+        }
+
+        private void SetSoundVolume() {
+            _sliderVolumeValue = _volumeSlider.value;
+            for (int i = 0; i < _audioSources.Count; i++) {
+                _audioSources[i].volume = _sliderVolumeValue;
+            }
+        }
+
+        private void Save(){
+
+            SetSoundVolume();
+            UIManager.Instance.ShowMenuScreen();
+        }
+
+
+    }
+}
