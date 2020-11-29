30e0fea298d47924ee8c422a2bd82f7b2c0358c7
Nikita <nikitadedov66@gmail.com>
2020-11-29 22:12:42 +0200

    difficult initialization script created

---
+++ Assets/Scripts/UI/DifficultInit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+public class DifficultInit : MonoBehaviour
+{
+    [SerializeField]
+    private ScriptableIntValue _difficultValue;
+
+    private void Start() {
+        _difficultValue.value = PlayerPrefs.GetInt("DifficultValue");
+    }
+}
