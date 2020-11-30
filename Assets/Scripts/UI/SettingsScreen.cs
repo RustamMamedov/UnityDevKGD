9e0eee41383433641fb65cf1ffd496ab363fd945
Nikita Shpulin <shpulinn@mail.ru>
2020-11-30 18:28:12 +0300

    SettingsScreen realized
    difficulty, volume and light now saves to player prefs and cancel if need to

--- Assets/Scripts/UI/SettingsScreen.cs
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -1,4 +1,5 @@
 ﻿using Audio;
+using Game;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,24 +26,56 @@ namespace UI {
         [SerializeField]
         private Button _cancelButton;
 
-        public void SetLevel() {
+        private int _difficulty;
+        private float _volume;
+        private int _light;
+
+        public void SetVolume() {
             foreach (var source in _audioSources) {
                 source.volume = _volumeSlider.value;
+                _volume = _volumeSlider.value;
             }
         }
 
+        private void SetDifficulty() {
+            _difficulty = (int)_difficultySlider.value;
+        }
+
+        private void SetLight() {
+            _light = (int)_lightSlider.value;
+        }
+        
+        private void SaveToPlayerPrefs() {
+            PlayerPrefs.SetFloat("Volume", _volume);
+            PlayerPrefs.SetInt("Difficulty", _difficulty);
+            PlayerPrefs.SetInt("Light", _light);
+        }
+
+        private void LoadFromPlayerPrefs() {
+            _volume = PlayerPrefs.GetFloat("Volume");
+            _difficulty = PlayerPrefs.GetInt("Difficulty");
+            _light = PlayerPrefs.GetInt("Light");
+            _volumeSlider.value = _volume;
+            _difficultySlider.value = _difficulty;
+            _lightSlider.value = _light;
+        }
+
         private void Awake() {
-            _volumeSlider.value = 0.5f;
+            LoadFromPlayerPrefs();
             _applyButton.onClick.AddListener(OnApplyButtonClick);
             _cancelButton.onClick.AddListener(OnCancelButtonClick);
-            _volumeSlider.onValueChanged.AddListener(delegate { SetLevel(); } );
+            _volumeSlider.onValueChanged.AddListener(delegate { SetVolume(); });
+            _difficultySlider.onValueChanged.AddListener(delegate { SetDifficulty(); });
+            _lightSlider.onValueChanged.AddListener(delegate { SetLight(); });
         }
 
         private void OnApplyButtonClick() {
+            SaveToPlayerPrefs();
             UIManager.Instance.LoadMenu();
         }
 
         private void OnCancelButtonClick() {
+            LoadFromPlayerPrefs();
             UIManager.Instance.LoadMenu();
         }
 
