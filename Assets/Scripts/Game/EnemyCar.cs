eda6b4de3fbf977635c2ba69c852b87c10c984d4
Valeriy <mr.robariii@gmail.com>
2020-11-30 15:33:26 +0200

    EnemyCar script update

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -8,9 +8,23 @@ namespace Game {
         [SerializeField]
         private EventDispatcher _carCollisionEventDispatcher;
 
+        [SerializeField]
+        private EventDispatcher _dodgeDispatcher;
+
+        [SerializeField]
+        private ScriptableIntValue _dodgeScore;
+
+        [SerializeField]
+        private ScriptableStringValue _carName;
+
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carCollisionEventDispatcher.Dispatch();
+            }
+            if (other.CompareTag("DodgeObject")) {
+                _carName.value = _carSettings.name;
+                _dodgeScore.value = _carSettings.dodgeScore;
+                _dodgeDispatcher.Dispatch();
             }
         }
     }
