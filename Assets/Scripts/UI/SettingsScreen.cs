f53302772bd51645031b593ebd32a5b02bdf5b82
vladislav <vladislavnikwork@gmail.com>
2020-11-30 07:43:19 +0200

    Created and added settings script into prefab

---
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class SettingsScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _ConfirmB;
+
+        [SerializeField]
+        private Button _CancelB;
+
+        private void Awake() {
+            _ConfirmB.onClick.AddListener(OnConfirmButtonClick);
+            _CancelB.onClick.AddListener(OnCancelButtonClick);
+        }
+
+        private void OnConfirmButtonClick() {
+            UIManager.Instance.ShowMenuScreen();
+        }
+
+        private void OnCancelButtonClick() {
+            UIManager.Instance.ShowMenuScreen();
+        }
+    }
+}
+
