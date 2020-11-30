61b623a63614caac1fa14e11efb745b15700789e
Alex Osi <noemail>
2020-11-30 19:44:03 +0500

    Volume changing realized

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -1,5 +1,7 @@
 ﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using UI;
+using Events;
 
 namespace Audio {
 
@@ -7,6 +9,13 @@ namespace Audio {
 
         [SerializeField]
         private AudioSource _audioSource;
+
+        [SerializeField]
+        private EventListener _volumeChangeEvent;
+
+        private float _startVolume;
+
+        private float _changedVolume;
 
         [Button]
         public void Play() {
@@ -19,8 +28,29 @@ namespace Audio {
             _audioSource.Stop();
         }
 
+        private void Awake() {
+            _changedVolume = 1;
+            _volumeChangeEvent.OnEventHappened += OnGlobalVolumeChanged;
+            _startVolume = _audioSource.volume;
+            if (Settings.Instance != null) {
+                _audioSource.volume = _startVolume * Settings.Instance.GlobalVolume;
+            }
+        }
+
+        void OnGlobalVolumeChanged() {
+            if (_changedVolume == 0) {
+                _audioSource.volume = 0;
+
+            }
+            else {
+                _audioSource.volume = _startVolume * Settings.Instance.GlobalVolume;
+
+            }
+        }
+
         public void SetAudioSourceVolume(float volume) {
             _audioSource.volume = volume;
+            _changedVolume = volume;
         }
         public float GetAudioSourceVolume() {
             return _audioSource.volume;
