e360f76678ba9f7093b7b2060f47d4c978380292
gg <71404862+gqggqg@users.noreply.github.com>
2020-11-30 19:33:52 +0200

    SettingsScreen: ok/cancel button logic

---
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Audio;
+using Game;
+
+namespace UI {
+
+    public class SettingsScreen : MonoBehaviour {
+
+        [SerializeField]
+        private Button _okButton;
+
+        [SerializeField]
+        private Button _resumeButton;
+
+        private void Awake() {
+            _okButton.onClick.AddListener(OnOkButtonClick);
+            _resumeButton.onClick.AddListener(OnResumeButtonClick);
+        }
+
+        private void OnOkButtonClick() {
+            UIManager.Instance.ShowMenuScreen();
+        }
+
+        private void OnResumeButtonClick() {
+            UIManager.Instance.ShowMenuScreen();
+        }
+    }
+}
