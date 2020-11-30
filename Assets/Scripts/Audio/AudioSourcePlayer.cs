c952e4658de50b0944457f48de30f2283e912d16
vladislav <vladislavnikwork@gmail.com>
2020-11-30 11:18:30 +0200

    Added logic settings audio volume

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -1,5 +1,7 @@
 ﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using Events;
+using Game;
 
 namespace Audio {
 
@@ -8,10 +10,29 @@ namespace Audio {
         [SerializeField]
         private AudioSource _audioSource;
 
+        [SerializeField]
+        private EventListener _update;
+
+        [SerializeField]
+        private ScriptableFloatValue _volumeCurrent;
+
         [Button]
         public void Play() {
             _audioSource.Play();
             Debug.Log("play");
+        }
+
+
+        private void OnEnable() {
+            _update.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            MixerSound();
+        }
+
+        private void MixerSound() {
+            _audioSource.volume = _volumeCurrent.value;
         }
 
         [Button]
