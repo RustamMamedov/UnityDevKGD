87054140b9329870630c08ea52e974a49d790a1f
SaraJess <sarycheva.00@mail.ru>
2020-11-30 18:33:48 +0200

    Button changes and asset volume

--- Assets/Scripts/UI/ChangeParameters.cs
+++ Assets/Scripts/UI/ChangeParameters.cs
@@ -30,21 +30,27 @@ namespace UI {
         [SerializeField]
         private Button _cancelButton;
 
+        [SerializeField]
+        private UIManager _uimanager;
+
         public void SaveChange() {
             _volume.value = _sliderVolume.value;
             _difficulty.value = (int)_sliderDifficulty.value;
             _timeLight.value = (int)_sliderTimeLight.value;
+            _uimanager.ShowMenuScreen();
         }
 
         public void RecoveryChange() {
             _sliderVolume.value = _volume.value;
             _sliderDifficulty.value = _difficulty.value; 
             _sliderTimeLight.value = _timeLight.value;
+            _uimanager.ShowMenuScreen();
         }
 
         private void Awake() {
             _oKButton.onClick.AddListener(SaveChange);
             _cancelButton.onClick.AddListener(RecoveryChange);
+            _volume.value = .5f;
         }
     }
 }
