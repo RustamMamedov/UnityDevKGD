cc3e3a3929dd9550bde6d4b79e8d4bae1a277d24
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-11-30 15:57:58 +0200

    Created Toggle script

---
+++ Assets/Scripts/UI/ToggleScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class ToggleScript : MonoBehaviour {
+        
+        [SerializeField]
+        private Toggle _toggle;
+
+        [SerializeField]
+        private Image _leftImage;
+
+        [SerializeField]
+        private Image _rightImage;
+
+        private void Awake() {
+            _toggle.onValueChanged.AddListener(ChangeTogglePosition);
+            ChangeTogglePosition(false);
+        }
+
+        private void ChangeTogglePosition(bool isOn) {
+            var color = new Color(164, 161, 161,255);
+            if(isOn) {
+                _leftImage.color = color;
+                _rightImage.color = Color.blue;
+            }
+            else {
+                _leftImage.color = Color.blue;
+                _rightImage.color = color;
+            }
+        }
+    }
+
+}
