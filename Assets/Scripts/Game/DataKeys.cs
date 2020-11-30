e271ec2ca964afb35634ce471101a107957f2fea
Valeriy <mr.robariii@gmail.com>
2020-11-30 15:32:58 +0200

    DataKeys script created

---
+++ Assets/Scripts/Game/DataKeys.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public static class DataKeys {
+
+        public const string VOLUME_KEY = "volume";
+        public const string DIFFICULT_KEY = "difficult";
+        public const string LIGHT_KEY = "light";
+        public const string RECORDS_KEY = "record";
+    }
+}
