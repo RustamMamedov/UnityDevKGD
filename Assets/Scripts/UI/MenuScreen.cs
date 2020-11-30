5b8838dca2534e209ffc3dee1533e8d284150b94
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-30 21:40:29 +0200

    Implement settings button

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -10,11 +10,15 @@ namespace UI {
         [SerializeField]
         private Button _playButton;
 
+        [SerializeField]
+        private Button _settingsButton;
+
 
         // Life cycle.
 
-        private void Awake() {
+        private void Start() {
             _playButton.onClick.AddListener(OnPlayButtonClick);
+            _settingsButton.onClick.AddListener(OnSettingsButtonClick);
         }
 
 
@@ -22,6 +26,11 @@ namespace UI {
 
         private void OnPlayButtonClick() {
             UIManager.Instance.LoadGameplayScene();
+        }
+
+        private void OnSettingsButtonClick() {
+            UIManager.Instance.HideAllScreens();
+            UIManager.Instance.ShowSettingsScreen();
         }
 
 
