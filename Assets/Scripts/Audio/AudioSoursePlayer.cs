18c7c002f1c5ee3e5c66aee0f84767aa4af3d139
TechesDePainture <kirillzelen00@gmail.com>
2020-11-30 14:43:29 +0200

    AudioSoursePlayer controled by volume

--- Assets/Scripts/Audio/AudioSoursePlayer.cs
+++ Assets/Scripts/Audio/AudioSoursePlayer.cs
@@ -9,8 +9,12 @@ namespace Audio {
         [SerializeField]
         private AudioSource _audioSource;
 
+        [SerializeField]
+        private ScriptableFloatValue _volume;
+
         [Button]
         public void Play() {
+            _audioSource.volume = _volume.value;
             _audioSource.Play();
         }
 
