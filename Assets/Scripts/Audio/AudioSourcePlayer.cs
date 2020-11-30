882fbe9eb6600037be581c50d33589652e0292aa
Sevastian Kozhunov <sky.seva@gmail.com>
2020-12-01 01:59:09 +0200

    Fix sounds not playing in menu

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -18,7 +18,7 @@ namespace Audio {
         // Currently set volume, before factors.
         private float _currentVolume = 0;
 
-        // Volume that is set on play.
+        // Volume factor from audio source component.
         private float _playVolume;
 
         // When not-null, volume is gradually changing to the value.
@@ -49,7 +49,7 @@ namespace Audio {
             if (_audioSource.volume > 0 && !_audioSource.isPlaying) {
                 SetVolume(0);
             }
-            _audioSource.volume = _currentVolume * _volumeFactorValue.value;
+            _audioSource.volume = _currentVolume * _playVolume * _volumeFactorValue.value;
             if (_audioSource.volume == 0) {
                 if (_audioSource.isPlaying) {
                     _audioSource.Stop();
@@ -66,12 +66,12 @@ namespace Audio {
         // Methods.
 
         [Button]
-        public void Play() => SetVolume(_playVolume);
+        public void Play() => SetVolume(1);
 
         [Button]
         public void Stop() => SetVolume(0);
 
-        public void Play(float changeDuration) => SetVolume(_playVolume, changeDuration);
+        public void Play(float changeDuration) => SetVolume(1, changeDuration);
 
         public void Stop(float changeDuration) => SetVolume(0, changeDuration);
 
