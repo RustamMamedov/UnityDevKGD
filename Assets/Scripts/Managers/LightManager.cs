df8d88ecf2720f00ba912c62fcc422e9546108c8
Valeriy <mr.robariii@gmail.com>
2020-11-30 15:34:50 +0200

    LightManager script created

---
+++ Assets/Scripts/Managers/LightManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    public class LightManager : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject DayLight;
+
+        [SerializeField]
+        private List<GameObject> NightLight = new List<GameObject>();
+
+
+        private void Awake() {
+            if (PlayerPrefs.GetInt(DataKeys.LIGHT_KEY) == 0) {
+                DayMode();
+            } else {
+                NightMode();
+            }
+        }
+        private void DayMode() {
+            DayLight.SetActive(true);
+
+            for (int i = 0; i < NightLight.Count; i++) {
+                NightLight[i].SetActive(false);
+            }
+        }
+
+        private void NightMode() {
+            DayLight.SetActive(false);
+
+            for (int i = 0; i < NightLight.Count; i++) {
+                NightLight[i].SetActive(true);
+            }
+        }
+    }
+}
