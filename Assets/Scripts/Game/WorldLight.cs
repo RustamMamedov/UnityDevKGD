916e0bf4e3aa3bcc90fd468edf69b661b7514d98
Sevastian Kozhunov <sky.seva@gmail.com>
2020-12-01 01:24:46 +0200

    Implement World Light prefab

---
+++ Assets/Scripts/Game/WorldLight.cs
@@ -0,0 +1,49 @@
+﻿using Events;
+using UnityEngine;
+using Values;
+
+namespace Game {
+    
+    public class WorldLight : MonoBehaviour {
+
+        // Fields.
+
+        [SerializeField]
+        private Light _light;
+
+        [SerializeField]
+        private EventListener _lightChangedListener;
+
+        [SerializeField]
+        private ScriptableIntValue _daytimeValue;
+
+        [SerializeField]
+        private float _daylightIntensity;
+
+        [SerializeField]
+        private float _nightlightIntensity;
+
+
+
+        // Life cycle.
+
+        private void Start() {
+            UpdateIntensity();
+            _lightChangedListener.OnEventHappened += UpdateIntensity;
+        }
+
+
+        // Event handlers.
+
+        private void UpdateIntensity() {
+            if (_daytimeValue.value == 0) {
+                _light.intensity = _daylightIntensity;
+            } else {
+                _light.intensity = _nightlightIntensity;
+            }
+        }
+
+
+    }
+
+}
