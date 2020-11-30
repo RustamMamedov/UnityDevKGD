fabddc6140067c256512aa907550d497c553a688
Valeriy <mr.robariii@gmail.com>
2020-11-30 15:33:12 +0200

    Dodger script created

---
+++ Assets/Scripts/Game/Dodger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Game;
+using Events;
+
+namespace Game {
+
+    public class Dodger : MonoBehaviour {
+
+        [SerializeField]
+        private EventDispatcher _dodgeDispatcher;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.GetComponent<EnemyCar>() != null) {
+                _dodgeDispatcher.Dispatch();
+            }
+        }
+    }
+}
