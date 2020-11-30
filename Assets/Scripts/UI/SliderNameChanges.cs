7d9087b42821ecec53452fc72344874a68f53ff4
TechesDePainture <kirillzelen00@gmail.com>
2020-11-30 14:40:46 +0200

    SliderNameChanger.cs created

---
+++ Assets/Scripts/UI/SliderNameChanges.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderNameChanges : MonoBehaviour
+{
+
+    [SerializeField]
+    private Slider _slider;
+
+    [SerializeField]
+    private string _nameValue1;
+
+    [SerializeField]
+    private float _value1;
+
+    [SerializeField]
+    private string _nameValue2;
+
+    [SerializeField]
+    private float _value2;
+
+    [SerializeField]
+    private Text _textLabel;
+
+    private void OnEnable() {
+        _slider.onValueChanged.AddListener(delegate { NameChanger(); });
+        if (_slider.value == _value1)
+            _textLabel.text = _nameValue1;
+        if (_slider.value == _value2)
+            _textLabel.text = _nameValue2;
+    }
+
+
+    public void NameChanger() {
+        if (_slider.value == _value1)
+            _textLabel.text = _nameValue1;
+        if (_slider.value == _value2)
+            _textLabel.text = _nameValue2;
+    }
+
+}
