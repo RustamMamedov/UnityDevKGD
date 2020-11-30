60380fa7f12deb2c559c3db841d86e455160e9df
SaraJess <sarycheva.00@mail.ru>
2020-11-30 16:51:11 +0200

    Create Slider TimeLight and Difficulty

---
+++ Assets/Scripts/UI/ChangeName.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class ChangeName : MonoBehaviour {
+
+        [SerializeField]
+        private Slider _slider;
+
+        [SerializeField]
+        private Text _text;
+
+        [SerializeField]
+        private string _zeroWord;        
+        
+        [SerializeField]
+        private string _oneWord;
+
+        public void ChangeNameButton() {
+            if (_slider.value == 0f) {
+                _text.text = _zeroWord;
+            } else {
+                _text.text = _oneWord;
+            }
+        }
+    }
+}
