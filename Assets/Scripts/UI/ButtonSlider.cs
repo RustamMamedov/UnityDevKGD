a1cc4957c5009ccd73e3e91514bbe1008f65b99f
Sevastian Kozhunov <sky.seva@gmail.com>
2020-11-30 21:40:52 +0200

    Implement Button Slider prefab

---
+++ Assets/Scripts/UI/ButtonSlider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public class ButtonSlider : MonoBehaviour {
+
+        // Delegates.
+
+        public delegate void ChoiceMadeHandler(int newChoice);
+
+
+        // Events.
+
+        public event ChoiceMadeHandler ChoiceMade;
+
+
+        // Fields.
+
+        [SerializeField]
+        private Button[] _buttons;
+
+        private int _currentChoice = 0;
+
+
+        // Life cycle.
+
+        private void Start() {
+            for (int i = 0; i < _buttons.Length; ++i) {
+                var button = _buttons[i];
+                button.interactable = (i != _currentChoice);
+                int choice = i;
+                button.onClick.AddListener(() => OnButtonClick(choice));
+            }
+        }
+
+
+        // Event handling.
+
+        private void OnButtonClick(int index) {
+            SetChoice(index);
+            ChoiceMade?.Invoke(index);
+        }
+
+
+        // Public methods.
+
+        public void SetChoice(int newChoice) {
+            if (_currentChoice >= 0 && _currentChoice < _buttons.Length) {
+                _buttons[_currentChoice].interactable = true;
+            }
+            _currentChoice = newChoice;
+            if (_currentChoice >= 0 && _currentChoice < _buttons.Length) {
+                _buttons[_currentChoice].interactable = false;
+            }
+        }
+
+
+    }
+
+}
