725883ba89eea7974833995a91dc9cf17f75e9fb
Nikita <Nkanunov@gmail.com>
2020-11-30 16:05:29 +0200

    Fix ToggleScript script

--- Assets/Scripts/UI/ToggleScript.cs
+++ Assets/Scripts/UI/ToggleScript.cs
@@ -19,15 +19,14 @@ namespace UI {
 
         }
 
-
         private void ChangeTogglePosition(bool isOn) {
-            var color = new Color32(255, 227, 227, 255);
+
             if (!isOn) {
-                _leftImage.color = color;
+                _leftImage.color = Color.white;
                 _rightImage.color = Color.red;
             } else {
                 _leftImage.color = Color.red;
-                _rightImage.color = color;
+                _rightImage.color = Color.white;
             }
 
         }
