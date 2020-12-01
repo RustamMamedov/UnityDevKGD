22b047fd08c4d64d65c1c60cc6d242cfa8879d59
Potapenko_Ivan <van.van123@list.ru>
2020-12-01 21:54:36 +0200

    Added pool in enemy spawner

--- Assets/Scripts/Game/EnemySpawner.cs
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -36,16 +36,41 @@ namespace Game {
         [SerializeField]
         private ScriptableFloatValue _roadWidth;
 
+        [SerializeField]
+        private int _listOfEnemyCarsSize; 
+
         private float _currentTimer;
+        [SerializeField]
+
         private List<GameObject> _cars = new List<GameObject>();
-
-
+        [SerializeField]
+        private List<GameObject> _listOfEnemyCars;
 
         private void Awake() {
             if(Settings.Instance!=null) {
                 if(!Settings.Instance.IsEasy) {
                     _spawnCooldown =_hardSpawnCooldown;
                 }
+            }
+            
+        }
+
+        private void Start() {
+            GeneratePool();
+        }
+        
+
+        void GeneratePool() {
+            _listOfEnemyCars = new List<GameObject>();
+            for(int i = 0;  i<_listOfEnemyCarsSize / _carPrefabs.Count;i++) {
+                for(int j = 0;j < _carPrefabs.Count;j++) {
+                    _listOfEnemyCars.Add(Instantiate(_carPrefabs[j]));
+                    _listOfEnemyCars[_listOfEnemyCars.Count - 1].SetActive(false);
+                }
+            }
+            for(int i =0;i<_listOfEnemyCarsSize % _carPrefabs.Count;i++) {
+                _listOfEnemyCars.Add(Instantiate(_carPrefabs[i]));
+                _listOfEnemyCars[_listOfEnemyCars.Count - 1].SetActive(false);
             }
         }
 
@@ -96,17 +121,28 @@ namespace Game {
 
         private void SpawnCar() {
             var randomRoad = Random.Range(-1, 2);
-            var randomCar = Random.Range(0, _carPrefabs.Count);
+
+            if(_listOfEnemyCars.Count==0) {
+                var randomCar = Instantiate(_carPrefabs[Random.Range(0,_carPrefabs.Count)]);
+                randomCar.SetActive(false);
+                _listOfEnemyCars.Add(randomCar);
+            }
+
+            var randomCarNum = Random.Range(0, _listOfEnemyCars.Count);
 
             var position = new Vector3(1f * randomRoad * _roadWidth.value, 0f, _playerPositionZ.value + _distanceToPlayerToSpawn);
-            var car = Instantiate(_carPrefabs[randomCar], position, Quaternion.Euler(0f, 180f, 0f));
-            _cars.Add(car);
+            _listOfEnemyCars[randomCarNum].transform.position = position;
+            _listOfEnemyCars[randomCarNum].transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            _listOfEnemyCars[randomCarNum].SetActive(true);
+            _cars.Add(_listOfEnemyCars[randomCarNum]);
+            _listOfEnemyCars.RemoveAt(randomCarNum);
         }
 
         private void HandleCarsBehindPlayer() {
             for (int i = _cars.Count - 1; i > -1; i--) {
                 if (_playerPositionZ.value - _cars[i].transform.position.z > _distanceToPlayerToDestroy) {
-                    Destroy(_cars[i]);
+                    _cars[i].SetActive(false);
+                    _listOfEnemyCars.Add(_cars[i]);
                     _cars.RemoveAt(i);
                 }
             }
