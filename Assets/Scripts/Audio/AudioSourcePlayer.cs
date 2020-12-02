bae1e470c83da67680fe1642290cde58358bc0a0
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-12-02 09:14:10 +0200

    Added logic for dinamic sound change in AudioSoursePlayer script

--- Assets/Scripts/Audio/AudioSourcePlayer.cs
+++ Assets/Scripts/Audio/AudioSourcePlayer.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Sirenix.OdinInspector;
+using Game;
+using Events;
+using System;
 
 namespace Audio {
 
@@ -9,14 +12,32 @@ namespace Audio {
         [SerializeField]
         private AudioSource _audioSource;
 
+        [SerializeField]
+        private ScriptableFloatValue _currentVolume;
+
+        [SerializeField]
+        private EventListener _volumeSliderPositionChangedEventListener;
+
         [Button]
         public void Play() {
+            _audioSource.volume = _currentVolume.value;
             _audioSource.Play();
         }
 
         [Button]
         public void Stop() {
             _audioSource.Stop();
+        }
+
+
+        private void Awake() {
+            _audioSource.volume = PlayerPrefs.GetFloat("SavedVolume");
+            _audioSource.maxDistance = PlayerPrefs.GetFloat("SavedVolume");
+            _volumeSliderPositionChangedEventListener.OnEventHappened += OnVolumeSliderPositionChanged;
+        }
+
+        private void OnVolumeSliderPositionChanged() {
+            _audioSource.volume = _currentVolume.value;
         }
 
         public void PlayMusic(float growingTime) {
@@ -29,7 +50,7 @@ namespace Audio {
 
         private IEnumerator GradualIncrease(float necessaryTime) {
             Play();
-            yield return StartCoroutine(MusicVolume(1f, necessaryTime));
+            yield return StartCoroutine(MusicVolume(_currentVolume.value, necessaryTime));
         }
 
         private IEnumerator GradualDecline(float necessaryTime) {
@@ -40,7 +61,7 @@ namespace Audio {
         private IEnumerator MusicVolume(float finalSound, float necessaryTime) {
             var time = 0f;
 
-            while (time < necessaryTime) {
+            while (time < necessaryTime && _audioSource.volume != finalSound) {
                 time += Time.deltaTime;
                 _audioSource.volume = Mathf.Lerp(_audioSource.volume, finalSound, time / necessaryTime);
                 yield return null;
