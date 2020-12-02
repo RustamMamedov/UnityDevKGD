7dc87162c95fed750aa0288050d0326f7e223613
Dan <kdanil01@mail.ru>
2020-12-02 17:30:23 +0200

    Created the AudioSlider class

---
+++ Assets/Scripts/Audio/AudioSlider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Audio {
+
+    [RequireComponent(typeof(Slider))]
+    public class AudioSlider : MonoBehaviour {
+
+        [SerializeField]
+        private AudioManager _audioManager;
+
+        [SerializeField]
+        private Slider _slider;
+        
+        private void OnEnable() {
+            _slider = GetComponent<Slider>();
+            _slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(float value) {
+            _audioManager.PlaySliderSound(value);
+        }
+    }
+}
