7caf0ed80b581a711aa59393690dd56fd4fa932c
alexsilkov <34021525+alexsilkov@users.noreply.github.com>
2020-12-02 09:14:48 +0200

    Added volume logic in SettingsScreen script

--- Assets/Scripts/UI/SettingsScreen.cs
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using Game;
+using Events;
 
 namespace UI {
 
@@ -23,14 +23,21 @@ namespace UI {
         [SerializeField]
         private Button _acceptButton;
 
+        [SerializeField]
+        private ScriptableFloatValue _currentVolumeAsset;
+
+        [SerializeField]
+        private EventDispatcher _volumeSliderPositionChangedEventDispatcher;
+
         private float _volumeToSave;
         private int _difficultyToSave;
         private int _lightingToSave;
 
 
         private void Awake() {
-            _acceptButton.onClick.AddListener(AcceptButtonClick);
-            _cancelButton.onClick.AddListener(CancelButtonClick);
+            _acceptButton.onClick.AddListener(OnAcceptButtonClick);
+            _cancelButton.onClick.AddListener(OnCancelButtonClick);
+            _volumeSlider.onValueChanged.AddListener(OnVolumeSliderPositionChanged) ;
             if (!PlayerPrefs.HasKey("SavedVolume")) {
                 _volumeToSave = _volumeSlider.value;
                 _difficultyToSave = Convert.ToInt32(_difficultyToggle.isOn);
@@ -46,7 +53,16 @@ namespace UI {
             }
         }
 
-        private void AcceptButtonClick() {
+        private void OnVolumeSliderPositionChanged(float value) {
+            _currentVolumeAsset.value = value;
+            _volumeSliderPositionChangedEventDispatcher.Dispatch();
+
+        }
+
+        private void Update() {
+            _currentVolumeAsset.value = _volumeSlider.value;
+        }
+        private void OnAcceptButtonClick() {
             _volumeToSave = _volumeSlider.value;
             _difficultyToSave = Convert.ToInt32(_difficultyToggle.isOn);
             _lightingToSave = Convert.ToInt32(_lightingToggle.isOn);
@@ -56,7 +72,8 @@ namespace UI {
             UIManager.Instansce.ShutDownSettingsScreen();
         }
 
-        private void CancelButtonClick() {
+        private void OnCancelButtonClick() {
+            _currentVolumeAsset.value = PlayerPrefs.GetFloat("SavedVolume");
             _volumeSlider.value = PlayerPrefs.GetFloat("SavedVolume");
             _difficultyToggle.isOn = Convert.ToBoolean(PlayerPrefs.GetInt("SavedDifficulty"));
             _lightingToggle.isOn = Convert.ToBoolean(PlayerPrefs.GetInt("SavedLight"));
