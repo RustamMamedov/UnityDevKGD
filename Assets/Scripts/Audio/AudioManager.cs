9d792c50bd4b29c9f862f48a9dbfe64e5f9d6c2a
Dan <kdanil01@mail.ru>
2020-12-02 17:30:07 +0200

    Created the AudioManager class and prefab

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Audio {
+    public class AudioManager : MonoBehaviour {
+     
+        [SerializeField]
+        private AudioSourcePlayer _buttonSound;
+
+        [SerializeField]
+        private AudioSourcePlayer _sliderSound;
+
+        public void PlayButtonSound() {
+            _buttonSound.Play();
+        }
+
+        public void PlaySliderSound(float value) {
+            _sliderSound.SetPitch(value / 2 + 0.5f);
+            _sliderSound.Play();
+        }
+    }
+}
