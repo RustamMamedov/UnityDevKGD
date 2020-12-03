a4701328a18b1a7fdd07c82e5188477c42ed8792
Anastasia <nastya_kalinina2000@mail.ru>
2020-12-03 18:23:15 +0200

    day and night and lights

--- Assets/Scripts/Cars/CarLight.cs
+++ Assets/Scripts/Cars/CarLight.cs
@@ -9,13 +9,23 @@ namespace Game {
 
         [SerializeField]
         private Light _light;
-        private void OnDrawGizmos() {
-            Gizmos.matrix = transform.localToWorldMatrix;
-            _light.range = _settings.lenghLightCar * 10f;
-            Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _settings.lenghLightCar, 1f, 1f);
-        }
+
+        [SerializeField]
+        private ScriptableIntValue _illumination;
+
+        //private void OnDrawGizmos() {
+        //    Gizmos.matrix = transform.localToWorldMatrix;
+        //    _light.range = _settings.lenghLightCar * 10f;
+        //    Gizmos.DrawFrustum(new Vector3(0f, 0f, -1f), 30f, _settings.lenghLightCar, 1f, 1f);
+        //}
 
         private void OnEnable() {
+            if(_illumination.value == 1) {
+                _light.gameObject.SetActive(true);
+            }
+            else {
+                _light.gameObject.SetActive(false);
+            }
             _light.range = _settings.lenghLightCar * 10f;
         }
     }
