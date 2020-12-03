eb125bfea4280fc00600a671aa59ce84b53eb5fa
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-12-04 01:55:22 +0200

    render enemy car + light
    - settings camera on CarSettings enemyCar
    - render manager added settings 
    -car light script changed

--- Assets/Scripts/Game/CarLight.cs
+++ Assets/Scripts/Game/CarLight.cs
@@ -12,12 +12,23 @@ namespace Game {
         [SerializeField]
         private CarSettings _carSettings;
 
+        [SerializeField]
+        private Light _light;
+
+        private void Awake() {
+            if (_light == null) {
+                return;
+            }
+
+            _light.range = _carSettings.CarLight * 10f;
+        }
+
         private void OnDrawGizmosSelected() {
 
             Gizmos.color = _gizmosColor;
             var tempMatrix = Gizmos.matrix;
             Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.DrawFrustum(Vector3.zero, 45f, 15f, 3f, 1f);
+            Gizmos.DrawFrustum(Vector3.zero, 45f, 0f, _carSettings.CarLight, 1f);
             Gizmos.matrix = tempMatrix;
         }
     }
--- Assets/Scripts/UI/RenderManager.cs
+++ Assets/Scripts/UI/RenderManager.cs
@@ -1,40 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace UI {
 
-
     public class RenderManager : MonoBehaviour {
 
-	public static RenderManager Instance;
+        public static RenderManager Instance;
 
-	[SerializeField]
-	private Camera _renderCamera;
+        [SerializeField]
+        private Camera _renderCamera;
 
-	[SerializeField]
-	private Transform _rootTransform;
+        [SerializeField]
+        private Transform _rootTransform;
 
-	private RenderTexture _texture;
+        private RenderTexture _texture;
 
-	private void Awake() {
-	    Instance = this;
-	}
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
 
-	public RenderTexture Render(GameObject prefab) {
-	    var carInstance = Instantiate(prefab, _rootTransform);
-	    _texture = RenderTexture.GetTemporary(64, 64, 16);
-	    _texture.antiAliasing = 8;
-	    _texture.Create();
-	    _renderCamera.targetTexture = _texture;
-	    _renderCamera.Render();
-	    _renderCamera.targetTexture = null;
-	    Destroy(carInstance);
-	    return _texture;
-	}
+        public RenderTexture Render(GameObject prefab, Vector3 position, Quaternion rotation) {
+            var carInstance = Instantiate(prefab, _rootTransform);
+            _texture = RenderTexture.GetTemporary(64, 64, 16);
+            _texture.antiAliasing = 8;
+            _texture.Create();
 
-	public void ReleaseTextures() {
-	    RenderTexture.ReleaseTemporary(_texture);
-	}
+            _renderCamera.transform.position = position;
+            _renderCamera.transform.rotation = rotation;
+            _renderCamera.targetTexture = _texture;
+            _renderCamera.Render();
+            _renderCamera.targetTexture = null;
+
+            Destroy(carInstance);
+            return _texture;
+        }
+
+        public void ReleaseTextures() {
+            RenderTexture.ReleaseTemporary(_texture);
+        }
     }
 }
