e46129c4485224b9f1c9cd8bee0333ba7cf36bf0
ErvinRudakov <ervin_rudakov@mail.ru>
2020-12-04 20:12:36 +0200

    Pool Added (Homework11)

--- Assets/Scripts/Game/EnemySpawner.cs
+++ Assets/Scripts/Game/EnemySpawner.cs
@@ -50,7 +50,8 @@ namespace Game {
         private int _numberOfPrefab;
         private float _currentTimer;
         private bool _dodged;
-        private List<GameObject> _cars = new List<GameObject>();
+        private List<Transform> _cars = new List<Transform>();
+        private Dictionary<int,Stack<Transform>> _stack;
 
         private bool ValidatePrefabsList(List<GameObject> prefabs) {
             for (int i = 0; i < prefabs.Count - 1; i++) {
@@ -83,31 +84,61 @@ namespace Game {
                 return;
             }
             _currentTimer = 0f;
+            GeneratePool();
             SpawnCar();
         }
         private void Dodge(CarSettings carSettings) {
                 carSettings.dodgeScoreValue.value++;
         }
         private void SpawnCar() {
+            _numberOfPrefab = Random.Range(0, 3);
+            var car = GetCarFromStack(_numberOfPrefab);
             var randomRoad = Random.Range(-1, 2);
             var position = new Vector3(1f * randomRoad * _roadWidth.value, 0f, _playerPositionZ.value + _distanceToPlayerToSpawn);
-            _numberOfPrefab = Random.Range(0, 3);
-            var car = Instantiate(_carPrefab[_numberOfPrefab], position, Quaternion.Euler(0f, 180f, 0f));
+            car.position = position;
             _cars.Add(car);
             _dodged = true;
             _enemiesSettings.Add(_numberOfPrefab);
         }
+        private GameObject CreateCar(int i) {
+            var car = Instantiate(_carPrefab[i], Vector3.zero, Quaternion.Euler(0f,180f,0f));
+            car.SetActive(false);
+            return car;
+        }
+        private void GeneratePool() {
+            _stack = new Dictionary<int, Stack<Transform>>();
+            for(int i=0;i<_carPrefab.Count;i++) {
+                _stack[i] = new Stack<Transform>();
+                _stack[i].Push(CreateCar(i).transform);
+            }
+        }
+        private Transform GetCarFromStack(int i) {
+            if (_stack[i].Count == 0) {
+                _stack[i].Push(CreateCar(i).transform);
+            }
+            var car = _stack[i].Pop();
+            car.gameObject.SetActive(true);
+            return car;
+        }
+        private void SetCarToStack(Transform car,int i) {
+            car.gameObject.SetActive(false);
+            _stack[i].Push(car);
+        }
+        private void MoveCarToStack(int index, int i) {
+            var car = _cars[index];
+            _cars.RemoveAt(index);
+            _enemiesSettings.RemoveAt(index);
+            SetCarToStack(car, i);
+        }
         private void HandleCarsBehindPlayer() {
             for (int i = _cars.Count - 1; i > -1; i--) {
-                if ((_playerPositionZ.value - _cars[i].transform.position.z >= _playerLength.value / 2 + _enemiesLength[_enemiesSettings[i]].value / 2) && (_dodged)) {
+                if ((_playerPositionZ.value - _cars[i].position.z >= _playerLength.value / 2 + _enemiesLength[_enemiesSettings[i]].value / 2) && (_dodged)) {
                     Dodge(_enemiesScores[_enemiesSettings[i]]);
                     _currentScore.value += _enemiesScores[_enemiesSettings[i]].dodgeScore;
                     _dodged = false;
                 }
-                if (_playerPositionZ.value - _cars[i].transform.position.z > _distanceToPlayerToDestroy) {
-                    Destroy(_cars[i]);
-                    _cars.RemoveAt(i);
-                    _enemiesSettings.RemoveAt(i);
+                if (_playerPositionZ.value - _cars[i].position.z > _distanceToPlayerToDestroy) {
+                    MoveCarToStack(i, _enemiesSettings[i]);
                 }
             }
         }
---
+++ Assets/Scripts/Managers/UI/UIManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Audio;
+using Events;
+
+namespace UI {
+
+    public class UIManager : MonoBehaviour {
+
+        public static UIManager Instance;
+
+        [SerializeField]
+        private Fader _fader;
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private GameObject _menuScreen;
+
+        [SerializeField]
+        private GameObject _gameScreen;
+
+        [SerializeField]
+        private GameObject _leaderboardScreen;
+
+        [SerializeField]
+        private GameObject _settingsScreen;
+
+        [SerializeField]
+        private MusicManager _musicManager;
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        private void Start() {
+            ShowMenuScreen();
+        }
+        public void LoadMenu() {
+            _fader.OnFadeOut += LoadMenuScene;
+            _fader.FadeOut();
+        }
+
+        public void LoadGameplay() {
+            _fader.OnFadeOut += LoadGameplayScene;
+            _fader.FadeOut();
+        }
+        private void LoadMenuScene() {
+            _fader.OnFadeOut -= LoadMenuScene;
+            StartCoroutine(LoadSceneCoroutine("Menu"));
+            ShowMenuScreen();
+        }
+        private void LoadGameplayScene() {
+            _fader.OnFadeOut -= LoadGameplayScene;
+            StartCoroutine(LoadSceneCoroutine("Gameplay"));
+            ShowGameScreen();
+        }
+        private IEnumerator LoadSceneCoroutine(string sceneName) {
+            var asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+            while (!asyncOperation.isDone) {
+                yield return null;
+            }
+            _fader.FadeIn();
+        }
+
+        public void ShowMenuScreen() {
+            HideAllScreens();
+            StartCoroutine(_musicManager.MuteGameCoroutine());
+            _menuScreen.SetActive(true);
+            _musicManager.PlayMenuMusic();
+        }
+
+        public void ShowGameScreen() {
+            HideAllScreens();
+            StartCoroutine(_musicManager.MuteMenuCoroutine());
+            _gameScreen.SetActive(true);
+            _musicManager.PlayGameMusic();
+        }
+
+        public void ShowLeaderboardsScreen() {
+            HideAllScreens();
+            _leaderboardScreen.SetActive(true);
+        }
+
+        public void SetSettings() {
+            _musicManager.SetVolume();
+        }
+        public void ShowSettingsScreen() {
+            HideAllScreens();
+            _settingsScreen.SetActive(true);
+            _updateEventListener.OnEventHappened += SetSettings;
+        }
+        public void HideAllScreens() {
+            _menuScreen.SetActive(false);
+            _gameScreen.SetActive(false);
+            _leaderboardScreen.SetActive(false);
+            if (_settingsScreen.active) {
+                _settingsScreen.SetActive(false);
+                _updateEventListener.OnEventHappened -= SetSettings;
+            }
+        }
+    }
+}
