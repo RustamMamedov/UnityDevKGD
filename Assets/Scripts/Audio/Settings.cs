b71832d9adc62c6498c9cbc52ccb99fd5218fddb
Rfnt <kvotua.job@gmail.com>
2020-12-05 02:33:55 +0200

    change day night

---
+++ Assets/Scripts/Audio/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UI;
+using Game;
+
+namespace Audio {
+    public class Settings : MonoBehaviour {
+
+        [SerializeField]
+        Slider _sliderVolume;
+
+        [SerializeField]
+        private Button _dayButton;
+
+        [SerializeField]
+        private Button _nightButton;
+
+        [SerializeField]
+        private ScriptableIntValue _dayOrNight;
+
+        private void Awake() {
+            _dayButton.onClick.AddListener(OnDayButtonClick);
+            _nightButton.onClick.AddListener(OnNightButtonClick);
+        }
+
+        private void OnDayButtonClick() {
+            _dayOrNight.value = 1;
+        }
+
+        private void OnNightButtonClick() {
+            _dayOrNight.value = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/DayNight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Game {
+    public class DayNight : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject[] _lightCar;
+
+        [SerializeField]
+        private GameObject _lightDir;
+
+        [SerializeField]
+        private ScriptableIntValue _dayOrNight;
+
+        private void Awake() {
+            if (_dayOrNight) {
+                _lightCar[0].SetActive(false);
+                _lightCar[1].SetActive(false);
+                _lightDir.SetActive(true);
+            } else {
+                _lightCar[0].SetActive(true);
+                _lightCar[1].SetActive(true);
+                _lightDir.SetActive(false);
+            }
+        }
+    }
+}
