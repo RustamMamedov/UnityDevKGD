06229e67f318ec535ba90643b7eacf34c083b2ad
ErvinRudakov <ervin_rudakov@mail.ru>
2020-12-07 18:27:28 +0200

    Star.cs added

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Audio;
+
+namespace Game {
+
+    public class Star : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject _star;
+        [SerializeField]
+        private AudioSourcePlayer _takeAudio;
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _star.SetActive(false);
+                _takeAudio.Play();
+            }
+        }
+    }
+}
