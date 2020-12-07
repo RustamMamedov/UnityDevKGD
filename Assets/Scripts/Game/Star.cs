6617e676d61706982ffb803d6547f283bc322565
Potapenko_Ivan <van.van123@list.ru>
2020-12-07 19:35:19 +0200

    Star script added

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+    public class Star : MonoBehaviour {
+        [SerializeField]
+        private EventDispatcher _onStarCollisionEventDispatcher;
+
+        [SerializeField]
+        private EventListener _updateEventListner;
+
+        [SerializeField]
+        private ScriptableFloatValue _playerPositionZ;
+
+        [SerializeField]
+        private float _distanceToDestroy;
+
+        private void OnEnable() {
+            _updateEventListner.OnEventHappened += 
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _onStarCollisionEventDispatcher.Dispatch();
+                Destroy(gameObject);
+            }
+        }
+
+        private void UpdateBehaviour(){
+           if(gameObject.transform.position.z+_distanceToDestroy<_playerPositionZ.value) {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
+
