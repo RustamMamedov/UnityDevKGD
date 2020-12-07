1aae4a239cad1fc5d722c7dd0bbda9636f54c36a
Potapenko_Ivan <van.van123@list.ru>
2020-12-07 19:35:42 +0200

    Star spawn realize

--- Assets/Scripts/Game/EnemyCar.cs
+++ Assets/Scripts/Game/EnemyCar.cs
@@ -17,10 +17,18 @@ namespace Game {
         [SerializeField]
         private ScriptableStringValue _dodgedCarName;
 
+        [SerializeField]
+        private GameObject _star;
+
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 _carCollisionEventDispatcher.Dispatch();
             }
+        }
+
+        private void OnEnable() {
+            var zPlusStarPosition = (Random.Range(0, 2) ==0 ? -1f : 1f)*_carSettings.distanceToStar;
+            Instantiate(_star, new Vector3(gameObject.transform.position.x,0,gameObject.transform.position.z+zPlusStarPosition),Quaternion.identity); 
         }
 
         private void OnTriggerExit(Collider other) {
