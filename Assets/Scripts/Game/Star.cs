ba44f4ace612e55f4e8be6da15cf978a3a0aa2b7
Maxim <korunets1901@gmail.com>
2020-12-07 17:17:30 +0200

    Create script Star

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    public class Star : MonoBehaviour
+    {
+        private int _index;
+
+        public void SetIndex() {
+            
+        }
+    }
+
+}
