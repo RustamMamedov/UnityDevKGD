40d3a56547842a5062dfe5e53c56e71e03c3e286
Dan <kdanil01@mail.ru>
2020-12-07 20:47:43 +0300

    Created the Star class

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,22 @@
+﻿using Events;
+using Audio;
+using UnityEngine;
+
+namespace Game {
+
+    public class Star : MonoBehaviour {
+
+        [SerializeField]
+        private EventDispatcher _starCollectedEventDispatcher;
+
+        [SerializeField]
+        private AudioSourcePlayer _starAudio;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _starCollectedEventDispatcher.Dispatch();
+                _starAudio.Play();
+            }
+        }
+    }
+}
