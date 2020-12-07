ce857025107cf7005439a3bab582b758a080bb39
Andrey <DrEMartstud@gmail.com>
2020-12-07 19:04:58 +0200

    Star plays sound and disables on collision

--- Assets/Scripts/Game/Stars.cs
+++ Assets/Scripts/Game/Stars.cs
@@ -1,9 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using Audio;
 using UnityEngine;
 
 namespace Game {
 
-    public class Stars : MonoBehaviour { }
-
+    public class Stars : MonoBehaviour {
+        
+        [SerializeField]
+        private GameObject _star;
+        
+        [SerializeField]
+        private AudioSourcePlayer _takeAudio;
+        
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                _takeAudio.Play();
+                _star.SetActive(false);
+            }
+        }
+    }
 }
