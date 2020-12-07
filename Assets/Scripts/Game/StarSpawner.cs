92eb9d00d134ff77bdd2c3646b9722f4741a7728
Alexandr <navacar@mail.ru>
2020-12-07 19:19:35 +0200

    StarSpawn random position

--- Assets/Scripts/Game/StarSpawner.cs
+++ Assets/Scripts/Game/StarSpawner.cs
@@ -6,7 +6,24 @@ namespace Game {
 
     public class StarSpawner : MonoBehaviour {
 
-        
+        [SerializeField]
+        private GameObject _star;
+
+        [SerializeField]
+        private ScriptableIntValue _spawnPosition;
+
+        [SerializeField]
+        private GameObject _car;
+
+
+        private void SetStarSpawnPosition() {
+            if(_spawnPosition == 1) {
+                _star.transform.position = _car.transform.position;
+            }
+            if(_spawnPosition == 2) {
+                _star.transform.position = _car.transform.position + _car.width();
+            }
+        }
 
     }
 }
