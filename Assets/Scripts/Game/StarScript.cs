97f1dbd0c882357a07dae2e9e95562b6b48e66db
Nikita <Nkanunov@gmail.com>
2020-12-07 19:13:13 +0200

    Created StarScript script

---
+++ Assets/Scripts/Game/StarScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Audio;
+
+namespace Game {
+
+    public class StarScript : MonoBehaviour {
+        
+        [SerializeField]
+        private AudioSourcePlayer _starAudio;
+
+        [SerializeField]
+        private ScriptableFloatValue _starDistance;
+    }
+}
