bf95c1418239f6644d0455f85790937898bdebe0
gg <71404862+gqggqg@users.noreply.github.com>
2020-12-07 20:33:56 +0200

    class Star created

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Audio;
+
+namespace Game {
+
+    public class Star : MonoBehaviour {
+
+        [SerializeField]
+        private AudioSourcePlayer _starAudio;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag == "Player") {
+                _starAudio.Play();
+            }
+        }
+    }
+}
