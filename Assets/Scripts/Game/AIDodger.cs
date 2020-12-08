d3dafd0cc562f84c5deb0bcd6e8a346ae1b341bf
Rfnt <kvotua.job@gmail.com>
2020-12-08 18:06:04 +0200

    created Dodger class

---
+++ Assets/Scripts/Game/AIDodger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Events;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public class AIDodger : MonoBehaviour {
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private AISettings _distanseToCarSpawner; 
+
+        [SerializeField]
+        private ScriptableFloatValue _playerPositionZ;
+
+        private void OnEnable() {
+            _updateEventListener.OnEventHappened += UpdateBehaviour;
+        }
+
+        private void UpdateBehaviour() {
+            
+        }
+    }
+}
