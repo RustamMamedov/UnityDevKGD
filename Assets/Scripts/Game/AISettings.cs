bddc1e265e7a5c74bc2d08dc877456155774627a
Anastasia <nastya_kalinina2000@mail.ru>
2020-12-08 19:41:30 +0200

    created AISettings Asset

---
+++ Assets/Scripts/Game/AISettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Sirenix.OdinInspector;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "AI Settings", menuName = "AISettings")]
+    public class AISettings : ScriptableObject {
+
+        [ValidateInput(nameof(CorrectnessDistance))]
+        public float distance;
+
+        private bool CorrectnessDistance() {
+            return (distance > 0 && distance % 2 == 0);
+        }
+    }
+}
