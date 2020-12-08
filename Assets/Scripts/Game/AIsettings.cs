3c22c7a662ac55b1c752421eb4db78fb0a97f00e
SaraJess <sarycheva.00@mail.ru>
2020-12-08 19:04:03 +0200

    Create and added AI Settings

---
+++ Assets/Scripts/Game/AIsettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Sirenix.OdinInspector;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "AI Settings",menuName = "new AI Settings")]
+    public class AIsettings : ScriptableObject {
+
+        [ValidateInput(nameof(ValidateDistace))]
+        public float distance;
+
+        private bool ValidateDistace() {
+            return (distance > 0 && distance % 2 == 0) ? true : false;
+        }
+    }
+}
