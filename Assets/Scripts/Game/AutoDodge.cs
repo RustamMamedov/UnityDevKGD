468a2b20d53c01bdb6a9f5e88b16bec261d43d2c
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-08 17:13:27 +0200

    Auto dogde script added

---
+++ Assets/Scripts/Game/AutoDodge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    public class AutoDodge : MonoBehaviour {
+
+        [SerializeField]
+        private BoxCollider _detectCollider;
+
+        [SerializeField]
+        private float _distanceForDodge;
+
+        private void OnEnable() {
+            _detectCollider.center = new Vector3(0, 0, _distanceForDodge / 2);
+            _detectCollider.size = new Vector3(0, 0, _distanceForDodge);
+        }
+    }
+}
