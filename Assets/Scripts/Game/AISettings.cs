8b17f0b167880d43db443ae355157ee37be3bb1e
Evgenii <jekaweek1@gmail.com>
2020-12-08 18:16:17 +0200

    Add AISettings script

---
+++ Assets/Scripts/Game/AISettings.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+namespace Game {
+
+        [CreateAssetMenu(fileName = "AISettings", menuName = "AISettings")]
+        public class AISettings : ScriptableObject{
+        [InfoBox("enter an even positive value", "ValidateDodgeScore", InfoMessageType = InfoMessageType.Error)]
+        public int distance;
+
+        private bool ValidateDodgeScore(int score) {
+            return score < 0 || score % 2 == 1;
+        }
+    }
+}
